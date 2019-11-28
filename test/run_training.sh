9e7bf53d31e866a54560e7a34c67b6fef77ee386
Javier Duarte <jduarte@ucsd.edu>
2019-11-27 20:09:05 -0800

    Update run_training.sh

--- test/run_training.sh
+++ test/run_training.sh
@@ -2,6 +2,4 @@
 
 SINGULARITY_IMAGE=/storage/user/jpata/gpuservers/singularity/images/over_edge.simg
 
-#singularity exec --nv -B /storage $SINGULARITY_IMAGE python3 test/train_gnn.py
-#singularity exec --nv -B /storage $SINGULARITY_IMAGE python3 test/draw_graphs.py
-singularity exec --nv -B /storage $SINGULARITY_IMAGE python3 test/benchmark_solution.py /storage/user/jduarte/particleflow/graph_data/raw/step3_AOD_1_ev1*.npz
+singularity exec --nv -B /storage $SINGULARITY_IMAGE python3 test/training.py
