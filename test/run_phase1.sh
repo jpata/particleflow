8b926e8d250085effc9144558a18135c47e002bf
Joosep Pata <joosep.pata@cern.ch>
2020-02-19 14:52:44 -0800

    up

--- test/run_phase1.sh
+++ test/run_phase1.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-#cmsDriver.py TTbar_14TeV_TuneCUETP8M1_cfi \
-#  --conditions auto:phase1_2021_realistic \
-#  -n 10 \
-#  --era Run3 \
-#  --eventcontent FEVTDEBUGHLT \
-#  -s GEN,SIM,DIGI,L1,DIGI2RAW,HLT \
-#  --datatier GEN-SIM \
-#  --geometry DB:Extended \
-#  --pileup Run3_Flat55To75_PoissonOOTPU \
-#  --pileup_input das:/RelValMinBias_14TeV/CMSSW_11_0_0_pre12-110X_mcRun3_2021_realistic_v5-v1/GEN-SIM \
-#  --no_exec \
-#  --python_filename=step2_phase1.py
+#Generate the MC
+cmsDriver.py TTbar_14TeV_TuneCUETP8M1_cfi \
+  --conditions auto:phase1_2021_realistic \
+  -n 10 \
+  --era Run3 \
+  --eventcontent FEVTDEBUGHLT \
+  -s GEN,SIM,DIGI,L1,DIGI2RAW,HLT \
+  --datatier GEN-SIM \
+  --geometry DB:Extended \
+  --pileup Run3_Flat55To75_PoissonOOTPU \
+  --pileup_input das:/RelValMinBias_14TeV/CMSSW_11_0_0_pre12-110X_mcRun3_2021_realistic_v5-v1/GEN-SIM \
+  --no_exec \
+  --python_filename=step2_phase1.py
 
+Run the reco sequences
 cmsDriver.py step3 \
   --conditions auto:phase1_2021_realistic \
   -n 10 \
@@ -23,5 +25,8 @@ cmsDriver.py step3 \
   --datatier GEN-SIM-RECO \
   --geometry DB:Extended \
   --no_exec \
-  --python_filename=RECO_fragment_phase1.py
+  --python_filename=step3_phase1.py
 
+#cmsRun step2_phase1.py
+#cmsRun step3_phase1.py
+#cmsRun CMSSW_11_0_0_pre12/src/RecoNtuples/HGCalAnalysis/test/pfntuple.py
---
+++ test/step2_phase1.py
@@ -0,0 +1,184 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: TTbar_14TeV_TuneCUETP8M1_cfi --conditions auto:phase1_2021_realistic -n 10 --era Run3 --eventcontent FEVTDEBUGHLT -s GEN,SIM,DIGI,L1,DIGI2RAW,HLT --datatier GEN-SIM --geometry DB:Extended --pileup Run3_Flat55To75_PoissonOOTPU --pileup_input das:/RelValMinBias_14TeV/CMSSW_11_0_0_pre12-110X_mcRun3_2021_realistic_v5-v1/GEN-SIM --no_exec --python_filename=step2_phase1.py
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Eras.Era_Run3_cff import Run3
+
+process = cms.Process('HLT',Run3)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mix_Run3_Flat55To75_PoissonOOTPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.GeometrySimDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic50ns13TeVCollision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.Digi_cff')
+process.load('Configuration.StandardSequences.SimL1Emulator_cff')
+process.load('Configuration.StandardSequences.DigiToRaw_cff')
+process.load('HLTrigger.Configuration.HLT_GRun_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1000),
+    output = cms.optional.untracked.allowed(cms.int32,cms.PSet)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+    FailPath = cms.untracked.vstring(),
+    IgnoreCompletely = cms.untracked.vstring(),
+    Rethrow = cms.untracked.vstring(),
+    SkipEvent = cms.untracked.vstring(),
+    allowUnscheduled = cms.obsolete.untracked.bool,
+    canDeleteEarly = cms.untracked.vstring(),
+    emptyRunLumiMode = cms.obsolete.untracked.string,
+    eventSetup = cms.untracked.PSet(
+        forceNumberOfConcurrentIOVs = cms.untracked.PSet(
+
+        ),
+        numberOfConcurrentIOVs = cms.untracked.uint32(1)
+    ),
+    fileMode = cms.untracked.string('FULLMERGE'),
+    forceEventSetupCacheClearOnNewRun = cms.untracked.bool(False),
+    makeTriggerResults = cms.obsolete.untracked.bool,
+    numberOfConcurrentLuminosityBlocks = cms.untracked.uint32(1),
+    numberOfConcurrentRuns = cms.untracked.uint32(1),
+    numberOfStreams = cms.untracked.uint32(0),
+    numberOfThreads = cms.untracked.uint32(1),
+    printDependencies = cms.untracked.bool(False),
+    sizeOfStackForThreadsInKB = cms.optional.untracked.uint32,
+    throwIfIllegalParameter = cms.untracked.bool(True),
+    wantSummary = cms.untracked.bool(False)
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('TTbar_14TeV_TuneCUETP8M1_cfi nevts:10'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.FEVTDEBUGHLToutput = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    ),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN-SIM'),
+        filterName = cms.untracked.string('')
+    ),
+    fileName = cms.untracked.string('step2_phase1_ev1000.root'),
+    outputCommands = process.FEVTDEBUGHLTEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+# Additional output definition
+
+# Other statements
+process.mix.input.fileNames = cms.untracked.vstring(['/store/relval/CMSSW_11_0_0_pre12/RelValMinBias_14TeV/GEN-SIM/110X_mcRun3_2021_realistic_v5-v1/20000/DF6784EE-E9E0-2B4A-9A4A-D2C7F8FD8CD6.root', '/store/relval/CMSSW_11_0_0_pre12/RelValMinBias_14TeV/GEN-SIM/110X_mcRun3_2021_realistic_v5-v1/20000/0334A557-4FEF-B940-9A73-B78C4421D53B.root', '/store/relval/CMSSW_11_0_0_pre12/RelValMinBias_14TeV/GEN-SIM/110X_mcRun3_2021_realistic_v5-v1/20000/40BABF7C-A1A6-EC44-98C3-5775528DBA15.root', '/store/relval/CMSSW_11_0_0_pre12/RelValMinBias_14TeV/GEN-SIM/110X_mcRun3_2021_realistic_v5-v1/20000/ED0B6A22-3C60-6A45-B0F3-0E4D9111F899.root', '/store/relval/CMSSW_11_0_0_pre12/RelValMinBias_14TeV/GEN-SIM/110X_mcRun3_2021_realistic_v5-v1/20000/0F3627E9-25DE-D147-BD84-00CE7D2183AA.root', '/store/relval/CMSSW_11_0_0_pre12/RelValMinBias_14TeV/GEN-SIM/110X_mcRun3_2021_realistic_v5-v1/20000/298060BF-60DD-9840-9C87-992106033ED6.root'])
+process.XMLFromDBSource.label = cms.string("Extended")
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase1_2021_realistic', '')
+
+process.generator = cms.EDFilter("Pythia8GeneratorFilter",
+    PythiaParameters = cms.PSet(
+        parameterSets = cms.vstring(
+            'pythia8CommonSettings', 
+            'pythia8CUEP8M1Settings', 
+            'processParameters'
+        ),
+        processParameters = cms.vstring(
+            'Top:gg2ttbar = on ', 
+            'Top:qqbar2ttbar = on ', 
+            '6:m0 = 175 '
+        ),
+        pythia8CUEP8M1Settings = cms.vstring(
+            'Tune:pp 14', 
+            'Tune:ee 7', 
+            'MultipartonInteractions:pT0Ref=2.4024', 
+            'MultipartonInteractions:ecmPow=0.25208', 
+            'MultipartonInteractions:expPow=1.6'
+        ),
+        pythia8CommonSettings = cms.vstring(
+            'Tune:preferLHAPDF = 2', 
+            'Main:timesAllowErrors = 10000', 
+            'Check:epTolErr = 0.01', 
+            'Beams:setProductionScalesFromLHEF = off', 
+            'SLHA:keepSM = on', 
+            'SLHA:minMassSM = 1000.', 
+            'ParticleDecays:limitTau0 = on', 
+            'ParticleDecays:tau0Max = 10', 
+            'ParticleDecays:allowPhotonRadiation = on'
+        )
+    ),
+    comEnergy = cms.double(14000.0),
+    filterEfficiency = cms.untracked.double(1.0),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    pythiaPylistVerbosity = cms.untracked.int32(0)
+)
+
+
+process.ProductionFilterSequence = cms.Sequence(process.generator)
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.digitisation_step = cms.Path(process.pdigi)
+process.L1simulation_step = cms.Path(process.SimL1Emulator)
+process.digi2raw_step = cms.Path(process.DigiToRaw)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.FEVTDEBUGHLToutput_step = cms.EndPath(process.FEVTDEBUGHLToutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.digitisation_step,process.L1simulation_step,process.digi2raw_step)
+process.schedule.extend(process.HLTSchedule)
+process.schedule.extend([process.endjob_step,process.FEVTDEBUGHLToutput_step])
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path).insert(0, process.ProductionFilterSequence)
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from HLTrigger.Configuration.customizeHLTforMC
+from HLTrigger.Configuration.customizeHLTforMC import customizeHLTforMC 
+
+#call to customisation function customizeHLTforMC imported from HLTrigger.Configuration.customizeHLTforMC
+process = customizeHLTforMC(process)
+
+# End of customisation functions
+
+# Customisation from command line
+
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
+
+process.mix.digitizers.mergedtruth.ignoreTracksOutsideVolume = True
+process.mix.digitizers.mergedtruth.allowDifferentSimHitProcesses = True
+
+#Keep tracks from pileup
+process.mix.digitizers.mergedtruth.select.signalOnlyTP = False
+
+#For Track reco to sim matching
+process.FEVTDEBUGHLToutput.outputCommands.append('keep *_simSiStripDigis_*_*')
+process.FEVTDEBUGHLToutput.outputCommands.append('keep *_simSiPixelDigis_*_*')
