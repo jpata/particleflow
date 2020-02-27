7688237bee1db5c5ec75ab09d50dea3b13736175
Joosep Pata <joosep.pata@cern.ch>
2020-02-27 12:37:39 -0800

    fix generation

--- test/run_phase1.sh
+++ test/run_phase1.sh
@@ -3,34 +3,35 @@
 set -e
 
 #Generate the MC
-#cmsDriver.py TTbar_14TeV_TuneCUETP8M1_cfi \
-#  --conditions auto:phase1_2021_realistic \
-#  -n 100 \
-#  --era Run3 \
-#  --eventcontent FEVTDEBUGHLT \
-#  -s GEN,SIM,DIGI,L1,DIGI2RAW,HLT \
-#  --datatier GEN-SIM \
-#  --geometry DB:Extended \
-#  --pileup Run3_Flat55To75_PoissonOOTPU \
-#  --pileup_input das:/RelValMinBias_14TeV/CMSSW_11_0_0_pre12-110X_mcRun3_2021_realistic_v5-v1/GEN-SIM \
-#  --no_exec \
-#  --fileout step2_phase1.root \
-#  --customise RecoNtuples/HGCalAnalysis/step2_customise.customise \
-#  --python_filename=step2_phase1.py
-#
-##Run the reco sequences
-#cmsDriver.py step3 \
-#  --conditions auto:phase1_2021_realistic \
-#  --era Run3 \
-#  --eventcontent FEVTDEBUGHLT \
-#  --runUnscheduled \
-#  -s RAW2DIGI,L1Reco,RECO,RECOSIM \
-#  --datatier GEN-SIM-RECO \
-#  --geometry DB:Extended \
-#  --no_exec \
-#  --filein step2_phase1.root \
-#  --fileout step3_phase1.root \
-#  --python_filename=step3_phase1.py
+cmsDriver.py TTbar_14TeV_TuneCUETP8M1_cfi \
+  --conditions auto:phase1_2021_realistic \
+  -n 1 \
+  --era Run3 \
+  --eventcontent FEVTDEBUGHLT \
+  -s GEN,SIM,DIGI,L1,DIGI2RAW,HLT \
+  --datatier GEN-SIM \
+  --geometry DB:Extended \
+  --pileup Run3_Flat55To75_PoissonOOTPU \
+  --pileup_input das:/RelValMinBias_14TeV/CMSSW_11_0_0_pre12-110X_mcRun3_2021_realistic_v5-v1/GEN-SIM \
+  --no_exec \
+  --fileout step2_phase1_new.root \
+  --customise Validation/RecoParticleFlow/customize_pfanalysis.customize_step2 \
+  --python_filename=step2_phase1_new.py
+
+#Run the reco sequences
+cmsDriver.py step3 \
+  --conditions auto:phase1_2021_realistic \
+  --era Run3 \
+  --eventcontent FEVTDEBUGHLT \
+  --runUnscheduled \
+  -s RAW2DIGI,L1Reco,RECO,RECOSIM \
+  --datatier GEN-SIM-RECO \
+  --geometry DB:Extended \
+  --no_exec \
+  --filein file:step2_phase1_new.root \
+  --fileout step3_phase1_new.root \
+  --customise Validation/RecoParticleFlow/customize_pfanalysis.customize_step3 \
+  --python_filename=step3_phase1_new.py
 
 cmsRun step2_phase1.py &> log_step2.txt
 cmsRun step3_phase1.py &> log_step3.txt
