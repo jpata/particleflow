e16213383e5e32cff3e132efcb61ab828f470cc9
Joosep Pata <joosep.pata@cern.ch>
2020-04-16 11:31:25 -0700

    up

--- test/run_postprocessing.sh
+++ test/run_postprocessing.sh
@@ -3,29 +3,29 @@
 IMG=~jpata/gpuservers/singularity/images/pytorch.simg
 
 #which dataset to process from data/
-#DATASET=TTbar_14TeV_TuneCUETP8M1_cfi
+DATASET=TTbar_14TeV_TuneCUETP8M1_cfi
 
 #how many event graphs to save in one pickle file
-#PERFILE=1
+PERFILE=1
 
 #how many pickle files to merge to one pytorch batch
-#MERGE=5
+MERGE=5
 
 #DATASET=SingleElectronFlatPt1To100_pythia8_cfi
 #PERFILE=-1
 #MERGE=5
 
-DATASET=SinglePiFlatPt0p7To10_cfi
-PERFILE=-1
-MERGE=10
+#DATASET=SinglePiFlatPt0p7To10_cfi
+#PERFILE=-1
+#MERGE=10
 
 cd data/$DATASET
 
-rm -Rf raw processed
+#rm -Rf raw processed
 mkdir -p raw
 mkdir -p processed
 
-\ls -1 pfntuple*.root | parallel -j24 --gnu python ../../test/postprocessing2.py --input {} --events-per-file $PERFILE --outpath raw --save-normalized-table
+seq 1 10 | parallel -j24 --gnu python ../../test/postprocessing2.py --input pfntuple_{}.root --events-per-file $PERFILE --outpath raw2 --save-normalized-table --save-full-graph --save-images
 
-#cd ../..
-#singularity exec --nv $IMG python test/graph_data.py --dataset data/$DATASET --num-files-merge $MERGE
+cd ../..
+singularity exec --nv $IMG python test/graph_data.py --dataset data/$DATASET --num-files-merge $MERGE
