b84c50af89e34698db1cd69d67d4b0cced581fb6
Joosep Pata <joosep.pata@cern.ch>
2020-05-08 16:55:08 -0700

    freeze

--- test/pred_tf_model.py
+++ test/pred_tf_model.py
@@ -1,25 +1,52 @@
-import setGPU
-import tensorflow as tf
-from tf_model import PFNet, prepare_df
-from tf_data import _parse_tfr_element
+import os
 import time
 import glob
+import numpy as np
 
 def get_X(X,y,w):
     return X
 
+def parse_args():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--weights", type=str, default=None, help="model weights to load")
+    parser.add_argument("--nhidden", type=int, default=256, help="hidden dimension")
+    parser.add_argument("--distance-dim", type=int, default=256, help="distance dimension")
+    parser.add_argument("--num-conv", type=int, default=1, help="number of convolution layers")
+    parser.add_argument("--nthreads", type=int, default=-1, help="number of threads to use")
+    parser.add_argument("--gpu", action="store_true", help="use GPU")
+    args = parser.parse_args()
+    return args
+
 if __name__ == "__main__":
+    args = parse_args()
+
+    if args.gpu:
+        import setGPU
+    else:
+        os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+    import tensorflow as tf
+    from tf_model import PFNet, prepare_df
+    from tf_data import _parse_tfr_element
     tfr_files = glob.glob("data/TTbar_14TeV_TuneCUETP8M1_cfi/tfr/cand/*.tfrecords")
+    tf.config.optimizer.set_jit(True)
 
-    #tf.config.threading.set_inter_op_parallelism_threads(1)
-    #tf.config.threading.set_intra_op_parallelism_threads(1)
-    #tf.config.set_visible_devices([], 'GPU')
+    if args.nthreads > 0:
+        tf.config.threading.set_inter_op_parallelism_threads(args.nthreads)
+        tf.config.threading.set_intra_op_parallelism_threads(args.nthreads)
+    if not args.gpu:
+        tf.config.set_visible_devices([], 'GPU')
 
-    nev = 100
-    dataset = tf.data.TFRecordDataset(tfr_files).map(_parse_tfr_element, num_parallel_calls=1).skip(1000).take(nev)
+    nev = 500
+    dataset = tf.data.TFRecordDataset(tfr_files).map(_parse_tfr_element, num_parallel_calls=1).skip(35000).take(nev)
     dataset_X = dataset.map(get_X)
 
-    model = PFNet(hidden_dim=512, distance_dim=512, num_conv=2)
+    model = PFNet(hidden_dim=args.nhidden, distance_dim=args.distance_dim, num_conv=args.num_conv)
+
+    num_particles = []
+    for X,y,w in dataset:
+        num_particles += [len(y)]
 
     #ensure model is compiled   
     for X in dataset_X:
@@ -27,8 +54,9 @@ if __name__ == "__main__":
         break
 
     #load the weights
-    model.load_weights("experiments/run_02/weights.09-0.038057.hdf5")
-    
+    model.load_weights(args.weights)
+
+    #prepare the dataframe
     prepare_df(0, model, dataset, ".")
 
     print("now timing")
@@ -37,3 +65,24 @@ if __name__ == "__main__":
     print()
     t1 = time.time()
     print("prediction time per event: {:.2f} ms".format(1000.0*(t1-t0)/nev))
+    print("prediction time per particle: {:.2f} us".format(1000000.0*(t1-t0)/np.median(num_particles)))
+
+    #https://leimao.github.io/blog/Save-Load-Inference-From-TF2-Frozen-Graph/
+    full_model = tf.function(lambda x: model(x))
+    full_model = full_model.get_concrete_function(
+        tf.TensorSpec(model.inputs[0].shape, model.inputs[0].dtype))
+
+    # Get frozen ConcreteFunction
+    from tensorflow.python.framework import convert_to_constants
+    frozen_func = convert_to_constants.convert_variables_to_constants_v2(full_model)
+    frozen_func.graph.as_graph_def()
+
+    tf.io.write_graph(graph_or_graph_def=frozen_func.graph,
+                      logdir="./model",
+                      name="frozen_graph.pb",
+                      as_text=False)
+
+    #signatures = {
+    #    'serving_default': model.call,
+    #}
+    #model.save('model', save_format='tf', overwrite=True, include_optimizer=False)
--- test/run_all_postprocessing.sh
+++ test/run_all_postprocessing.sh
@@ -4,6 +4,8 @@
 ./test/run_postprocessing.sh data/SingleElectronFlatPt1To100_pythia8_cfi 100 100
 ./test/run_postprocessing.sh data/SingleMuFlatPt0p7To10_cfi 100 100
 ./test/run_postprocessing.sh data/SinglePi0E10_pythia8_cfi 100 100
+./test/run_postprocessing.sh data/MinBias_14TeV_pythia8_TuneCUETP8M1_cfi 100 100
+./test/run_postprocessing.sh data/SingleTauFlatPt2To150_cfi 100 100
 
 #this one is quite time-consuming, up to 24h
 #./test/run_postprocessing.sh data/TTbar_14TeV_TuneCUETP8M1_cfi 5 100
