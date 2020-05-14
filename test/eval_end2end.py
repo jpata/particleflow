c1ab8e7e5dcd1453d4ffb2783d97c1cdac2eda7b
Javier Duarte <jduarte@ucsd.edu>
2020-05-14 16:34:51 -0700

    PyTorch development (#24)
    * dev
    
    * update pickling
    
    * update kwargs, test->val
    
    * fix eval
    
    * add comet_ml
    
    * allow overwrite
    
    * update plotting for pytorch models
    
    * Update train_end2end.py
    
    Former-commit-id: 63a1405c23304b34411a109b5ee4c04f58ff1669

--- test/eval_end2end.py
+++ test/eval_end2end.py
@@ -4,7 +4,7 @@ import torch_geometric
 import sklearn
 import numpy as np
 import matplotlib.pyplot as plt
-from torch_geometric.data import Data, DataLoader
+from torch_geometric.data import Data, DataLoader, DataListLoader, Batch
 import pandas
 import mplhep
 import pickle
@@ -13,36 +13,50 @@ import graph_data
 import train_end2end
 import time
 
-def collate(batch):
-    print(batch)
-    return batch
-        
+def collate(items):
+    l = sum(items, [])
+    return Batch.from_data_list(l)
+
+
+def parse_args():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, choices=sorted(train_end2end.model_classes.keys()), help="type of model to use", default="PFNet6")
+    parser.add_argument("--path", type=str, help="path to model", default="data/PFNet7_TTbar_14TeV_TuneCUETP8M1_cfi_gen__npar_221073__cfg_ee19d91068__user_jovyan__ntrain_400__lr_0.0001__1588215695")
+    parser.add_argument("--epoch", type=str, default="best", help="Epoch to use; could be 'last' or 'best'")
+    parser.add_argument("--dataset", type=str, help="Input dataset", required=True)
+    parser.add_argument("--start", type=int, default=3800, help="starting testing event")
+    parser.add_argument("--stop", type=int, default=4000, help="stopping testing event")
+    args = parser.parse_args()
+    return args
+
 if __name__ == "__main__":
+    args = parse_args()
     device = torch.device("cuda")
    
-    epoch = 40 
-    model = "PFNet6_TTbar_14TeV_TuneCUETP8M1_cfi_gen__npar_425491__cfg_36ec608897__user_jpata__ntrain_100__lr_0.0002__1586359112"
-    weights = torch.load("data/{}/epoch_{}/weights.pth".format(model, epoch))
-    
-    model = train_end2end.PFNet6(23, 128, 7, dropout_rate=0.2)
+    epoch = args.epoch
+    model = args.model
+    path = args.path
+    weights = torch.load("{}/epoch_{}/weights.pth".format(path, epoch))
+
+    with open('{}/model_kwargs.pkl'.format(path),'rb') as f:
+        model_kwargs = pickle.load(f)
+        
+    model_class = train_end2end.model_classes[args.model]
+    model = model_class(**model_kwargs)
     model.load_state_dict(weights)
     model = model.to(device)
     model.eval()
 
-    for dataset, start, stop in [
-        #("test/SinglePiFlatPt0p7To10_cfi", 900, 1000),
-        #("test/SingleElectronFlatPt1To100_pythia8_cfi", 0, 100),
-        #("test/SingleGammaFlatPt10To100_pythia8_cfi", 0, 100),
-        ("test/TTbar_14TeV_TuneCUETP8M1_cfi", 9000, 10000),
-        ]:
-        print(dataset)    
-        full_dataset = graph_data.PFGraphDataset(root=dataset)
-        test_dataset = torch.utils.data.Subset(full_dataset, np.arange(start=start, stop=stop))
-        
-        loader = DataLoader(test_dataset, batch_size=None, batch_sampler=None, pin_memory=False, shuffle=False)
-        loader.collate_fn = collate
-
-        big_df = train_end2end.prepare_dataframe(model, loader)
-   
-        big_df.to_pickle("{}.pkl.bz2".format(dataset))
-        print(big_df[big_df["cand_pid"]!=1].head())
+    
+    print(args.dataset)    
+    full_dataset = graph_data.PFGraphDataset(root=args.dataset)
+    test_dataset = torch.utils.data.Subset(full_dataset, np.arange(start=args.start, stop=args.stop))
+    
+    loader = DataListLoader(test_dataset, batch_size=1, pin_memory=False, shuffle=False)
+    loader.collate_fn = collate
+    
+    big_df = train_end2end.prepare_dataframe(model, loader)
+    
+    big_df.to_pickle("{}/test.pkl.bz2".format(path))
+    print(big_df[big_df["cand_pid"]!=1].head())
