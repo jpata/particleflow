d756ed8ac310bd7096d9e2d34616a6e4ea0fad45
Joosep Pata <joosep.pata@cern.ch>
2020-05-19 22:17:02 -0700

    lr schedule

--- test/train.sh
+++ test/train.sh
@@ -14,10 +14,6 @@
 
 ###Keras model trainings - experimental
 
-#CUDA_VISIBLE_DEVICES=2,3,4,5 singularity exec -B /storage --nv ~jpata/gpuservers/singularity/images/pytorch.simg \
-#  python3 test/tf_model.py --target cand --ntrain 400000 --ntest 100000 --nepochs 500 --lr 0.00001 \
-#  --nhidden 512 --distance-dim 256 --num-conv 4 --weights inverse
-
-singularity exec -B /storage --nv ~jpata/gpuservers/singularity/images/pytorch.simg \
-  python3 test/tf_model.py --target cand --ntrain 400000 --ntest 100000 --nepochs 500 --lr 0.00001 \
+CUDA_VISIBLE_DEVICES=2,3,4,5,6,7 singularity exec -B /storage --nv ~jpata/gpuservers/singularity/images/pytorch.simg \
+  python3 test/tf_model.py --target cand --ntrain 400000 --ntest 100000 --nepochs 1000 --lr 0.00001 \
   --nhidden 256 --distance-dim 256 --num-conv 4 --weights inverse
