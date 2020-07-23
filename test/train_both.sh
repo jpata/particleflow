7932a1dcaa271f30fa03bd0298234b36c606a691
Kyriacos Xanthos <67141092+kyrxanthos@users.noreply.github.com>
2020-07-23 16:57:07 +0300

    Create train_both.sh

---
+++ test/train_both.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Train both PFNet and PFNet2 models one after the other
+# Parameters have been set to be exactly the same for both models
+
+singularity exec -B /storage --nv ~jpata/gpuservers/singularity/images/pytorch.simg \
+  python3 test/tf_model.py --target cand --ntrain 400000 --ntest 100000 --nepochs 100 --lr 0.00001 \
+  --datapath /storage/group/gpu/bigdata/particleflow/TTbar_14TeV_TuneCUETP8M1_cfi \
+  --nhidden 256 --distance-dim 256 --num-conv 4 --weights inverse --lr-decay 0 --convlayer ghconv --model PFNet \
+&& \
+singularity exec -B /storage --nv ~jpata/gpuservers/singularity/images/pytorch.simg \
+  python3 test/tf_model.py --target cand --ntrain 400000 --ntest 100000 --nepochs 100 --lr 0.00001 \
+  --datapath /storage/group/gpu/bigdata/particleflow/TTbar_14TeV_TuneCUETP8M1_cfi \
+  --nhidden 256 --distance-dim 256 --num-conv 4 --weights inverse --lr-decay 0 --convlayer ghconv --model PFNet2
+  
