8b8dd6b0f3922cf6740d7e2cbfe899e5f1688133
Joosep Pata <joosep.pata@cern.ch>
2020-07-30 04:06:21 -0700

    up

--- test/train.sh
+++ test/train.sh
@@ -6,7 +6,7 @@ singularity exec -B /storage --nv ~jpata/gpuservers/singularity/images/pytorch.s
   --dataset data/TTbar_14TeV_TuneCUETP8M1_cfi \
   --n_train 800 --n_val 200 \
   --model PFNet7 --convlayer gravnet-radius --lr 0.0001 \
-  --hidden_dim 512 --n_epochs 50 \
+  --hidden_dim 256 --n_epochs 50 \
   --l1 1.0 --l2 0.001 --l3 0.0 \
   --target cand --batch_size 1 \
   --dropout 0.2
