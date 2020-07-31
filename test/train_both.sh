bcfd8877f84a6f3e88ebf0d9882e6423aab098a6
Kyriacos Xanthos <67141092+kyrxanthos@users.noreply.github.com>
2020-07-31 13:23:55 +0300

    Update number of epochs

--- test/train_both.sh
+++ test/train_both.sh
@@ -3,12 +3,12 @@
 # Parameters have been set to be exactly the same for both models
 
 singularity exec -B /storage --nv ~jpata/gpuservers/singularity/images/pytorch.simg \
-  python3 test/tf_model.py --target cand --ntrain 400000 --ntest 100000 --nepochs 100 --lr 0.00001 \
+  python3 test/tf_model.py --target cand --ntrain 400000 --ntest 100000 --nepochs 25 --lr 0.00001 \
   --datapath /storage/group/gpu/bigdata/particleflow/TTbar_14TeV_TuneCUETP8M1_cfi \
   --nhidden 256 --distance-dim 256 --num-conv 4 --weights inverse --lr-decay 0 --convlayer ghconv --model PFNet \
 && \
 singularity exec -B /storage --nv ~jpata/gpuservers/singularity/images/pytorch.simg \
-  python3 test/tf_model.py --target cand --ntrain 400000 --ntest 100000 --nepochs 100 --lr 0.00001 \
+  python3 test/tf_model.py --target cand --ntrain 400000 --ntest 100000 --nepochs 25 --lr 0.00001 \
   --datapath /storage/group/gpu/bigdata/particleflow/TTbar_14TeV_TuneCUETP8M1_cfi \
   --nhidden 256 --distance-dim 256 --num-conv 4 --weights inverse --lr-decay 0 --convlayer ghconv --model PFNet2
   
