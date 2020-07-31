9c012ae0e87b0d9be5d6c7ea245aa7f219c6e1ab
Kyriacos Xanthos <67141092+kyrxanthos@users.noreply.github.com>
2020-07-31 13:21:33 +0300

    added tfr4

--- test/eval_both.sh
+++ test/eval_both.sh
@@ -7,13 +7,13 @@ IMG=~jpata/gpuservers/singularity/images/pytorch.simg
 
 
 singularity exec -B /storage --nv $IMG \
-  python3 test/pred_tf_model.py --nhidden 256 --distance-dim 256 --convlayer ghconv --num-conv 4 --ntrain 400000 --ntest 100000 --weights $1 --gpu --datapath /storage/group/gpu/bigdata/particleflow/TTbar_14TeV_TuneCUETP8M1_cfi/tfr3 --model PFNet --target cand \
+  python3 test/pred_tf_model.py --nhidden 256 --distance-dim 256 --convlayer ghconv --num-conv 4 --ntrain 400000 --ntest 100000 --weights $1 --gpu --datapath /storage/group/gpu/bigdata/particleflow/TTbar_14TeV_TuneCUETP8M1_cfi/tfr4 --model PFNet --target cand \
 && \
 singularity exec -B /storage $IMG \
  python3 test/plots.py --pkl df_1.pkl.bz2 --target cand \
 && \
 singularity exec -B /storage --nv $IMG \
-  python3 test/pred_tf_model.py --nhidden 256 --distance-dim 256 --convlayer ghconv --num-conv 4 --ntrain 400000 --ntest 100000 --weights $2 --gpu --datapath /storage/group/gpu/bigdata/particleflow/TTbar_14TeV_TuneCUETP8M1_cfi/tfr3 --model PFNet2 --target cand 
+  python3 test/pred_tf_model.py --nhidden 256 --distance-dim 256 --convlayer ghconv --num-conv 4 --ntrain 400000 --ntest 100000 --weights $2 --gpu --datapath /storage/group/gpu/bigdata/particleflow/TTbar_14TeV_TuneCUETP8M1_cfi/tfr4 --model PFNet2 --target cand 
 
 singularity exec -B /storage $IMG \
  python3 test/plots.py --pkl df_1.pkl.bz2 --target cand
