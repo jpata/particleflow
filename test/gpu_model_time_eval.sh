a8050de6954a1ea4cb9d65d2854994b41c4a5755
Aadi <aadi@manivald.hep.kbfi.ee>
2020-08-18 17:10:22 +0300

    gpu_model_time_eval.sh
    Former-commit-id: 993074fe96fe313ba640bdc238aebe342e4a3d78

---
+++ test/gpu_model_time_eval.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH --gpus 1
+
+
+#Hello World
+#singularity exec -B /scratch -B /home --nv /home/software/singularity/base.simg:2020-08-13 \
+ # python3 hello_world.py
+
+
+singularity exec -B /home --nv /home/software/singularity/base.simg:latest python3 model_time_eval.py
+
+
+
+
+~                           
