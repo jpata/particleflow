ef97f48864e2f733ee25e26325a33ed21d3d2b10
Joosep Pata <joosep.pata@gmail.com>
2020-10-18 00:48:33 +0300

    knn instead of attention

--- mlpf/tensorflow/opt.py
+++ mlpf/tensorflow/opt.py
@@ -6,34 +6,39 @@ from argparse import Namespace
 import kerastuner as kt
 
 args = Namespace()
-args.datapath = "/scratch/joosep/TTbar_14TeV_TuneCUETP8M1_cfi"
+args.datapath = "./data/TTbar_14TeV_TuneCUETP8M1_cfi"
 args.ntrain = 1000
-args.ntest = 500
+args.ntest = 200
 args.weights = "inverse"
 args.convlayer = "ghconv"
 args.batch_size = 5
-args.nepochs = 100
+args.nepochs = 10
 args.target = "cand"
-args.lr = 0.001
+args.lr = 0.005
 args.outdir = "testout"
+args.num_conv_reg = 1
+args.hidden_dim_reg = 256
 
 def model_builder(hp):
-    args.hidden_dim = hp.Choice('hidden_dim', values = [16, 32, 64, 128, 256])
+    args.hidden_dim_id = hp.Choice('hidden_dim_id', values = [16, 32, 64, 128, 256])
+    args.num_convs_id = hp.Choice('num_convs_id', values = [1,2,3])
     args.distance_dim = hp.Choice('distance_dim', values = [16, 32, 64, 128, 256])
-    args.attention_layer_cutoff = hp.Float('attention_layer_cutoff', 0.01, 1.0, sampling="log")
-    args.dropout = hp.Choice('dropout', values = [0.1, 0.2, 0.3, 0.4, 0.5])
+    args.num_neighbors = hp.Choice('num_neighbors', [2,3,4,5,6])
+    args.dropout = hp.Choice('dropout', values = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5])
     args.nbins = hp.Choice('nbins', values = [10, 20, 50])
 
     model = PFNet(
-        hidden_dim=args.hidden_dim,
+        hidden_dim_id=args.hidden_dim_id,
+        hidden_dim_reg=args.hidden_dim_reg,
+        num_convs_id=args.num_convs_id,
+        num_convs_reg=args.num_convs_reg,
         distance_dim=args.distance_dim,
         convlayer=args.convlayer,
         dropout=args.dropout,
         batch_size=args.batch_size,
         nbins=args.nbins,
-        attention_layer_cutoff=args.attention_layer_cutoff,
+        num_neighbors=args.num_neighbors
     )
-
     loss_fn = my_loss_cls
     model.gnn_reg.trainable = False
     model.layer_momentum.trainable = False
@@ -45,45 +50,13 @@ def model_builder(hp):
         sample_weight_mode="temporal")
     return model
 
-    # callbacks = []
-    # tb = tf.keras.callbacks.TensorBoard(
-    #     log_dir=args.outdir, histogram_freq=0, write_graph=False, write_images=False, profile_batch=0,
-    #     update_freq='epoch',
-    # )
-    # tb.set_model(model)
-    # callbacks += [tb]
-
-    # callbacks += [hp.KerasCallback(logdir, hparams)]
-    # ret = model.fit(ds_train_r,
-    #     validation_data=ds_test_r, epochs=args.nepochs,
-    #     steps_per_epoch=args.ntrain/global_batch_size, validation_steps=args.ntest/global_batch_size,
-    #     verbose=True,
-    #     callbacks=callbacks
-    # )
-
-#count = 0
-#class MyTuner(kt.Hyperband):
-#    def on_trial_end(self, trial):
-#        super().on_trial_end(trial)
-#        global count
-#        vals = trial.hyperparameters.get_config()['values']
-#        with tf.summary.create_file_writer('logs/hparam_tuning').as_default():
-#            score = trial.score
-#            tf.summary.scalar("val_loss", trial.score, step=count)
-#            tf.summary.scalar("hidden_dim", vals["hidden_dim"], step=count)
-#            tf.summary.scalar("distance_dim", vals["distance_dim"], step=count)
-#            tf.summary.scalar("attention_layer_cutoff", vals["attention_layer_cutoff"], step=count)
-#            tf.summary.scalar("dropout", vals["dropout"], step=count)
-#            tf.summary.scalar("nbins", vals["nbins"], step=count)
-#        count += 1
-
 if __name__ == "__main__":
     global_batch_size = args.batch_size
     dataset = load_dataset_ttbar(args.datapath, args.target)
 
     ps = (tf.TensorShape([num_max_elems, 15]), tf.TensorShape([num_max_elems, 5]), tf.TensorShape([num_max_elems, ]))
-    ds_train = dataset.take(args.ntrain).map(weight_schemes[args.weights]).padded_batch(global_batch_size, padded_shapes=ps)
-    ds_test = dataset.skip(args.ntrain).take(args.ntest).map(weight_schemes[args.weights]).padded_batch(global_batch_size, padded_shapes=ps)
+    ds_train = dataset.take(args.ntrain).map(weight_schemes[args.weights]).padded_batch(global_batch_size, padded_shapes=ps).cache().prefetch(tf.data.experimental.AUTOTUNE)
+    ds_test = dataset.skip(args.ntrain).take(args.ntest).map(weight_schemes[args.weights]).padded_batch(global_batch_size, padded_shapes=ps).cache().prefetch(tf.data.experimental.AUTOTUNE)
     
     ds_train_r = ds_train.repeat()
     ds_test_r = ds_test.repeat()
