3a63adbee4ad43be95a8a9c6fbad7a35e6917b5e
Joosep Pata <joosep.pata@cern.ch>
2020-10-20 11:18:55 +0300

    merge

--- mlpf/tallinn/train.sh
+++ mlpf/tallinn/train.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 #SBATCH -p gpu
-#SBATCH --gpus 4
+#SBATCH --gpus 1
 
 IMG=/home/software/singularity/base.simg:latest
 cd ~/particleflow
 
 #TF training
-singularity exec --nv $IMG python3 test/tf_model.py --datapath data/TTbar_14TeV_TuneCUETP8M1_cfi --target cand --ntrain 16000 --ntest 4000 --convlayer ghconv --lr 1e-4 --nepochs 1000
+singularity exec -B /scratch --nv $IMG python3 mlpf/tensorflow/tf_model.py \
+  --datapath /scratch/joosep/TTbar_14TeV_TuneCUETP8M1_cfi \
+  --target cand --ntrain 70000 --ntest 20000 --convlayer ghconv \
+  --lr 1e-3 --nepochs 100 --num-neighbors 20 --num-hidden-id 2 --num-hidden-reg 2 \
+  --nbins 10 --hidden-dim-id 512 --hidden-dim-reg 512 --batch-size 5 --distance-dim 512 \
+  --dropout 0.3 --train-cls --num-convs-id 2 --num-convs-reg 2
 
 #Pytorch  training
 #singularity exec -B /home --nv $IMG \
