2a576f39fa0cdca794226039e836315bd8f8f86e
Joosep Pata <joosep.pata@gmail.com>
2020-10-20 11:18:08 +0300

    sparse tensor adding fixed
    Former-commit-id: 1c3b223316dc03421cc676536088e807d417fdc7

--- mlpf/tensorflow/opt.py
+++ mlpf/tensorflow/opt.py
@@ -8,27 +8,37 @@ import kerastuner as kt
 args = Namespace()
 args.datapath = "./data/TTbar_14TeV_TuneCUETP8M1_cfi"
 args.ntrain = 1000
-args.ntest = 200
+args.ntest = 500
 args.weights = "inverse"
 args.convlayer = "ghconv"
 args.batch_size = 5
 args.nepochs = 10
 args.target = "cand"
-args.lr = 0.005
+args.lr = 0.0001
 args.outdir = "testout"
-args.num_conv_reg = 1
-args.hidden_dim_reg = 256
+args.num_convs_reg = 1
+args.hidden_dim_reg = 128
+args.num_hidden_reg_enc = 0
+args.num_hidden_reg_dec = 0
 
 def model_builder(hp):
     args.hidden_dim_id = hp.Choice('hidden_dim_id', values = [16, 32, 64, 128, 256])
-    args.num_convs_id = hp.Choice('num_convs_id', values = [1,2,3])
+    args.num_hidden_id_enc = hp.Choice('hidden_dim_id_enc', values = [0, 1, 2, 3])
+    args.num_hidden_id_dec = hp.Choice('hidden_dim_id_dec', values = [0, 1, 2, 3])
+    args.num_convs_id = hp.Choice('num_convs_id', values = [1, 2, 3, 4])
     args.distance_dim = hp.Choice('distance_dim', values = [16, 32, 64, 128, 256])
-    args.num_neighbors = hp.Choice('num_neighbors', [2,3,4,5,6])
+    args.num_neighbors = hp.Choice('num_neighbors', [2, 3, 4, 5, 6, 7, 8, 9, 10, 20])
     args.dropout = hp.Choice('dropout', values = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5])
     args.nbins = hp.Choice('nbins', values = [10, 20, 50])
+    args.dist_mult = hp.Choice('dist_mult', values = [0.1, 1.0, 10.0])
+    args.cosine_dist = hp.Choice('cosine_dist', values = [True, False])
 
     model = PFNet(
+        num_hidden_id_enc=args.num_hidden_id_enc,
+        num_hidden_id_dec=args.num_hidden_id_dec,
         hidden_dim_id=args.hidden_dim_id,
+        num_hidden_reg_enc=args.num_hidden_reg_enc,
+        num_hidden_reg_dec=args.num_hidden_reg_dec,
         hidden_dim_reg=args.hidden_dim_reg,
         num_convs_id=args.num_convs_id,
         num_convs_reg=args.num_convs_reg,
@@ -37,7 +47,9 @@ def model_builder(hp):
         dropout=args.dropout,
         batch_size=args.batch_size,
         nbins=args.nbins,
-        num_neighbors=args.num_neighbors
+        num_neighbors=args.num_neighbors,
+        dist_mult=args.dist_mult,
+        cosine_dist=cosine_dist
     )
     loss_fn = my_loss_cls
     model.gnn_reg.trainable = False
@@ -55,8 +67,8 @@ if __name__ == "__main__":
     dataset = load_dataset_ttbar(args.datapath, args.target)
 
     ps = (tf.TensorShape([num_max_elems, 15]), tf.TensorShape([num_max_elems, 5]), tf.TensorShape([num_max_elems, ]))
-    ds_train = dataset.take(args.ntrain).map(weight_schemes[args.weights]).padded_batch(global_batch_size, padded_shapes=ps).cache().prefetch(tf.data.experimental.AUTOTUNE)
-    ds_test = dataset.skip(args.ntrain).take(args.ntest).map(weight_schemes[args.weights]).padded_batch(global_batch_size, padded_shapes=ps).cache().prefetch(tf.data.experimental.AUTOTUNE)
+    ds_train = dataset.take(args.ntrain).map(weight_schemes[args.weights]).padded_batch(global_batch_size, padded_shapes=ps)
+    ds_test = dataset.skip(args.ntrain).take(args.ntest).map(weight_schemes[args.weights]).padded_batch(global_batch_size, padded_shapes=ps)
     
     ds_train_r = ds_train.repeat()
     ds_test_r = ds_test.repeat()
@@ -67,12 +79,16 @@ if __name__ == "__main__":
         max_epochs = args.nepochs,
         factor = 3,
         hyperband_iterations = 3,
-        directory = 'my_dir',
-        project_name = 'intro_to_kt')
+        directory = 'kerastuner_out',
+        project_name = 'mlpf')
     
-    tuner.search(
-        ds_train_r,
-        validation_data=ds_test_r,
-        steps_per_epoch=args.ntrain/args.batch_size,
-        validation_steps=args.ntest/args.batch_size,
-    )
+    # tuner.search(
+    #     ds_train_r,
+    #     validation_data=ds_test_r,
+    #     steps_per_epoch=args.ntrain/args.batch_size,
+    #     validation_steps=args.ntest/args.batch_size,
+    #     #callbacks=[tf.keras.callbacks.EarlyStopping(patience=2, monitor='val_loss')]
+    # )
+    tuner.results_summary()
+    for trial in tuner.oracle.get_best_trials(num_trials=10):
+        print(trial.hyperparameters.values, trial.score)
