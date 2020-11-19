b9bdbd1a8884dc13eae1575ce4d05256bd9456c9
Javier Duarte <jduarte@ucsd.edu>
2020-11-19 09:43:48 -0800

    generate ttbar events then run delphes, ntuplizer
    Former-commit-id: 3d597b55d8530ea906cf5389fa9192661c3f72df

--- delphes/run_ntuple.sh
+++ delphes/run_ntuple.sh
@@ -4,4 +4,21 @@ source /opt/hepsim.sh
 export LD_LIBRARY_PATH=/opt/hepsim/delphes:$LD_LIBRARY_PATH
 export ROOT_INCLUDE_PATH=/opt/hepsim/delphes:/opt/hepsim/delphes/external
 
-python ntuplizer.py 
+XDIR="out/pythia8_ttbar"
+mkdir -p $XDIR
+rm -f $XDIR/*.pkl
+
+NSYST=1
+echo "Run  $NSYST jobs and collect files in $XDIR/"
+
+n=0
+#------------------------------ start loop ----------------------------                                                           
+while  [ $n -lt $NSYST ]
+do
+  echo "------------ Do run = $n"
+  NUM=`printf "%03d" $n`
+  INROOT="tev14_pythia8_ttbar_$NUM.root"
+  OUTPKL="tev14_pythia8_ttbar_$NUM.pkl"
+  python ntuplizer.py $XDIR/$INROOT $XDIR/$OUTPKL
+  let "n = $n + 1"
+done
