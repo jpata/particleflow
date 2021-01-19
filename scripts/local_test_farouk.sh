849bbb3a1f51de0c92a6d05d0ed45275999ede3a
Farouk <farouk.mokhtar@gmail.com>
2021-01-19 19:57:02 +0200

    updates
    Former-commit-id: 693b53ad496c4e3cc0b458b565746b62e7e139e7

--- scripts/local_test_farouk.sh
+++ scripts/local_test_farouk.sh
@@ -1,34 +1,34 @@
-#!/bin/bash
-set -e
+# #!/bin/bash
+# set -e
+#
+# rm -Rf test_tmp
+# mkdir test_tmp
+# cd test_tmp
+#
+# mkdir -p data/TTbar_14TeV_TuneCUETP8M1_cfi
+# cd data/TTbar_14TeV_TuneCUETP8M1_cfi
+#
+# #download the root input file
+# wget --no-check-certificate https://login-1.hep.caltech.edu/~jpata/particleflow/2020-07/TTbar_14TeV_TuneCUETP8M1_cfi/pfntuple_1.root
+# cd ../..
+#
+# mkdir -p data/TTbar_14TeV_TuneCUETP8M1_cfi/raw
+# mkdir -p data/TTbar_14TeV_TuneCUETP8M1_cfi/processed
+#
+# #generate pickle data files from root
+# python3 ../mlpf/data/postprocessing2.py --input data/TTbar_14TeV_TuneCUETP8M1_cfi/pfntuple_1.root \
+#   --events-per-file 1 --outpath data/TTbar_14TeV_TuneCUETP8M1_cfi/raw --save-normalized-table
+#
+# #generate pytorch data files
+# python3 ../mlpf/pytorch/graph_data.py --dataset data/TTbar_14TeV_TuneCUETP8M1_cfi \
+#   --processed_dir data/TTbar_14TeV_TuneCUETP8M1_cfi/processed --num-files-merge 1 --num-proc 1
 
-rm -Rf test_tmp
-mkdir test_tmp
-cd test_tmp
-
-mkdir -p data/TTbar_14TeV_TuneCUETP8M1_cfi
-cd data/TTbar_14TeV_TuneCUETP8M1_cfi
-
-#download the root input file
-wget --no-check-certificate https://login-1.hep.caltech.edu/~jpata/particleflow/2020-07/TTbar_14TeV_TuneCUETP8M1_cfi/pfntuple_1.root
-cd ../..
-
-mkdir -p data/TTbar_14TeV_TuneCUETP8M1_cfi/raw
-mkdir -p data/TTbar_14TeV_TuneCUETP8M1_cfi/processed
-
-#generate pickle data files from root
-python3 ../mlpf/data/postprocessing2.py --input data/TTbar_14TeV_TuneCUETP8M1_cfi/pfntuple_1.root \
-  --events-per-file 1 --outpath data/TTbar_14TeV_TuneCUETP8M1_cfi/raw --save-normalized-table
-
-#generate pytorch data files
-python3 ../mlpf/pytorch/graph_data.py --dataset data/TTbar_14TeV_TuneCUETP8M1_cfi \
-  --processed_dir data/TTbar_14TeV_TuneCUETP8M1_cfi/processed --num-files-merge 1 --num-proc 1
-
-# #run the pytorch training
-# COMET_API_KEY="bla" python3 ../mlpf/pytorch/train_end2end.py \
-#   --dataset data/TTbar_14TeV_TuneCUETP8M1_cfi --space_dim 2 --n_train 3 \
-#   --n_val 2 --model PFNet7 --convlayer gravnet-radius --convlayer2 sgconv \
-#   --lr 0.0001 --hidden_dim 32 --n_epochs 2 --l1 1.0 --l2 0.001 --target cand \
-#   --batch_size 1 --dropout 0.2 --disable-comet
+#run the pytorch training
+COMET_API_KEY="MpRzrVcduw8RODK5gbZxTWBgN" python3 ../mlpf/pytorch/train_end2end_farouk.py \
+  --dataset data/TTbar_14TeV_TuneCUETP8M1_cfi --space_dim 2 --n_train 3 \
+  --n_val 2 --model PFNet7 --convlayer gravnet-radius --convlayer2 sgconv \
+  --lr 0.0001 --hidden_dim 32 --n_epochs 2 --l1 1.0 --l2 0.001 --target cand \
+  --batch_size 1 --dropout 0.2 --disable-comet
 #
 # #generate dataframe with predictions from the pytorch model
 # python3 ../mlpf/pytorch/eval_end2end.py --dataset data/TTbar_14TeV_TuneCUETP8M1_cfi \
