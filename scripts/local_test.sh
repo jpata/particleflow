e08fa4efc18bb7afaebc3395fbcb1d119e341e42
Joosep Pata <joosep.pata@gmail.com>
2021-01-20 14:45:41 +0200

    up

--- scripts/local_test.sh
+++ scripts/local_test.sh
@@ -4,12 +4,17 @@ set -e
 mkdir -p delphes/out/pythia8_ttbar
 cd delphes/out/pythia8_ttbar
 
-#download a test input file (you can also download everything from Zenodo)
-wget --no-check-certificate https://jpata.web.cern.ch/jpata/particleflow/pythia8_ttbar/tev14_pythia8_ttbar_0_0.pkl.bz2
+#download a test input file (you can also download everything from Zenodo at 10.5281/zenodo.4452283)
+wget --no-check-certificate -nc https://zenodo.org/record/4452283/files/tev14_pythia8_ttbar_0_0.pkl.bz2
+wget --no-check-certificate -nc https://zenodo.org/record/4452283/files/tev14_pythia8_ttbar_8_0.pkl.bz2
 
 cd ../..
 
+mkdir -p experiments
+rm -Rf experiments/test-*
+
 python3 ../mlpf/tensorflow/delphes_data.py --datapath out/pythia8_ttbar
 python3 ../mlpf/tensorflow/delphes_model.py --model-spec parameters/test.yaml --action train
-python3 ../mlpf/tensorflow/delphes_model.py --model-spec parameters/test.yaml --action validate --weights ./experiments/test-*/weights.100-*.hdf5
-python3 ../mlpf/tensorflow/delphes_model.py --model-spec parameters/test.yaml --action timing --weights ./experiments/test-*/weights.100-*.hdf5
+python3 ../mlpf/tensorflow/delphes_model.py --model-spec parameters/test.yaml --action validate --weights ./experiments/test-*/weights.10-*.hdf5
+python3 ../mlpf/tensorflow/delphes_model.py --model-spec parameters/test.yaml --action timing --weights ./experiments/test-*/weights.10-*.hdf5
+
