22a5eedd2ab7ccb5e79c2059fb662e79924f0d36
Joosep Pata <joosep.pata@gmail.com>
2021-01-26 16:28:14 +0200

    up
    Former-commit-id: 2231e5704692a75c4ca495350eda700b602a3003

--- scripts/local_test.sh
+++ scripts/local_test.sh
@@ -2,11 +2,13 @@
 set -e
 
 mkdir -p data/pythia8_ttbar
+mkdir -p data/pythia8_ttbar/val
 cd data/pythia8_ttbar
 
 #download a test input file (you can also download everything from Zenodo at 10.5281/zenodo.4452283)
 wget --no-check-certificate -nc https://zenodo.org/record/4452283/files/tev14_pythia8_ttbar_0_0.pkl.bz2
 wget --no-check-certificate -nc https://zenodo.org/record/4452283/files/tev14_pythia8_ttbar_0_1.pkl.bz2
+mv tev14_pythia8_ttbar_0_1.pkl.bz2 val/
 
 cd ../..
 
