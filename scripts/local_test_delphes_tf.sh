c5fd7b077477740879f6c3ebe0b33be1052f4645
Joosep Pata <joosep.pata@gmail.com>
2021-01-28 14:20:05 +0200

    up

--- scripts/local_test_delphes_tf.sh
+++ scripts/local_test_delphes_tf.sh
@@ -6,8 +6,8 @@ mkdir -p data/pythia8_ttbar/val
 cd data/pythia8_ttbar
 
 #download a test input file (you can also download everything from Zenodo at 10.5281/zenodo.4452283)
-wget --no-check-certificate -nc https://zenodo.org/record/4452283/files/tev14_pythia8_ttbar_0_0.pkl.bz2
-wget --no-check-certificate -nc https://zenodo.org/record/4452283/files/tev14_pythia8_ttbar_0_1.pkl.bz2
+wget -q --no-check-certificate -nc https://zenodo.org/record/4452283/files/tev14_pythia8_ttbar_0_0.pkl.bz2
+wget -q --no-check-certificate -nc https://zenodo.org/record/4452283/files/tev14_pythia8_ttbar_0_1.pkl.bz2
 mv tev14_pythia8_ttbar_0_1.pkl.bz2 val/
 
 cd ../..
@@ -17,14 +17,14 @@ rm -Rf experiments/test-*
 
 #Run a simple training on a few events
 rm -Rf data/pythia8_ttbar/tfr
-python3 mlpf/launcher.py --model-spec parameters/test.yaml --action data
+python3 mlpf/launcher.py --model-spec parameters/test-delphes.yaml --action data
 
 #Run a simple training on a few events
-python3 mlpf/launcher.py --model-spec parameters/test.yaml --action train
+python3 mlpf/launcher.py --model-spec parameters/test-delphes.yaml --action train
 
 #Generate the pred.npz file of predictions
-python3 mlpf/launcher.py --model-spec parameters/test.yaml --action eval --weights ./experiments/test-*/weights.01-*.hdf5
+python3 mlpf/launcher.py --model-spec parameters/test-delphes.yaml --action eval --weights ./experiments/test-*/weights.01-*.hdf5
 
 #Generate the timing file
-python3 mlpf/launcher.py --model-spec parameters/test.yaml --action time --weights ./experiments/test-*/weights.01-*.hdf5
+python3 mlpf/launcher.py --model-spec parameters/test-delphes.yaml --action time --weights ./experiments/test-*/weights.01-*.hdf5
 
