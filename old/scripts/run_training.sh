3efc0f793152db49351ab67f052114b917ed635a
Joosep Pata <joosep.pata@cern.ch>
2021-03-26 15:22:36 +0200

    cleanup

--- old/scripts/run_training.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-singularity exec --nv -B /storage ~jpata/gpuservers/singularity/images/pytorch.simg python3 \
-    test/train_end2end.py --model PFNet6 --n_train 200 \
-    --batch_size 2 --n_epoch 100 --lr 0.0001 --hidden_dim 512
