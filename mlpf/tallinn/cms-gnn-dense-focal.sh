a2512b0640d3c4b4eb922b7f3a008075152d3908
Joosep Pata <joosep.pata@cern.ch>
2021-06-17 17:58:26 +0300

    transfer learning

---
+++ mlpf/tallinn/cms-gnn-dense-focal.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH --gpus 5
+#SBATCH --mem-per-gpu=8G
+
+IMG=/home/software/singularity/base.simg:latest
+cd ~/particleflow
+
+#TF training
+singularity exec --nv $IMG python3 mlpf/launcher.py --model-spec parameters/cms-gnn-dense-focal.yaml --action train
