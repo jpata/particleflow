4d108adb61293b2e12372d2b9a12a1fde1dee7d4
Eric Wulff <eric.wulff@cern.ch>
2021-06-24 22:23:07 +0200

    feat: OneCycle learning rate scheduler
    This commit also includes
     - Custom tensorboard callback logging learning rate & momentum
     - A utils.py file collecting utilities used in more than one file
     - Clean-up of how output files are organized
     - Configuration files using the OneCycle scheduler
    
    
    Former-commit-id: b7f59ea966af78cb67eaecaeb8da0adb320d2f7d

---
+++ mlpf/tfmodel/onecycle_scheduler.py
@@ -0,0 +1,144 @@
+import numpy as np
+import logging
+
+import tensorflow as tf
+from tensorflow.python.framework import ops
+from tensorflow.keras.optimizers.schedules import LearningRateSchedule
+from tensorflow.keras.callbacks import Callback
+
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
+
+
+class CosineAnnealer:
+    def __init__(self, start, end, steps):
+        self.start = start
+        self.end = end
+        self.steps = steps
+        self.n = 0
+
+    def step(self):
+        cos = np.cos(np.pi * (self.n / self.steps)) + 1
+        self.n += 1
+        return self.end + (self.start - self.end) / 2.0 * cos
+
+
+class OneCycleScheduler(LearningRateSchedule):
+    """`Callback` that schedules the learning rate on a 1cycle policy as per Leslie Smith's paper
+    (https://arxiv.org/pdf/1803.09820.pdf).
+
+    The implementation adopts additional improvements as per the fastai library:
+    https://docs.fast.ai/callbacks.one_cycle.html, where only two phases are used and the adaptation is done using
+    cosine annealing. In the warm-up phase the LR increases from `lr_max / div_factor` to `lr_max` and momentum
+    decreases from `mom_max` to `mom_min`. In the second phase the LR decreases from `lr_max` to `lr_max / final_div`
+    and momemtum from `mom_max` to `mom_min`. By default the phases are not of equal length, with the warm-up phase
+    controlled by the parameter `warmup_ratio`.
+
+    NOTE: The momentum is not controlled through this class. This class is intended to be used together with the
+    `MomentumOneCycleScheduler` callback defined below.
+    """
+
+    def __init__(
+        self,
+        lr_max,
+        steps,
+        mom_min=0.85,
+        mom_max=0.95,
+        warmup_ratio=0.3,
+        div_factor=25.0,
+        final_div=100000.0,
+        name=None,
+    ):
+        super(OneCycleScheduler, self).__init__()
+        lr_min = lr_max / div_factor
+
+        if final_div is None:
+            final_lr = lr_max / (div_factor * 1e4)
+        else:
+            final_lr = lr_max / (final_div)
+
+        phase_1_steps = steps * warmup_ratio
+        phase_2_steps = steps - phase_1_steps
+
+        self.lr_max = lr_max
+        self.steps = steps
+        self.mom_min = mom_min
+        self.mom_max = mom_max
+        self.warmup_ratio = warmup_ratio
+        self.div_factor = div_factor
+        self.final_div = final_div
+        self.name = name
+
+        phases = [CosineAnnealer(lr_min, lr_max, phase_1_steps), CosineAnnealer(lr_max, final_lr, phase_2_steps)]
+
+        step = 0
+        phase = 0
+        full_lr_schedule = np.zeros(int(steps))
+        for ii in np.arange(np.floor(steps), dtype=int):
+            step += 1
+            if step >= phase_1_steps:
+                phase = 1
+            full_lr_schedule[ii] = phases[phase].step()
+
+        self.full_lr_schedule = tf.convert_to_tensor(full_lr_schedule)
+
+    def __call__(self, step):
+        with ops.name_scope(self.name or "OneCycleScheduler"):
+            return self.full_lr_schedule[tf.cast(step, "int32") - 1]
+
+    def get_config(self):
+        return {
+            "lr_max": self.lr_max,
+            "steps": self.steps,
+            "mom_min": self.mom_min,
+            "mom_max": self.mom_max,
+            "warmup_ratio": self.warmup_ratio,
+            "div_factor": self.div_factor,
+            "final_div": self.final_div,
+            "name": self.name,
+        }
+
+
+class MomentumOneCycleScheduler(Callback):
+    """`Callback` that schedules the momentun according to the 1cycle policy as per Leslie Smith's paper
+    (https://arxiv.org/pdf/1803.09820.pdf).
+    NOTE: This callback only schedules the momentum parameter, not the learning rate. It is intended to be used with the
+    KerasOneCycle learning rate scheduler above or similar.
+    """
+
+    def __init__(self, steps, mom_min=0.85, mom_max=0.95, warmup_ratio=0.3):
+        super(MomentumOneCycleScheduler, self).__init__()
+
+        phase_1_steps = steps * warmup_ratio
+        phase_2_steps = steps - phase_1_steps
+
+        self.phase_1_steps = phase_1_steps
+        self.phase_2_steps = phase_2_steps
+        self.phase = 0
+        self.step = 0
+
+        self.phases = [CosineAnnealer(mom_max, mom_min, phase_1_steps), CosineAnnealer(mom_min, mom_max, phase_2_steps)]
+
+    def on_train_begin(self, logs=None):
+        self.set_momentum(self.mom_schedule().step())
+
+    def on_train_batch_end(self, batch, logs=None):
+        self.step += 1
+        if self.step >= self.phase_1_steps:
+            self.phase = 1
+
+        self.set_momentum(self.mom_schedule().step())
+
+    def set_momentum(self, mom):
+        # In Adam, the momentum parameter is called beta_1
+        if isinstance(self.model.optimizer, tf.keras.optimizers.Adam):
+            tf.keras.backend.set_value(self.model.optimizer.beta_1, mom)
+        # In SDG, the momentum parameter is called momentum
+        elif isinstance(self.model.optimizer, tf.keras.optimizers.SGD):
+            tf.keras.backend.set_value(self.model.optimizer.momentum, mom)
+        else:
+            raise NotImplementedError(
+                "Only SGD and Adam are supported by MomentumOneCycleScheduler: {}".format(type(self.model.optimizer))
+            )
+
+    def mom_schedule(self):
+        return self.phases[self.phase]
---
+++ mlpf/tfmodel/utils.py
@@ -0,0 +1,101 @@
+import os
+import yaml
+from pathlib import Path
+import datetime
+import platform
+
+import tensorflow as tf
+
+from tfmodel.onecycle_scheduler import OneCycleScheduler, MomentumOneCycleScheduler
+
+
+def load_config(config_file_path):
+    with open(config_file_path, "r") as ymlfile:
+        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+    return cfg
+
+
+def create_experiment_dir(prefix=None, suffix=None):
+    if prefix is None:
+        train_dir = Path("experiments") / datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    else:
+        train_dir = Path("experiments") / (prefix + datetime.datetime.now().strftime("%Y%m%d_%H%M%S"))
+
+    if suffix is not None:
+        train_dir = train_dir.with_name(train_dir.name + "." + platform.node())
+
+    train_dir.mkdir(parents=True)
+    return str(train_dir)
+
+
+def get_strategy():
+    global_batch_size = None
+    try:
+        gpus = [int(x) for x in os.environ.get("CUDA_VISIBLE_DEVICES", "0").split(",")]
+        num_gpus = len(gpus)
+        print("num_gpus=", num_gpus)
+        if num_gpus > 1:
+            strategy = tf.distribute.MirroredStrategy()
+            global_batch_size = num_gpus * global_batch_size
+        else:
+            strategy = tf.distribute.OneDeviceStrategy("gpu:0")
+    except Exception as e:
+        print("fallback to CPU", e)
+        strategy = tf.distribute.OneDeviceStrategy("cpu")
+        num_gpus = 0
+    return strategy, global_batch_size
+
+
+def get_lr_schedule(config, lr, steps):
+    callbacks = []
+    schedule = config["setup"]["lr_schedule"]
+    if schedule == "onecycle":
+        onecycle_cfg = config["onecycle"]
+        lr_schedule = OneCycleScheduler(
+            lr_max=lr,
+            steps=steps,
+            mom_min=onecycle_cfg["mom_min"],
+            mom_max=onecycle_cfg["mom_max"],
+            warmup_ratio=onecycle_cfg["warmup_ratio"],
+            div_factor=onecycle_cfg["div_factor"],
+            final_div=onecycle_cfg["final_div"],
+        )
+        callbacks.append(
+            MomentumOneCycleScheduler(
+                steps=steps,
+                mom_min=onecycle_cfg["mom_min"],
+                mom_max=onecycle_cfg["mom_max"],
+                warmup_ratio=onecycle_cfg["warmup_ratio"],
+            )
+        )
+    elif schedule == "exponentialdecay":
+        lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+            lr,
+            decay_steps=steps,
+            decay_rate=0.99,
+            staircase=True,
+        )
+    return lr_schedule, callbacks
+
+
+def compute_weights_invsqrt(X, y, w):
+    wn = tf.cast(tf.shape(w)[-1], tf.float32) / tf.sqrt(w)
+    wn *= tf.cast(X[:, 0] != 0, tf.float32)
+    # wn /= tf.reduce_sum(wn)
+    return X, y, wn
+
+
+def compute_weights_none(X, y, w):
+    wn = tf.ones_like(w)
+    wn *= tf.cast(X[:, 0] != 0, tf.float32)
+    return X, y, wn
+
+
+def get_weights_func(config):
+    sampling = config["setup"]["sample_weights"]
+    if sampling == "inverse_sqrt":
+        return compute_weights_invsqrt
+    elif sampling == "none":
+        return compute_weights_none
+    else:
+        raise ValueError("Only supported weight samplings are 'inverse_sqrt' and 'none'.")
