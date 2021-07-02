4b3171742ad2c3a03bb06bfe73a074b56e35e4f8
Eric Wulff <eric.wulff@cern.ch>
2021-07-02 16:29:59 +0200

    feat: Add tests of pipeline for cms and delphes

---
+++ scripts/local_test_delphes_pipeline.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+mkdir -p data/pythia8_ttbar
+mkdir -p data/pythia8_ttbar/val
+cd data/pythia8_ttbar
+
+#download a test input file (you can also download everything from Zenodo at 10.5281/zenodo.4559324)
+wget -q --no-check-certificate -nc https://zenodo.org/record/4559324/files/tev14_pythia8_ttbar_0_0.pkl.bz2
+wget -q --no-check-certificate -nc https://zenodo.org/record/4559324/files/tev14_pythia8_ttbar_0_1.pkl.bz2
+mv tev14_pythia8_ttbar_0_1.pkl.bz2 val/
+
+cd ../..
+
+mkdir -p experiments
+rm -Rf experiments/test-*
+
+#Run a simple training on a few events
+rm -Rf data/pythia8_ttbar/tfr
+python3 mlpf/launcher.py --model-spec parameters/test-delphes.yaml --action data
+
+#Run a simple training on a few events
+python3 mlpf/pipeline.py train -c parameters/test-delphes.yaml -p test-delphes-
+
+#Generate the pred.npz file of predictions
+python3 mlpf/pipeline.py evaluate -c parameters/test-delphes.yaml -t ./experiments/test-delphes-*
+
+#Generate the timing file
+python3 mlpf/launcher.py --model-spec parameters/test-delphes.yaml --action time --weights ./experiments/test-delphes-*/weights/weights-01-*.hdf5
+
