d80238bcb06c6b3635e470191d0e5d16f7e90462
Farouk <farouk.mokhtar@gmail.com>
2021-08-23 11:10:45 -0700

    fixing optimized model definition
    Former-commit-id: 38e6f8d25fc0641e3da342ce0f2422d6c2b056ad

--- mlpf/pytorch_delphes/model_optimized.py
+++ mlpf/pytorch_delphes/model_optimized.py
@@ -26,21 +26,28 @@ class PFNet7(nn.Module):
         output_dim_id=6,
         output_dim_p4=6,
         space_dim=4, propagate_dimensions=22, nearest=16,
-        target="gen"):
+        target="gen", nn1=True, nn3=True):
 
         super(PFNet7, self).__init__()
 
-        self.elu = nn.ELU
+        self.target = target
+        self.nn1 = nn1
+        self.nn3 = nn3
+
+        self.act = nn.LeakyReLU
         self.act_f = torch.nn.functional.leaky_relu
+        self.act_tanh = torch.nn.Tanh
+        self.elu = nn.ELU
 
         # (1) DNN: encoding/decoding of all tracks and clusters
-        self.nn1 = nn.Sequential(
-            nn.Linear(input_dim, hidden_dim_nn1),
-            self.elu(),
-            nn.Linear(hidden_dim_nn1, hidden_dim_nn1),
-            self.elu(),
-            nn.Linear(hidden_dim_nn1, input_encoding),
-        )
+        if self.nn1:
+            self.nn1 = nn.Sequential(
+                nn.Linear(input_dim, hidden_dim_nn1),
+                self.elu(),
+                nn.Linear(hidden_dim_nn1, hidden_dim_nn1),
+                self.elu(),
+                nn.Linear(hidden_dim_nn1, input_encoding),
+            )
 
         # (2) CNN: Gravnet layer
         self.conv1 = GravNetConv(input_encoding, encoding_dim, space_dim, propagate_dimensions, nearest)
@@ -57,21 +64,25 @@ class PFNet7(nn.Module):
         )
 
         # (4) DNN layer: regressing p4
-        self.nn3 = nn.Sequential(
-            nn.Linear(encoding_dim + output_dim_id + input_dim, hidden_dim),
-            self.elu(),
-            nn.Linear(hidden_dim, hidden_dim),
-            self.elu(),
-            nn.Linear(hidden_dim, hidden_dim),
-            self.elu(),
-            nn.Linear(hidden_dim, output_dim_p4),
-        )
+        if self.nn3:
+            self.nn3 = nn.Sequential(
+                nn.Linear(input_dim + output_dim_id + encoding_dim, hidden_dim),
+                self.elu(),
+                nn.Linear(hidden_dim, hidden_dim),
+                self.elu(),
+                nn.Linear(hidden_dim, hidden_dim),
+                self.elu(),
+                nn.Linear(hidden_dim, output_dim_p4),
+            )
 
     def forward(self, data):
         x0 = data.x
 
         # Encoder/Decoder step
-        x = self.nn1(x0)
+        if self.nn1:
+            x = self.nn1(x0)
+        else:
+            x=x0
 
         # Gravnet step
         x, edge_index, edge_weight = self.conv1(x)
@@ -81,7 +92,10 @@ class PFNet7(nn.Module):
         pred_ids = self.nn2(x)
 
         # DNN to predict p4
-        nn3_input = torch.cat([x, pred_ids, x0], axis=-1)
-        pred_p4 = self.nn3(nn3_input)
+        if self.nn3:
+            nn3_input = torch.cat([x0, pred_ids, x], axis=-1)
+            pred_p4 = self.nn3(nn3_input)
+        else:
+            pred_p4 = torch.zeros_like(data.ycand)
 
         return pred_ids, pred_p4, data.ygen_id, data.ygen, data.ycand_id, data.ycand
