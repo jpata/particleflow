d4508d07caeb1c91b0e041221b50ff6451c85a48
Eric Wulff <eric.wulff@cern.ch>
2021-08-24 15:38:12 +0200

    feat: log nvidia-smi info to csv file
    Former-commit-id: f13718f699a266d6cf4ad420a21867b412e860ab

--- mlpf/flatiron/start-head.sh
+++ mlpf/flatiron/start-head.sh
@@ -5,5 +5,7 @@ export LANG=C.UTF-8
 
 echo "starting ray head node"
 # Launch the head node
+mkdir -p "/mnt/ceph/users/ewulff/nvidia_smi_logs/$3_$2"
+nvidia-smi --query-gpu=timestamp,name,pci.bus_id,driver_version,pstate,pcie.link.gen.max,pcie.link.gen.current,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv -l 1 -f "/mnt/ceph/users/ewulff/nvidia_smi_logs/$3_$2/head.csv" &
 ray start --head --node-ip-address=$1 --port=6379
 sleep infinity
