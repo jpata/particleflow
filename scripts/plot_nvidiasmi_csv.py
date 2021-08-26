1fbf4e6bd842bbdb869b0f3d8358e302b94538e2
Eric Wulff <eric.wulff@cern.ch>
2021-08-26 12:47:18 +0200

    feat: Add logging of GPU power
    In addition, the nvidia-smi plot script is modified
    to also plot the power.
    
    
    Former-commit-id: 2d83dfa5506bad3da410a206cc164d3d5952c2d3

--- scripts/plot_nvidiasmi_csv.py
+++ scripts/plot_nvidiasmi_csv.py
@@ -13,21 +13,29 @@ def parse_args():
     return args
 
 
-def plot_gpu_util(df, cuda_device):
-    plt.figure(figsize=(12,9))
-    plt.plot(df["time"], df["GPU{}_util".format(cuda_device)], alpha=0.8)
-    plt.xlabel("Time [s]")
-    plt.ylabel("GPU utilization [%]")
-    plt.title("GPU{}".format(cuda_device))
-    plt.grid(alpha=0.3)
-
-
 def plot_gpu_util(df, cuda_device, ax):
     ax.plot(df["time"], df["GPU{}_util".format(cuda_device)], alpha=0.8)
     ax.set_xlabel("Time [s]")
     ax.set_ylabel("GPU utilization [%]")
     ax.set_title("GPU{}".format(cuda_device))
     ax.grid(alpha=0.3)
+
+def plot_gpu_power(df, cuda_device, ax):
+    ax.plot(df["time"], df["GPU{}_power".format(cuda_device)], alpha=0.8)
+    ax.set_xlabel("Time [s]")
+    ax.set_ylabel("Power consumption [W]")
+    ax.set_title("GPU{}".format(cuda_device))
+    ax.grid(alpha=0.3)
+
+def plot_dfs(dfs, plot_func, suffix):
+    fig, axs = plt.subplots(2, 2, figsize=(12,9), tight_layout=True)
+    for ax in axs.flat:
+        ax.label_outer()
+
+    for cuda_device, (df, ax) in enumerate(zip(dfs, axs.flat)):
+        plot_func(df, cuda_device, ax)
+    plt.suptitle("{}".format(file.stem))
+    plt.savefig(args.dir + "/{}_{}.jpg".format(file.stem, suffix))
 
 
 if __name__ == "__main__":
@@ -43,14 +51,9 @@ if __name__ == "__main__":
         for ii, gpu in enumerate(np.unique(df[" pci.bus_id"].values)):
             dfs.append(pd.DataFrame({
                 "GPU{}_util".format(ii): df[df[" pci.bus_id"] == gpu][" utilization.gpu [%]"].map(lambda x: int(x.split(" ")[1])),
+                "GPU{}_power".format(ii): df[df[" pci.bus_id"] == gpu][" power.draw [W]"].map(lambda x: float(x.split(" ")[1])),
                 "time": df[df[" pci.bus_id"] == gpu]["timestamp"].map(lambda x: datetime.strptime(x, "%Y/%m/%d %H:%M:%S.%f").timestamp() - start_t),
             }).dropna())
-    
-        fig, axs = plt.subplots(2, 2, figsize=(12,9), tight_layout=True)
-        for ax in axs.flat:
-            ax.label_outer()
 
-        for cuda_device, (df, ax) in enumerate(zip(dfs, axs.flat)):
-            plot_gpu_util(df, cuda_device, ax)
-        plt.suptitle("{}".format(file.stem))
-        plt.savefig(args.dir + "/{}_gpu_util.jpg".format(file.stem))
+        plot_dfs(dfs, plot_gpu_util, "gpu_util")
+        plot_dfs(dfs, plot_gpu_power, "gpu_power")
