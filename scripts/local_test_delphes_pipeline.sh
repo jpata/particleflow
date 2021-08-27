1a3bbf40003028febd08f5de69be1eaa2dbadb76
Joosep Pata <joosep.pata@gmail.com>
2021-08-27 10:45:45 +0300

    fix paths once again
    Former-commit-id: e9d5bc57b5c1f9397d47159177e591d3d0ea9541

--- scripts/local_test_delphes_pipeline.sh
+++ scripts/local_test_delphes_pipeline.sh
@@ -8,8 +8,8 @@ cd data/pythia8_ttbar
 #download a test input file (you can also download everything from Zenodo at 10.5281/zenodo.4559324)
 wget -q --no-check-certificate -nc https://zenodo.org/record/4559324/files/tev14_pythia8_ttbar_0_0.pkl.bz2
 mv tev14_pythia8_ttbar_0_0.pkl.bz2 raw/
-wget -q --no-check-certificate -nc https://zenodo.org/record/4559324/files/tev14_pythia8_ttbar_0_1.pkl.bz2
-mv tev14_pythia8_ttbar_0_1.pkl.bz2 val/
+wget -q --no-check-certificate -nc https://zenodo.org/record/4559324/files/tev14_pythia8_qcd_10_0.pkl.bz2
+mv tev14_pythia8_qcd_10_0.pkl.bz2 val/
 
 cd ../..
 
@@ -23,4 +23,4 @@ python3 mlpf/pipeline.py data -c parameters/delphes.yaml
 python3 mlpf/pipeline.py train -c parameters/delphes.yaml --nepochs 2 --ntrain 5 --ntest 5
 
 #Generate the pred.npz file of predictions
-python3 mlpf/pipeline.py evaluate -c parameters/delphes.yaml -t ./experiments/delphes-*
+python3 mlpf/pipeline.py evaluate -c parameters/delphes.yaml -t ./experiments/delphes_*
