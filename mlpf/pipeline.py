28efe9810de71f3d07595b2900e9372b772b9e6e
Joosep Pata <joosep.pata@cern.ch>
2021-08-29 19:51:35 +0300

    fix
    Former-commit-id: d03a64d1968e898057d17db53d69b9350e661307

--- mlpf/pipeline.py
+++ mlpf/pipeline.py
@@ -49,8 +49,7 @@ from tfmodel.utils import (
     get_loss_dict,
     parse_config,
     get_best_checkpoint,
-    delete_all_but_best_checkpoint,
-    classwise_energy_normalization
+    delete_all_but_best_checkpoint
 )
 
 from tfmodel.onecycle_scheduler import OneCycleScheduler, MomentumOneCycleScheduler
@@ -140,10 +139,6 @@ def train(config, weights, ntrain, ntest, nepochs, recreate, prefix, plot_freq, 
 
     dataset_def = get_dataset_def(config)
     ds_train_r, ds_test_r, dataset_transform = get_train_val_datasets(config, global_batch_size, n_train, n_test)
-
-    # for X, y, w in ds_train_r:
-    #     classwise_energy_normalization(X,y,w)
-    #     break
 
     #FIXME: split up training/test and validation dataset and parameters
     dataset_def.padded_num_elem_size = 6400
