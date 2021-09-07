b54fa71105ba403acb82984053c83dcffe54ecc5
Joosep Pata <joosep.pata@cern.ch>
2021-09-07 08:33:04 +0300

    up
    Former-commit-id: 0e9e92d218f372837eb7fff62f24c93b13440c87

--- mlpf/tallinn/cms.sh
+++ mlpf/tallinn/cms.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #SBATCH -p gpu
-#SBATCH --gpus 4
+#SBATCH --gpus 8
 #SBATCH --mem-per-gpu=8G
 
-IMG=/home/software/singularity/base.simg:latest
+#IMG=/home/software/singularity/base.simg:latest
+IMG=/home/joosep/singularity/tf26.simg
 cd ~/particleflow
 
 #TF training
