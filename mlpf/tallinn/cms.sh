741177bc75cc336958e79aa44377fc49cf3827b2
Joosep Pata <joosep.pata@cern.ch>
2021-09-07 20:03:12 +0300

    fix batching

--- mlpf/tallinn/cms.sh
+++ mlpf/tallinn/cms.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #SBATCH -p gpu
-#SBATCH --gpus 8
+#SBATCH --gpus 4
 #SBATCH --mem-per-gpu=8G
 
-#IMG=/home/software/singularity/base.simg:latest
-IMG=/home/joosep/singularity/tf26.simg
+IMG=/home/software/singularity/tf26.simg:latest
 cd ~/particleflow
 
 #TF training
-singularity exec --nv $IMG python3 mlpf/pipeline.py train -c parameters/cms.yaml --plot-freq 1
+PYTHONPATH=hep_tfds singularity exec --nv $IMG python3 mlpf/pipeline.py train -c parameters/cms.yaml --plot-freq 1
