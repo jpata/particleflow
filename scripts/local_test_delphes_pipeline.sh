cf675961df27bdf95dc4755f3e17e870612169a0
Joosep Pata <joosep.pata@gmail.com>
2021-09-08 18:36:40 +0300

    fixes for the delphes pipeline
    Former-commit-id: f73a8d0cc4c09bcac32a65742cbce9b1e362c805

--- scripts/local_test_delphes_pipeline.sh
+++ scripts/local_test_delphes_pipeline.sh
@@ -2,16 +2,16 @@
 set -e
 export PYTHONPATH=`pwd`/hep_tfds
 
-mkdir -p data/pythia8_ttbar/raw
-mkdir -p data/pythia8_qcd/val
+mkdir -p data/delphes_pf/pythia8_ttbar/raw
+mkdir -p data/delphes_pf/pythia8_qcd/val
 
 #download a test input file (you can also download everything from Zenodo at 10.5281/zenodo.4559324)
 wget -q --no-check-certificate -nc https://zenodo.org/record/4559324/files/tev14_pythia8_ttbar_0_0.pkl.bz2
-mv tev14_pythia8_ttbar_0_0.pkl.bz2 data/pythia8_ttbar/raw/
+mv tev14_pythia8_ttbar_0_0.pkl.bz2 data/delphes_pf/pythia8_ttbar/raw/
 wget -q --no-check-certificate -nc https://zenodo.org/record/4559324/files/tev14_pythia8_qcd_10_0.pkl.bz2
-mv tev14_pythia8_qcd_10_0.pkl.bz2 data/pythia8_qcd/val/
+mv tev14_pythia8_qcd_10_0.pkl.bz2 data/delphes_pf/pythia8_qcd/val/
 
-tfds build hep_tfds/heptfds/delphes_pf --manual_dir data/
+tfds build hep_tfds/heptfds/delphes_pf --download_dir data/
 
 #Run a simple training on a few events
 python3 mlpf/pipeline.py train -c parameters/delphes.yaml --nepochs 2 --ntrain 5 --ntest 5
