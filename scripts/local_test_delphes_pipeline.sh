3dc875659165ef504e318932c3f4e52ac6e76574
Joosep Pata <joosep.pata@gmail.com>
2021-09-08 16:16:25 +0300

    up

--- scripts/local_test_delphes_pipeline.sh
+++ scripts/local_test_delphes_pipeline.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+export PYTHONPATH=`pwd`/hep_tfds
 
 mkdir -p data/pythia8_ttbar/raw
 mkdir -p data/pythia8_qcd/val
@@ -10,25 +11,7 @@ mv tev14_pythia8_ttbar_0_0.pkl.bz2 data/pythia8_ttbar/raw/
 wget -q --no-check-certificate -nc https://zenodo.org/record/4559324/files/tev14_pythia8_qcd_10_0.pkl.bz2
 mv tev14_pythia8_qcd_10_0.pkl.bz2 data/pythia8_qcd/val/
 
-mkdir -p experiments
-
-echo "Cloning hep_tfds."
-git clone https://github.com/erwulff/hep_tfds.git
-echo "Installing hep_tfds."
-cd hep_tfds
-sudo python3 setup.py install
-cd ..
-
-#Run a simple training on a few events
-rm -Rf data/pythia8_ttbar/tfr
-python3 mlpf/pipeline.py data -c parameters/delphes.yaml
-
-cd hep_tfds
-echo "Building TFRecords files."
-tfds build heptfds/delphes_pf --manual_dir data/
-cd ../
-sudo rm -rf hep_tfds
-echo "Removed hep_tfds repo."
+tfds build hep_tfds/heptfds/delphes_pf --manual_dir data/
 
 #Run a simple training on a few events
 python3 mlpf/pipeline.py train -c parameters/delphes.yaml --nepochs 2 --ntrain 5 --ntest 5
