aa1dc8d3100fc0cbdc77cecdfd95501b469e7067
Joosep Pata <joosep.pata@cern.ch>
2021-09-15 16:33:08 +0300

    up

--- scripts/generate_tfds.sh
+++ scripts/generate_tfds.sh
@@ -5,6 +5,6 @@ export PYTHONPATH=hep_tfds
 IMG=/home/software/singularity/tf26.simg:latest
 
 singularity exec -B /hdfs $IMG tfds build hep_tfds/heptfds/cms_pf/singleele --manual_dir $MANUAL_DIR --overwrite
-#singularity exec -B /hdfs $IMG tfds build hep_tfds/heptfds/cms_pf/singlepi --manual_dir $MANUAL_DIR --overwrite
-#singularity exec -B /hdfs $IMG tfds build hep_tfds/heptfds/cms_pf/singletau --manual_dir $MANUAL_DIR --overwrite
+singularity exec -B /hdfs $IMG tfds build hep_tfds/heptfds/cms_pf/singlepi --manual_dir $MANUAL_DIR --overwrite
+singularity exec -B /hdfs $IMG tfds build hep_tfds/heptfds/cms_pf/singletau --manual_dir $MANUAL_DIR --overwrite
 singularity exec -B /hdfs $IMG tfds build hep_tfds/heptfds/cms_pf/ttbar --manual_dir $MANUAL_DIR --overwrite
