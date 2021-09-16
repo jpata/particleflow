66c13bc6781b00d322eee8e0b64e2ea4a6235003
Joosep Pata <joosep.pata@gmail.com>
2021-09-16 13:56:12 +0300

    add timing

--- mlpf/tfmodel/timing.py
+++ mlpf/tfmodel/timing.py
@@ -1,26 +1,36 @@
 import numpy as np
 import time
-import subprocess
-import shlex
+import pynvml
 
 #pip install only onnxruntime_gpu, not onnxruntime!
 import onnxruntime
 
 if __name__ == "__main__":
+    pynvml.nvmlInit()
+    handle = pynvml.nvmlDeviceGetHandleByIndex(0)
+
     EP_list = ['CUDAExecutionProvider']
 
-    nvidia_smi_call = "nvidia-smi --query-gpu=timestamp,name,pci.bus_id,pstate,power.draw,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv -l 1 -f nvidia_smi_log.csv"
-    p = subprocess.Popen(shlex.split(nvidia_smi_call))
-
     time.sleep(5)
-    onnx_sess = onnxruntime.InferenceSession("model.onnx", providers=EP_list)
 
-    for num_elems in [3200, 6400, 12800, 25600, 12800, 6400, 3200]:
+    mem = pynvml.nvmlDeviceGetMemoryInfo(handle)
+    mem_initial = mem.used/1000/1000
+    print("mem_initial", mem_initial)
+    
+    onnx_sess = onnxruntime.InferenceSession("model.onnx", providers=EP_list)
+    time.sleep(5)
+    
+    mem = pynvml.nvmlDeviceGetMemoryInfo(handle)
+    mem_onnx = mem.used/1000/1000
+    print("mem_onnx", mem_initial)
+
+    for num_elems in [1600, 3200, 6400, 12800, 25600]:
         times = []
-        for i in range(250):
+        mem_used = []
+        for i in range(500):
 
             #allocate array in system RAM
-            X = np.array(np.random.randn(1, num_elems, 15), np.float32)
+            X = np.array(np.random.randn(1, num_elems, 18), np.float32)
             
             #transfer data to GPU, run model, transfer data back
             t0 = time.time()
@@ -28,8 +38,8 @@ if __name__ == "__main__":
             t1 = time.time()
             dt = t1 - t0
             times.append(dt)
+            mem = pynvml.nvmlDeviceGetMemoryInfo(handle)
+            mem_used.append(mem.used/1000/1000)
 
-        print("Nelem={} mean_time={:.2f}ms stddev_time={:.2f} ms".format(num_elems, 1000.0*np.mean(times), 1000.0*np.std(times)))
+        print("Nelem={} mean_time={:.2f} ms stddev_time={:.2f} ms mem_used={:.0f} MB".format(num_elems, 1000.0*np.mean(times), 1000.0*np.std(times), np.max(mem_used)))
         time.sleep(5)
-
-    p.terminate()
