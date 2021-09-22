b0245299b6d81a8b21dbc683254dddbc66df6a3e
Joosep Pata <joosep.pata@gmail.com>
2021-09-22 10:14:04 +0300

    uncomment in gen scripts

--- mlpf/data/prepare_args.py
+++ mlpf/data/prepare_args.py
@@ -4,16 +4,16 @@ import sys, os, fnmatch
 
 outdir = "/hdfs/local/joosep/mlpf/gen"
 samples = [
-    #"SinglePiFlatPt0p7To10_cfi",
+    "SinglePiFlatPt0p7To10_cfi",
     "SingleTauFlatPt2To150_cfi",
     "SingleMuFlatPt0p7To10_cfi",
-    #"SingleElectronFlatPt1To100_pythia8_cfi",
+    "SingleElectronFlatPt1To100_pythia8_cfi",
     "SingleGammaFlatPt10To100_pythia8_cfi",
-    #"SinglePi0E10_pythia8_cfi",
+    "SinglePi0E10_pythia8_cfi",
 ]
 
 samples_pu = [
-    #"TTbar_14TeV_TuneCUETP8M1_cfi",
+    "TTbar_14TeV_TuneCUETP8M1_cfi",
 ]
 
 if __name__ == "__main__":
@@ -24,8 +24,7 @@ if __name__ == "__main__":
         os.makedirs(outdir + "/" + s + "/raw", exist_ok=True)
         os.makedirs(outdir + "/" + s + "/root", exist_ok=True)
 
-        #for iseed in range(1,51):
-        for iseed in range(51,2001):
+        for iseed in range(1,2001):
             if not os.path.isfile(outdir+"/"+s+"/raw/pfntuple_{}.pkl".format(iseed)):
                 if is_pu:
                     print("sbatch mlpf/tallinn/genjob_pu.sh {} {}".format(s, iseed))
