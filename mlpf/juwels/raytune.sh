0f7549f7c596d993e77ec2fd90a28fe5387f4c42
Eric Wulff <eric.g.t.wulff@gmail.com>
2021-09-30 11:02:59 +0200

    hep_tfds support in raytune (#82)
    * feat: Add more plots to raytune-analysis command
    
    * fix: add install of seaborn to github tests
    
    * feat: hep_tfds support for raytune
    
    * fix: Update parameter name in raytune config
    
    * fix: Timeout error running Ray Tune on HPC systems
    
    * up: update raytune parameters
    
    * fix: cleaner sbatch scripts for raytune
    
    * fix: bug in raytune sbatch script for flatiron
    
    * chore: add check for specification of raytune local_dir
    
    * feat: add plotting script for exported comet.ml json
    
    * chore: Add raytune default_metric to config file
    
    * feat: add more hyperparameters to tune in raytune
    
    * fix: remove extra tab
    
    * feat: time raytune command and log time
    
    * feat: log best parameters after raytune run
    
    * fix: raytune dropout parameter
    
    The raytune dropout parameter now sets both the
    combined_graph_layer dropout and the output decoding dropout.
    The combined_graph_layer dropout will be set to dropout/2.
    
    Former-commit-id: 7ddbd3411c64e858f2bfd96ea02e0673cb57d16e

--- mlpf/juwels/raytune.sh
+++ mlpf/juwels/raytune.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH -t 168:00:00
-#SBATCH -N 12
+#SBATCH --account=prcoe12
+#SBATCH --partition=booster
+#SBATCH --time 6:00:00
+#SBATCH --nodes 12
 #SBATCH --tasks-per-node=1
-#SBATCH -p gpu
-#SBATCH --constraint=a100,sxm4
+#SBATCH --gres=gpu:4
 #SBATCH --gpus-per-task=4
-#SBATCH --cpus-per-task=64
-
+#SBATCH --cpus-per-task=96
 # Job name
 #SBATCH -J raytune
 
@@ -20,19 +20,22 @@ echo "#################### Job submission script. #############################"
 cat $0
 echo "################# End of job submission script. #########################"
 
-export TUNE_RESULT_DIR="/mnt/ceph/users/ewulff/ray_results/tune_result_dir"
+export TUNE_RESULT_DIR="/p/project/prcoe12/wulff1/ray_results/tune_result_dir"
 export TUNE_MAX_PENDING_TRIALS_PG=${SLURM_NNODES}
 
 module purge
-module load slurm gcc cuda/11.1.0_455.23.05 cudnn/v8.0.4-cuda-11.1
+module load GCC/10.3.0 CUDA/11.0 cuDNN/8.0.2.39-CUDA-11.0
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+jutil env activate -p prcoe12
 nvidia-smi
 
-source ~/miniconda3/bin/activate tf2
+source /p/project/prcoe12/wulff1/miniconda3/bin/activate tf2
+echo "Python used:"
 which python3
 python3 --version
 
 
-################# DON NOT CHANGE THINGS HERE UNLESS YOU KNOW WHAT YOU ARE DOING ###############
+################# DO NOT CHANGE THINGS HERE UNLESS YOU KNOW WHAT YOU ARE DOING ###############
 # This script is a modification to the implementation suggest by gregSchwartz18 here:
 # https://github.com/ray-project/ray/issues/826#issuecomment-522116599
 redis_password=$(uuidgen)
@@ -42,15 +45,16 @@ echo "Redis password: ${redis_password}"
 nodes=$(scontrol show hostnames $SLURM_JOB_NODELIST) # Getting the node names
 nodes_array=( $nodes )
 
-node_1=${nodes_array[0]} 
-ip=$(srun --nodes=1 --ntasks=1 -w $node_1 hostname --ip-address) # making redis-address
+node_1=${nodes_array[0]}
+# ip=$(srun --nodes=1 --ntasks=1 -w $node_1 hostname --ip-address) # making redis-address
+ip=$(srun --nodes=1 --ntasks=1 -w $node_1 host ${node_1}i | awk '{ print $4 }') # making redis-address
 port=6379
 ip_head=$ip:$port
 export ip_head
 echo "IP Head: $ip_head"
 
 echo "STARTING HEAD at $node_1"
-srun --nodes=1 --ntasks=1 -w $node_1 mlpf/raytune/start-head.sh $ip $SLURM_JOB_ID $2 "/mnt/ceph/users/ewulff/nvidia_smi_logs/" &
+srun --nodes=1 --ntasks=1 -w $node_1 mlpf/raytune/start-head.sh $ip $SLURM_JOB_ID $2 "/p/project/prcoe12/wulff1/nvidia_smi_logs/" &
 sleep 30
 
 worker_num=$(($SLURM_JOB_NUM_NODES - 1)) #number of nodes other than the head node
@@ -58,11 +62,12 @@ for ((  i=1; i<=$worker_num; i++ ))
 do
   node_i=${nodes_array[$i]}
   echo "STARTING WORKER $i at $node_i"
-  srun --nodes=1 --ntasks=1 -w $node_i mlpf/raytune/start-worker.sh $ip_head $SLURM_JOB_ID $i $2 "/mnt/ceph/users/ewulff/nvidia_smi_logs/" &
+  srun --nodes=1 --ntasks=1 -w ${node_i} mlpf/raytune/start-worker.sh $ip_head $SLURM_JOB_ID $i $2 "/p/project/prcoe12/wulff1/nvidia_smi_logs/" &
   sleep 5
 done
 ##############################################################################################
 
 #### call your code below
-python3 mlpf/pipeline.py raytune -c $1 -n $2 --cpus "${SLURM_CPUS_PER_TASK}" --gpus "${SLURM_GPUS_PER_TASK}"
+
+python3 mlpf/pipeline.py raytune -c $1 -n $2 --cpus "${SLURM_CPUS_PER_TASK}" --gpus "${SLURM_GPUS_PER_TASK}" -r
 exit
