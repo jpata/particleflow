25989faaff58d0b6963e851a27cffa568201f36b
Eric Wulff <eric.wulff@cern.ch>
2021-10-03 16:01:34 +0200

    update raytune scripts for Flatiron cluster

--- mlpf/raytune/start-head.sh
+++ mlpf/raytune/start-head.sh
@@ -5,7 +5,7 @@ export LANG=C.UTF-8
 
 echo "starting ray head node"
 # Launch the head node
-mkdir -p "$4$3_$2"
-nvidia-smi --query-gpu=timestamp,name,pci.bus_id,pstate,power.draw,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv -l 1 -f "$4$3_$2/head.csv" &
-ray start --head --node-ip-address=$1 --port=6379
+# mkdir -p "$4$3_$2"
+# nvidia-smi --query-gpu=timestamp,name,pci.bus_id,pstate,power.draw,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv -l 1 -f "$4$3_$2/head.csv" &
+ray start --head --node-ip-address=$1 --dashboard-host 0.0.0.0 --port=6379 --block
 sleep infinity
