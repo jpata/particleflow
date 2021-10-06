0eafc6052e347e38054a92fc9ed39e2ca5b59dcc
Eric Wulff <eric.wulff@cern.ch>
2021-10-06 13:59:22 +0200

    fix: update pfntuple names in test

--- scripts/local_test_cms_pipeline.sh
+++ scripts/local_test_cms_pipeline.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+# set -e
 export PYTHONPATH=`pwd`/hep_tfds
 
 rm -Rf local_test_data/TTbar_14TeV_TuneCUETP8M1_cfi
@@ -8,9 +8,9 @@ mkdir -p local_test_data/TTbar_14TeV_TuneCUETP8M1_cfi/root
 cd local_test_data/TTbar_14TeV_TuneCUETP8M1_cfi/root
 
 #Only CMS-internal use is permitted by CMS rules! Do not use these MC simulation files otherwise!
-wget -q --no-check-certificate -nc https://jpata.web.cern.ch/jpata/mlpf/cms/TTbar_14TeV_TuneCUETP8M1_cfi/root/pfntuple_1.root
-wget -q --no-check-certificate -nc https://jpata.web.cern.ch/jpata/mlpf/cms/TTbar_14TeV_TuneCUETP8M1_cfi/root/pfntuple_2.root
-wget -q --no-check-certificate -nc https://jpata.web.cern.ch/jpata/mlpf/cms/TTbar_14TeV_TuneCUETP8M1_cfi/root/pfntuple_3.root
+wget -q --no-check-certificate -nc https://jpata.web.cern.ch/jpata/mlpf/cms/TTbar_14TeV_TuneCUETP8M1_cfi/root/pfntuple_12001.root
+wget -q --no-check-certificate -nc https://jpata.web.cern.ch/jpata/mlpf/cms/TTbar_14TeV_TuneCUETP8M1_cfi/root/pfntuple_12002.root
+wget -q --no-check-certificate -nc https://jpata.web.cern.ch/jpata/mlpf/cms/TTbar_14TeV_TuneCUETP8M1_cfi/root/pfntuple_12003.root
 
 cd ../../..
 
