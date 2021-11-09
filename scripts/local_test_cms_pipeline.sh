6d34c398f7039f90a2486803de7c487a877a7654
Joosep Pata <joosep.pata@gmail.com>
2021-11-09 14:38:43 +0200

    ACAT2021 benchmark (#92)
    * benchmark model for ACAT2021
    
    * make the network lighter
    
    * fixed estimator version
    
    * fix keras version
    
    * use the existing config
    
    * update CMS plots

--- scripts/local_test_cms_pipeline.sh
+++ scripts/local_test_cms_pipeline.sh
@@ -34,6 +34,6 @@ python3 mlpf/pipeline.py train -c parameters/cms.yaml --nepochs 2 --customize pi
 ls ./experiments/cms_*/weights/
 
 #Generate the pred.npz file of predictions
-python3 mlpf/pipeline.py evaluate -c parameters/cms.yaml -t ./experiments/cms_*
+python3 mlpf/pipeline.py evaluate -t ./experiments/cms_*
 
 python3 mlpf/pipeline.py train -c parameters/cms-transformer.yaml --nepochs 2 --customize pipeline_test
