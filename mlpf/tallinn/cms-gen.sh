4944ed1ba4b9f8b924b6228bed2a40a3310fbc86
Joosep Pata <joosep.pata@cern.ch>
2022-04-17 09:48:15 +0300

    up

--- mlpf/tallinn/cms-gen.sh
+++ mlpf/tallinn/cms-gen.sh
@@ -7,4 +7,4 @@ IMG=/home/software/singularity/tf-2.8.0.simg
 cd ~/particleflow
 
 #TF training
-singularity exec --nv --env PYTHONPATH=hep_tfds $IMG python mlpf/pipeline.py train -c parameters/cms-gen.yaml --plot-freq 10
+singularity exec -B /scratch-persistent --nv --env PYTHONPATH=hep_tfds --env TFDS_DATA_DIR=/scratch-persistent/joosep/tensorflow_datasets $IMG python mlpf/pipeline.py train -c parameters/cms-gen.yaml --plot-freq 10
