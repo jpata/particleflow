1a7471449002afb5c4037037a3ca420ae06ccc12
Farouk <farouk.mokhtar@gmail.com>
2022-05-16 10:46:39 -0700

    up

--- mlpf/pyg/preprocess_data.py
+++ mlpf/pyg/preprocess_data.py
@@ -12,9 +12,8 @@ from glob import glob
 import pickle
 import multiprocessing
 
-# assumes pkl files exist in /test_tmp_delphes/data/pythia8_ttbar/raw
-# they are processed and saved as pt files in /test_tmp_delphes/data/pythia8_ttbar/processed
-# PFGraphDataset -> returns for 1 event: Data(x=[5139, 12], ycand=[5139, 6], ycand_id=[5139, 6], ygen=[5139, 6], ygen_id=[5139, 6])
+# assumes pkl files exist in /data/sample/raw
+# they are processed and saved as pt files in /data/sample/processed
 
 
 def relabel_indices(pid_array):
