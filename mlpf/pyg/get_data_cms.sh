0d2b375852e274a469f08d9fd0479f49f28ba0e2
Farouk <farouk.mokhtar@gmail.com>
2022-05-17 11:10:30 -0700

    test

--- mlpf/pyg/get_data_cms.sh
+++ mlpf/pyg/get_data_cms.sh
@@ -8,21 +8,21 @@ set -e
 # # get the cms data
 # rsync -r --progress lxplus.cern.ch:/eos/user/j/jpata/mlpf/cms data/
 
-# unzip the data
-for sample in cms/* ; do
-  echo $sample
-    mv $sample/root $sample/processed
-    cd $sample/raw/
-    bzip2 -d *
-    cd ../../../
-done
+# # unzip the data
+# for sample in cms/* ; do
+#   echo $sample
+#     mv $sample/root $sample/processed
+#     cd $sample/raw/
+#     bzip2 -d *
+#     cd ../../../
+# done
 
 # process the cms data
 for sample in cms/* ; do
   echo $sample
-  #generate pytorch data files from pkl files
-  python3 preprocess_data.py --data cms --dataset $sample \
-    --processed_dir $sample/processed --num-files-merge 10 --num-proc 1
+  # #generate pytorch data files from pkl files
+  # python3 preprocess_data.py --data cms --dataset $sample \
+  #   --processed_dir $sample/processed --num-files-merge 10 --num-proc 1
 done
 
-mv cms ../../data/
+# mv cms ../../data/
