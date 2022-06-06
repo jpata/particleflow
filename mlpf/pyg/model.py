e1fbdde8c58aa70bd2e0dc734b6ae2cce040679e
Farouk <farouk.mokhtar@gmail.com>
2022-06-06 18:42:39 +0200

    up

--- mlpf/pyg/model.py
+++ mlpf/pyg/model.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import scipy.spatial
 import pickle as pkl
 import os.path as osp
@@ -162,9 +163,7 @@ class GravNetConv_MLPF(MessagePassing):
         if (torch.unique(b[0], return_counts=True)[1] < self.k).sum() != 0:
             raise RuntimeError(f'Not enough elements in a region to perform the k-nearest neighbors. Current k-value={self.k}')
 
-        # edge_index = knn(s_l, s_r, self.k, b[0], b[1]).flip([0])
-        import torch_cluster.knn_cuda
-        edge_index = torch_cluster.knn_cuda.knn(s_l, s_r, self.k, b[0], b[1])
+        edge_index = knn(s_l, s_r, self.k, b[0], b[1]).flip([0])
 
         edge_weight = (s_l[edge_index[0]] - s_r[edge_index[1]]).pow(2).sum(-1)
         edge_weight = torch.exp(-10. * edge_weight)  # 10 gives a better spread
@@ -192,44 +191,65 @@ class GravNetConv_MLPF(MessagePassing):
         return (f'{self.__class__.__name__}({self.in_channels}, '
                 f'{self.out_channels}, k={self.k})')
 
-#
-# def knn(x, y, k, batch_x=None, batch_y=None):
-#
-#     if batch_x is None:
-#         batch_x = x.new_zeros(x.size(0), dtype=torch.long)
-#
-#     if batch_y is None:
-#         batch_y = y.new_zeros(y.size(0), dtype=torch.long)
-#
-#     x = x.view(-1, 1) if x.dim() == 1 else x
-#     y = y.view(-1, 1) if y.dim() == 1 else y
-#
-#     assert x.dim() == 2 and batch_x.dim() == 1
-#     assert y.dim() == 2 and batch_y.dim() == 1
-#     assert x.size(1) == y.size(1)
-#     assert x.size(0) == batch_x.size(0)
-#     assert y.size(0) == batch_y.size(0)
-#
-#     if x.is_cuda:
-#
-#         # Rescale x and y.
-#     min_xy = min(x.min().item(), y.min().item())
-#     x, y = x - min_xy, y - min_xy
-#
-#     max_xy = max(x.max().item(), y.max().item())
-#     x, y, = x / max_xy, y / max_xy
-#
-#     # Concat batch/features to ensure no cross-links between examples exist.
-#     x = torch.cat([x, 2 * x.size(1) * batch_x.view(-1, 1).to(x.dtype)], dim=-1)
-#     y = torch.cat([y, 2 * y.size(1) * batch_y.view(-1, 1).to(y.dtype)], dim=-1)
-#
-#     tree = scipy.spatial.cKDTree(x.detach().numpy())
-#     dist, col = tree.query(
-#         y.detach().cpu(), k=k, distance_upper_bound=x.size(1))
-#     dist = torch.from_numpy(dist).to(x.dtype)
-#     col = torch.from_numpy(col).to(torch.long)
-#     row = torch.arange(col.size(0), dtype=torch.long).view(-1, 1).repeat(1, k)
-#     mask = 1 - torch.isinf(dist).view(-1)
-#     row, col = row.view(-1)[mask], col.view(-1)[mask]
-#
-#     return torch.stack([row, col], dim=0)
+
+@torch.jit.script
+def knn(x: torch.Tensor, y: torch.Tensor, k: int,
+        batch_x: Optional[torch.Tensor] = None,
+        batch_y: Optional[torch.Tensor] = None, cosine: bool = False,
+        num_workers: int = 1) -> torch.Tensor:
+    r"""Finds for each element in :obj:`y` the :obj:`k` nearest points in
+    :obj:`x`.
+    Args:
+        x (Tensor): Node feature matrix
+            :math:`\mathbf{X} \in \mathbb{R}^{N \times F}`.
+        y (Tensor): Node feature matrix
+            :math:`\mathbf{X} \in \mathbb{R}^{M \times F}`.
+        k (int): The number of neighbors.
+        batch_x (LongTensor, optional): Batch vector
+            :math:`\mathbf{b} \in {\{ 0, \ldots, B-1\}}^N`, which assigns each
+            node to a specific example. :obj:`batch_x` needs to be sorted.
+            (default: :obj:`None`)
+        batch_y (LongTensor, optional): Batch vector
+            :math:`\mathbf{b} \in {\{ 0, \ldots, B-1\}}^M`, which assigns each
+            node to a specific example. :obj:`batch_y` needs to be sorted.
+            (default: :obj:`None`)
+        cosine (boolean, optional): If :obj:`True`, will use the Cosine
+            distance instead of the Euclidean distance to find nearest
+            neighbors. (default: :obj:`False`)
+        num_workers (int): Number of workers to use for computation. Has no
+            effect in case :obj:`batch_x` or :obj:`batch_y` is not
+            :obj:`None`, or the input lies on the GPU. (default: :obj:`1`)
+    :rtype: :class:`LongTensor`
+    .. code-block:: python
+        import torch
+        from torch_cluster import knn
+        x = torch.Tensor([[-1, -1], [-1, 1], [1, -1], [1, 1]])
+        batch_x = torch.tensor([0, 0, 0, 0])
+        y = torch.Tensor([[-1, 0], [1, 0]])
+        batch_y = torch.tensor([0, 0])
+        assign_index = knn(x, y, 2, batch_x, batch_y)
+    """
+
+    x = x.view(-1, 1) if x.dim() == 1 else x
+    y = y.view(-1, 1) if y.dim() == 1 else y
+    x, y = x.contiguous(), y.contiguous()
+
+    batch_size = 1
+    if batch_x is not None:
+        assert x.size(0) == batch_x.numel()
+        batch_size = int(batch_x.max()) + 1
+    if batch_y is not None:
+        assert y.size(0) == batch_y.numel()
+        batch_size = max(batch_size, int(batch_y.max()) + 1)
+
+    ptr_x: Optional[torch.Tensor] = None
+    ptr_y: Optional[torch.Tensor] = None
+    if batch_size > 1:
+        assert batch_x is not None
+        assert batch_y is not None
+        arange = torch.arange(batch_size + 1, device=x.device)
+        ptr_x = torch.bucketize(arange, batch_x)
+        ptr_y = torch.bucketize(arange, batch_y)
+
+    return torch.ops.torch_cluster.knn(x, y, ptr_x, ptr_y, k, cosine,
+                                       num_workers)
