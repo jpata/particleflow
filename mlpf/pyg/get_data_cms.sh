3d1104a3b8153f1ca293328ec4a6c927e265a415
Farouk <farouk.mokhtar@gmail.com>
2022-06-07 13:29:13 +0200

    up

--- mlpf/pyg/get_data_cms.sh
+++ mlpf/pyg/get_data_cms.sh
@@ -2,27 +2,27 @@
 
 set -e
 
-# # make directory to hold datafiles in home directory of the repo
-# mkdir -p ../../data
-#
-# # get the cms data
-# rsync -r --progress lxplus.cern.ch:/eos/user/j/jpata/mlpf/cms data/
-#
-# # unzip the data
-# for sample in cms/* ; do
-#   echo $sample
-#     mv $sample/root $sample/processed
-#     cd $sample/raw/
-#     bzip2 -d *
-#     cd ../../../
-# done
+# make directory to hold datafiles in home directory of the repo
+mkdir -p ../../data
 
-# process the cms data
+# get the cms data
+rsync -r --progress lxplus.cern.ch:/eos/user/j/jpata/mlpf/cms data/
+
+# unzip the data
 for sample in cms/* ; do
   echo $sample
-  #generate pytorch data files from pkl files
-  python3 dataset.py --data cms --dataset $sample \
-    --processed_dir $sample/processed --num-files-merge 1 --num-proc 1
+    mv $sample/root $sample/processed
+    cd $sample/raw/
+    bzip2 -d *
+    cd ../../../
 done
+
+# # process the cms data
+# for sample in cms/* ; do
+#   echo $sample
+#   #generate pytorch data files from pkl files
+#   python3 dataset.py --data cms --dataset $sample \
+#     --processed_dir $sample/processed --num-files-merge 1 --num-proc 1
+# done
 
 # mv cms ../../data/
