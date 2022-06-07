37cd6a1fbc2d305fe284b5ad607da4c56f87a8c2
Farouk <farouk.mokhtar@gmail.com>
2022-06-07 14:53:50 +0200

    up

--- mlpf/pyg/model.py
+++ mlpf/pyg/model.py
@@ -18,10 +18,10 @@ import torch.nn.functional as F
 from typing import Optional, Union
 from torch_geometric.typing import OptTensor, PairTensor, PairOptTensor
 
-# try:
-#     from torch_cluster import knn
-# except ImportError:
-#     knn = None
+try:
+    from torch_cluster import knn
+except ImportError:
+    knn = None
 
 
 class MLPF(nn.Module):
@@ -187,66 +187,3 @@ class GravNetConv_MLPF(MessagePassing):
     def __repr__(self) -> str:
         return (f'{self.__class__.__name__}({self.in_channels}, '
                 f'{self.out_channels}, k={self.k})')
-
-
-@torch.jit.script
-def knn(x: torch.Tensor, y: torch.Tensor, k: int,
-        batch_x: Optional[torch.Tensor] = None,
-        batch_y: Optional[torch.Tensor] = None, cosine: bool = False,
-        num_workers: int = 1) -> torch.Tensor:
-    r"""Finds for each element in :obj:`y` the :obj:`k` nearest points in
-    :obj:`x`.
-    Args:
-        x (Tensor): Node feature matrix
-            :math:`\mathbf{X} \in \mathbb{R}^{N \times F}`.
-        y (Tensor): Node feature matrix
-            :math:`\mathbf{X} \in \mathbb{R}^{M \times F}`.
-        k (int): The number of neighbors.
-        batch_x (LongTensor, optional): Batch vector
-            :math:`\mathbf{b} \in {\{ 0, \ldots, B-1\}}^N`, which assigns each
-            node to a specific example. :obj:`batch_x` needs to be sorted.
-            (default: :obj:`None`)
-        batch_y (LongTensor, optional): Batch vector
-            :math:`\mathbf{b} \in {\{ 0, \ldots, B-1\}}^M`, which assigns each
-            node to a specific example. :obj:`batch_y` needs to be sorted.
-            (default: :obj:`None`)
-        cosine (boolean, optional): If :obj:`True`, will use the Cosine
-            distance instead of the Euclidean distance to find nearest
-            neighbors. (default: :obj:`False`)
-        num_workers (int): Number of workers to use for computation. Has no
-            effect in case :obj:`batch_x` or :obj:`batch_y` is not
-            :obj:`None`, or the input lies on the GPU. (default: :obj:`1`)
-    :rtype: :class:`LongTensor`
-    .. code-block:: python
-        import torch
-        from torch_cluster import knn
-        x = torch.Tensor([[-1, -1], [-1, 1], [1, -1], [1, 1]])
-        batch_x = torch.tensor([0, 0, 0, 0])
-        y = torch.Tensor([[-1, 0], [1, 0]])
-        batch_y = torch.tensor([0, 0])
-        assign_index = knn(x, y, 2, batch_x, batch_y)
-    """
-
-    x = x.view(-1, 1) if x.dim() == 1 else x
-    y = y.view(-1, 1) if y.dim() == 1 else y
-    x, y = x.contiguous(), y.contiguous()
-
-    batch_size = 1
-    if batch_x is not None:
-        assert x.size(0) == batch_x.numel()
-        batch_size = int(batch_x.max()) + 1
-    if batch_y is not None:
-        assert y.size(0) == batch_y.numel()
-        batch_size = max(batch_size, int(batch_y.max()) + 1)
-
-    ptr_x: Optional[torch.Tensor] = None
-    ptr_y: Optional[torch.Tensor] = None
-    if batch_size > 1:
-        assert batch_x is not None
-        assert batch_y is not None
-        arange = torch.arange(batch_size + 1, device=x.device)
-        ptr_x = torch.bucketize(arange, batch_x)
-        ptr_y = torch.bucketize(arange, batch_y)
-
-    return torch.ops.torch_cluster.knn(x, y, ptr_x, ptr_y, k, cosine,
-                                       num_workers)
