f79350e6ab360206208a2c3cfe268279aa8ff833
Farouk <farouk.mokhtar@gmail.com>
2022-06-16 15:59:10 +0200

    up

--- mlpf/ddp.py
+++ mlpf/ddp.py
@@ -55,30 +55,32 @@ class ToyModel(nn.Module):
     def forward(self, x):
         return self.net2(self.relu(self.net1(x)))
 
-    def demo_basic(rank, world_size):
-        print(f"Running basic DDP example on rank {rank}.")
-        setup(rank, world_size)
 
-        # create model and move it to GPU with id rank
-        model = ToyModel().to(rank)
-        ddp_model = DDP(model, device_ids=[rank])
+def demo_basic(rank, world_size):
+    print(f"Running basic DDP example on rank {rank}.")
+    setup(rank, world_size)
 
-        loss_fn = nn.MSELoss()
-        optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
+    # create model and move it to GPU with id rank
+    model = ToyModel().to(rank)
+    ddp_model = DDP(model, device_ids=[rank])
 
-        optimizer.zero_grad()
-        outputs = ddp_model(torch.randn(20, 10))
-        labels = torch.randn(20, 5).to(rank)
-        loss_fn(outputs, labels).backward()
-        optimizer.step()
+    loss_fn = nn.MSELoss()
+    optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
 
-        cleanup()
+    optimizer.zero_grad()
+    outputs = ddp_model(torch.randn(20, 10))
+    labels = torch.randn(20, 5).to(rank)
+    loss_fn(outputs, labels).backward()
+    optimizer.step()
 
-    def run_demo(demo_fn, world_size):
-        mp.spawn(demo_fn,
-                 args=(world_size,),
-                 nprocs=world_size,
-                 join=True)
+    cleanup()
+
+
+def run_demo(demo_fn, world_size):
+    mp.spawn(demo_fn,
+             args=(world_size,),
+             nprocs=world_size,
+             join=True)
 
 
 if __name__ == "__main__":
