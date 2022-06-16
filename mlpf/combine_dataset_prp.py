e19bd2f5143fa6d1aea8d43b80671a4bf5558901
Farouk <farouk.mokhtar@gmail.com>
2022-06-16 13:54:13 +0200

    up

--- mlpf/combine_dataset_prp.py
+++ mlpf/combine_dataset_prp.py
@@ -16,29 +16,32 @@ import multiprocessing
 from pyg import make_file_loaders
 from pyg.dataset import PFGraphDataset, one_hot_embedding
 
+#
+# path_from = '/particleflowvol/particleflow/data/cms/TTbar_14TeV_TuneCUETP8M1_cfi/'
+# path_to = '/particleflowvol/all/'
+#
+# os.makedirs(path_to, exist_ok=True)
+#
+# dataset = PFGraphDataset(path_from, 'cms')
+#
+# dataset = torch.utils.data.Subset(dataset, np.arange(start=0, stop=500))
+# loader = make_file_loaders(dataset, 1, num_workers=10, prefetch_factor=10)
+#
+# all = []
+# c = 0
+# j = 0
+#
+# for i, file in enumerate(loader):
+#     print(f'file # {i}')
+#     all = all + file
+#     c = c + 1
+#
+#     if c == 10:
+#         print(f'SAVING FILE # {j}')
+#         torch.save(all, path_to + f'data_{j}.pt')
+#         c = 0
+#         all = []
+#         j = j + 1
 
-path_from = '/particleflowvol/particleflow/data/cms/TTbar_14TeV_TuneCUETP8M1_cfi/'
-path_to = '/particleflowvol/all/'
-
-os.makedirs(path_to, exist_ok=True)
-
-dataset = PFGraphDataset(path_from, 'cms')
-
-dataset = torch.utils.data.Subset(dataset, np.arange(start=0, stop=500))
-loader = make_file_loaders(dataset, 1, num_workers=10, prefetch_factor=10)
-
-all = []
-c = 0
-j = 0
-
-for i, file in enumerate(loader):
-    print(f'file # {i}')
-    all = all + file
-    c = c + 1
-
-    if c == 10:
-        print(f'SAVING FILE # {j}')
-        torch.save(all, path_to + f'data_{j}.pt')
-        c = 0
-        all = []
-        j = j + 1
+a = torch.load('/particleflowvol/particleflow/data/cms/10s/TTbar_14TeV_TuneCUETP8M1_cfi/processed/data_0.pt')
+print(a)
