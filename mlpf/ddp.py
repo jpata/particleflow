884866eab81ee72ac6f8fbee503e4dc4165f0c4c
Farouk <farouk.mokhtar@gmail.com>
2022-06-16 17:49:11 +0200

    up

--- mlpf/ddp.py
+++ mlpf/ddp.py
@@ -107,7 +107,6 @@ def training_loop(rank, world_size):
 
             loss_clf = torch.nn.functional.cross_entropy(pred[:, :9], target['ygen_id'])  # for classifying PID
 
-            print(rank, loss_clf)
             optimizer.zero_grad()
             loss_clf.backward()
             optimizer.step()
@@ -117,48 +116,6 @@ def training_loop(rank, world_size):
         t0 = time.time()
 
     print(f'total time is {round(time.time()-tt0,3)}s')
-
-    cleanup()
-
-
-def demo_checkpoint(rank, world_size):
-    print(f"Running DDP checkpoint example on rank {rank}.")
-    setup(rank, world_size)
-
-    model = ToyModel().to(rank)
-    ddp_model = DDP(model, device_ids=[rank])
-
-    CHECKPOINT_PATH = tempfile.gettempdir() + "/model.checkpoint"
-    if rank == 0:
-        # All processes should see same parameters as they all start from same
-        # random parameters and gradients are synchronized in backward passes.
-        # Therefore, saving it in one process is sufficient.
-        torch.save(ddp_model.state_dict(), CHECKPOINT_PATH)
-
-    # Use a barrier() to make sure that process 1 loads the model after process
-    # 0 saves it.
-    dist.barrier()
-    # configure map_location properly
-    map_location = {'cuda:%d' % 0: 'cuda:%d' % rank}
-    ddp_model.load_state_dict(
-        torch.load(CHECKPOINT_PATH, map_location=map_location))
-
-    loss_fn = nn.MSELoss()
-    optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
-
-    optimizer.zero_grad()
-    outputs = ddp_model(torch.randn(20, 10))
-    labels = torch.randn(20, 5).to(rank)
-
-    loss_fn(outputs, labels).backward()
-    optimizer.step()
-
-    # Not necessary to use a dist.barrier() to guard the file deletion below
-    # as the AllReduce ops in the backward pass of DDP already served as
-    # a synchronization.
-
-    if rank == 0:
-        os.remove(CHECKPOINT_PATH)
 
     cleanup()
 
