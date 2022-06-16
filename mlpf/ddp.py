f9f97b0948e3e3acf3227dbb65c4c811df6570bc
Farouk <farouk.mokhtar@gmail.com>
2022-06-16 15:58:34 +0200

    ddp

---
+++ mlpf/ddp.py
@@ -0,0 +1,90 @@
+
+import torch
+import torch_geometric
+from torch_geometric.loader import DataLoader, DataListLoader
+
+import mplhep as hep
+import time
+import matplotlib.pyplot as plt
+from glob import glob
+import sys
+import os
+import os.path as osp
+import shutil
+import pickle as pkl
+import json
+import math
+import time
+import tqdm
+import numpy as np
+import pandas as pd
+import sklearn
+import matplotlib
+
+import os
+import sys
+import tempfile
+import torch
+import torch.distributed as dist
+import torch.nn as nn
+import torch.optim as optim
+import torch.multiprocessing as mp
+
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+
+def setup(rank, world_size):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12355'
+
+    # initialize the process group
+    dist.init_process_group("gloo", rank=rank, world_size=world_size)
+
+
+def cleanup():
+    dist.destroy_process_group()
+
+
+class ToyModel(nn.Module):
+    def __init__(self):
+        super(ToyModel, self).__init__()
+        self.net1 = nn.Linear(10, 10)
+        self.relu = nn.ReLU()
+        self.net2 = nn.Linear(10, 5)
+
+    def forward(self, x):
+        return self.net2(self.relu(self.net1(x)))
+
+    def demo_basic(rank, world_size):
+        print(f"Running basic DDP example on rank {rank}.")
+        setup(rank, world_size)
+
+        # create model and move it to GPU with id rank
+        model = ToyModel().to(rank)
+        ddp_model = DDP(model, device_ids=[rank])
+
+        loss_fn = nn.MSELoss()
+        optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
+
+        optimizer.zero_grad()
+        outputs = ddp_model(torch.randn(20, 10))
+        labels = torch.randn(20, 5).to(rank)
+        loss_fn(outputs, labels).backward()
+        optimizer.step()
+
+        cleanup()
+
+    def run_demo(demo_fn, world_size):
+        mp.spawn(demo_fn,
+                 args=(world_size,),
+                 nprocs=world_size,
+                 join=True)
+
+
+if __name__ == "__main__":
+    n_gpus = torch.cuda.device_count()
+    assert n_gpus >= 2, f"Requires at least 2 GPUs to run, but got {n_gpus}"
+    world_size = n_gpus
+    run_demo(demo_basic, world_size)
+    run_demo(demo_checkpoint, world_size)
+    run_demo(demo_model_parallel, world_size)
