dad1f63a80d25dbdf7474c42fd24c23d5ede24ff
Farouk <farouk.mokhtar@gmail.com>
2022-06-16 16:12:28 +0200

    up

--- mlpf/ddp.py
+++ mlpf/ddp.py
@@ -1,3 +1,33 @@
+from torch_geometric.data import Data, Batch
+from pyg import parse_args
+from pyg import PFGraphDataset, one_hot_embedding
+from pyg import MLPF, training_loop, make_predictions, make_plots
+from pyg import save_model, load_model, make_directories_for_plots
+from pyg import features_delphes, features_cms, target_p4
+from pyg import make_file_loaders
+
+import torch
+import torch_geometric
+from torch_geometric.loader import DataLoader, DataListLoader
+
+import mplhep as hep
+import time
+import matplotlib.pyplot as plt
+from glob import glob
+import sys
+import os
+import os.path as osp
+import shutil
+import pickle as pkl
+import json
+import math
+import time
+import tqdm
+import numpy as np
+import pandas as pd
+import sklearn
+import matplotlib
+
 
 import torch
 import torch_geometric
@@ -45,34 +75,35 @@ def cleanup():
     dist.destroy_process_group()
 
 
-class ToyModel(nn.Module):
-    def __init__(self):
-        super(ToyModel, self).__init__()
-        self.net1 = nn.Linear(10, 10)
-        self.relu = nn.ReLU()
-        self.net2 = nn.Linear(10, 5)
-
-    def forward(self, x):
-        print(x.device)
-        return self.net2(self.relu(self.net1(x)))
-
-
-def demo_basic(rank, world_size):
-    print(f"Running basic DDP example on rank {rank}.")
+def training_loop(rank, world_size):
+    print(f"Running training_loop DDP example on rank {rank}.")
     setup(rank, world_size)
 
+    dataset = PFGraphDataset('/particleflowvol/particleflow/data/cms/TTbar_14TeV_TuneCUETP8M1_cfi/', 'cms')
+    train_dataset = torch.utils.data.Subset(dataset, np.arange(start=0, stop=1))
+    # construct file loaders
+    file_loader_train = make_file_loaders(train_dataset)
+
     # create model and move it to GPU with id rank
-    model = ToyModel().to(rank)
+    model = MLPF().to(rank)
+
     ddp_model = DDP(model, device_ids=[rank])
 
-    loss_fn = nn.MSELoss()
-    optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
+    optimizer = torch.optim.Adam(ddp_model.parameters(), lr=0.001)
 
-    optimizer.zero_grad()
-    outputs = ddp_model(torch.randn(20, 10))
-    labels = torch.randn(20, 5).to(rank)
-    loss_fn(outputs, labels).backward()
-    optimizer.step()
+    for num, file in enumerate(file_loader):
+        file = [x for t in file for x in t]     # unpack the list of tuples to a list
+        loader = DataListLoader(file, batch_size=batch_size)
+
+        for i, batch in enumerate(loader):
+
+            pred, target = ddp_model(batch)
+
+            loss_clf = torch.nn.functional.cross_entropy(pred[:, :num_classes], target['ygen_id'])  # for classifying PID
+
+            optimizer.zero_grad()
+            loss_clf.backward()
+            optimizer.step()
 
     cleanup()
 
@@ -130,5 +161,6 @@ if __name__ == "__main__":
     n_gpus = torch.cuda.device_count()
     assert n_gpus >= 2, f"Requires at least 2 GPUs to run, but got {n_gpus}"
     world_size = n_gpus
-    run_demo(demo_basic, world_size)
-    run_demo(demo_checkpoint, world_size)
+
+    run_demo(training_loop, world_size)
+    # run_demo(demo_checkpoint, world_size)
