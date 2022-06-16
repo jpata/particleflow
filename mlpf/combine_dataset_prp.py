05e2b0531758f9180c0e8a8bfc0842d7fba7e588
Farouk <farouk.mokhtar@gmail.com>
2022-06-16 14:04:19 +0200

    up

--- mlpf/combine_dataset_prp.py
+++ mlpf/combine_dataset_prp.py
@@ -18,7 +18,6 @@ from pyg import PFGraphDataset, one_hot_embedding
 
 #
 # path_from = '/particleflowvol/particleflow/data/cms/TTbar_14TeV_TuneCUETP8M1_cfi/'
-# path_to = '/particleflowvol/all/'
 #
 # os.makedirs(path_to, exist_ok=True)
 #
@@ -43,5 +42,11 @@ from pyg import PFGraphDataset, one_hot_embedding
 #         all = []
 #         j = j + 1
 
-a = torch.load('/particleflowvol/particleflow/data/cms/10s/TTbar_14TeV_TuneCUETP8M1_cfi/processed/data_0.pt')
-print(a)
+path_from = '/particleflowvol/particleflow/data/cms/10s/TTbar_14TeV_TuneCUETP8M1_cfi/processed/'
+path_to = '/particleflowvol/particleflow/data/cms/10s2/TTbar_14TeV_TuneCUETP8M1_cfi/processed/'
+
+for i in range(50):
+    print(f'file # {i}')
+    prp = torch.load(path_from + f'data_{i}.pt')
+    torch.save([x for t in prp for x in t], path_to + f'data_{i}.pt')
+    print(f'SAVING FILE # {i}')
