68eb32a57552dc1b854c092429a537487de78b8b
Farouk <farouk.mokhtar@gmail.com>
2022-06-20 16:39:20 +0200

    up

--- mlpf/combine_dataset_prp.py
+++ mlpf/combine_dataset_prp.py
@@ -16,38 +16,38 @@ import multiprocessing
 from pyg import make_file_loaders
 from pyg import PFGraphDataset, one_hot_embedding
 
-
-path_from = '/particleflowvol/particleflow/data/cms/TTbar_14TeV_TuneCUETP8M1_cfi/'
-path_to = '/particleflowvol/particleflow/data/cms/2s/TTbar_14TeV_TuneCUETP8M1_cfi/processed/'
-
-os.makedirs(path_to, exist_ok=True)
-
-dataset = PFGraphDataset(path_from, 'cms')
-
-dataset = torch.utils.data.Subset(dataset, np.arange(start=0, stop=500))
-loader = make_file_loaders(0, dataset, 1, num_workers=2, prefetch_factor=4)
-
-all = []
-c = 0
-j = 0
-
-for i, file in enumerate(loader):
-    print(f'file # {i}')
-    all = all + file
-    c = c + 1
-
-    if c == 2:
-        print(f'SAVING FILE # {j}')
-        torch.save(all, path_to + f'data_{j}.pt')
-        c = 0
-        all = []
-        j = j + 1
 #
-# path_from = '/particleflowvol/particleflow/data/cms/10s/TTbar_14TeV_TuneCUETP8M1_cfi/processed/'
-# path_to = '/particleflowvol/particleflow/data/cms/10s2/TTbar_14TeV_TuneCUETP8M1_cfi/processed/'
+# path_from = '/particleflowvol/particleflow/data/cms/TTbar_14TeV_TuneCUETP8M1_cfi/'
+# path_to = '/particleflowvol/particleflow/data/cms/2s/TTbar_14TeV_TuneCUETP8M1_cfi/processed/'
 #
-# for i in range(50):
+# os.makedirs(path_to, exist_ok=True)
+#
+# dataset = PFGraphDataset(path_from, 'cms')
+#
+# dataset = torch.utils.data.Subset(dataset, np.arange(start=0, stop=500))
+# loader = make_file_loaders(0, dataset, 1, num_workers=2, prefetch_factor=4)
+
+# all = []
+# c = 0
+# j = 0
+#
+# for i, file in enumerate(loader):
 #     print(f'file # {i}')
-#     prp = torch.load(path_from + f'data_{i}.pt')
-#     torch.save([x for t in prp for x in t], path_to + f'data_{i}.pt')
-#     print(f'SAVING FILE # {i}')
+#     all = all + file
+#     c = c + 1
+#
+#     if c == 2:
+#         print(f'SAVING FILE # {j}')
+#         torch.save(all, path_to + f'data_{j}.pt')
+#         c = 0
+#         all = []
+#         j = j + 1
+
+path_from = '/particleflowvol/particleflow/data/cms/2s/TTbar_14TeV_TuneCUETP8M1_cfi/processed/'
+path_to = '/particleflowvol/particleflow/data/cms/2sv2/TTbar_14TeV_TuneCUETP8M1_cfi/processed/'
+
+for i in range(250):
+    print(f'file # {i}')
+    prp = torch.load(path_from + f'data_{i}.pt')
+    torch.save([x for t in prp for x in t], path_to + f'data_{i}.pt')
+    print(f'SAVING FILE # {i}')
