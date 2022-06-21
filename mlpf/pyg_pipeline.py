a87cef1f96257e9fe88cdc6fa677eef48eba8b93
Farouk <farouk.mokhtar@gmail.com>
2022-06-21 15:12:44 +0200

    up

--- mlpf/pyg_pipeline.py
+++ mlpf/pyg_pipeline.py
@@ -57,8 +57,7 @@ def setup(rank, world_size):
     os.environ['MASTER_PORT'] = '12355'
 
     # dist.init_process_group("gloo", rank=rank, world_size=world_size)
-    # dist.init_process_group("nccl", rank=rank, world_size=world_size)   # should be faster for DistributedDataParallel on gpus
-    dist.init_process_group("mpi", rank=rank, world_size=world_size)
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)   # should be faster for DistributedDataParallel on gpus
 
 
 def cleanup():
