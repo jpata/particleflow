9b9cd00e66ea0e77d4c88930ac22770cbed9d4fb
Farouk <farouk.mokhtar@gmail.com>
2022-06-24 12:34:30 +0200

    remove iteration

--- mlpf/pyg/evaluate.py
+++ mlpf/pyg/evaluate.py
@@ -112,12 +112,12 @@ def make_predictions(rank, data, model, file_loader, batch_size, num_classes, ou
                     yvals[f'gen_{key}'].append(vars_padded['ygen'][:, :, feat].reshape(-1, padded_num_elem_size, 1))
                     yvals[f'cand_{key}'].append(vars_padded['ycand'][:, :, feat].reshape(-1, padded_num_elem_size, 1))
                     yvals[f'pred_{key}'].append(vars_padded['pred_p4'][:, :, feat].reshape(-1, padded_num_elem_size, 1))
-
-            if i == 2:
-                break
-
-        if num == 2:
-            break
+        #
+        #     if i == 2:
+        #         break
+        #
+        # if num == 2:
+        #     break
 
         print(f'Average inference time per batch on rank {rank} is {round((t / len(loader)), 3)}s')
 
