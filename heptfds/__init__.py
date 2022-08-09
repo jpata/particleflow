e9f77679f8664d3124c1700e8c949ef6d995000d
Joosep Pata <joosep.pata@cern.ch>
2022-08-09 16:41:59 +0300

    up

--- heptfds/__init__.py
+++ heptfds/__init__.py
@@ -3,11 +3,13 @@
 from .delphes_pf import DelphesPf
 from .cms_pf import (
     CmsPfTtbar,
+    CmsPfQcd,
+    CmsPfQcdHighPt,
+    CmsPfZtt,
     CmsPfSinglePi,
     CmsPfSingleElectron,
     CmsPfSingleMu,
     CmsPfSingleTau,
     CmsPfSinglePi0,
-    CmsPfSingleGamma,
-    CmsPfZtt
+    CmsPfSingleGamma
 )
