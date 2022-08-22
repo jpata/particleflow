8735d0d3bef50beb99f7a25d8f8ffadb7c525b84
Joosep Pata <joosep.pata@cern.ch>
2022-08-22 15:04:07 +0300

    compare all

---
+++ mlpf/tallinn/submit-test-eventloss.sh
@@ -0,0 +1,3 @@
+sbatch mlpf/tallinn/test-swd.sh parameters/test-swd/baseline.yaml
+sbatch mlpf/tallinn/test-swd.sh parameters/test-swd/swd.yaml
+sbatch mlpf/tallinn/test-swd.sh parameters/test-swd/h2d.yaml
--- mlpf/tallinn/submit-test-swd.sh
+++
@@ -1,4 +0,0 @@
-sbatch mlpf/tallinn/test-swd.sh parameters/test-swd/no-swd.yaml
-sbatch mlpf/tallinn/test-swd.sh parameters/test-swd/swd-4.yaml
-sbatch mlpf/tallinn/test-swd.sh parameters/test-swd/swd-5.yaml
-sbatch mlpf/tallinn/test-swd.sh parameters/test-swd/swd-6.yaml
