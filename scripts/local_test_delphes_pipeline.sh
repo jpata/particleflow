ef9ea15c2820d7a39f1fd827772f1837a1fe73fa
Javier Duarte <jduarte@ucsd.edu>
2022-08-24 06:41:10 -0700

    Gen jet loss (#126)
    * first attempt gen jet loss
    
    * update
    
    * try python 3.9
    
    * try again
    
    * pip install ./hep_tfds
    
    * WIP debugging
    
    * update hep_tfds for bugfix
    
    * disable mask_reg_cls0, revert debugging
    
    * compute correct sum_pt in histogram bins
    
    * Update cms-gen.yaml
    
    * Update delphes.yaml
    
    Co-authored-by: Joosep Pata <joosep.pata@gmail.com>
    Former-commit-id: ac15a9ddfc25cfdc2e9d2f821bf73997e1ecb1c5

--- scripts/local_test_delphes_pipeline.sh
+++ scripts/local_test_delphes_pipeline.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-export PYTHONPATH=`pwd`/hep_tfds
 export TFDS_DATA_DIR=`pwd`/tensorflow_datasets
 
 mkdir -p data/delphes_pf/pythia8_ttbar/raw
@@ -15,9 +14,9 @@ mv tev14_pythia8_qcd_10_0.pkl.bz2 data/delphes_pf/pythia8_qcd/val/
 tfds build hep_tfds/heptfds/delphes_pf --download_dir data/
 
 #Run a simple training on a few events
-python3 mlpf/pipeline.py train -c parameters/delphes.yaml --nepochs 1 --ntrain 5 --ntest 5 --customize pipeline_test
+python mlpf/pipeline.py train -c parameters/delphes.yaml --nepochs 1 --ntrain 5 --ntest 5 --customize pipeline_test
 
 ls ./experiments/delphes_*/weights/
 
 #Generate the pred.npz file of predictions
-python3 mlpf/pipeline.py evaluate -t ./experiments/delphes_*
+python mlpf/pipeline.py evaluate -t ./experiments/delphes_*
