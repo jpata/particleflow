6c52a806852f523ac332b3cdc7e39ff73b979b37
Joosep Pata <joosep.pata@gmail.com>
2022-08-30 11:35:44 +0300

    move path

--- scripts/local_test_cms_pipeline.sh
+++ scripts/local_test_cms_pipeline.sh
@@ -17,7 +17,7 @@ cd ../../..
 rm -Rf local_test_data/TTbar_14TeV_TuneCUETP8M1_cfi/raw
 mkdir -p local_test_data/TTbar_14TeV_TuneCUETP8M1_cfi/raw
 for file in `\ls -1 local_test_data/TTbar_14TeV_TuneCUETP8M1_cfi/root/*.root`; do
-	python mlpf/data/postprocessing2.py \
+	python mlpf/data_cms/postprocessing2.py \
 	  --input $file \
 	  --outpath local_test_data/TTbar_14TeV_TuneCUETP8M1_cfi/raw \
 	  --save-normalized-table --num-events 10
