b4e6a27e3191eb1b85cac514cbf33588a9caa651
Joosep Pata <joosep.pata@cern.ch>
2022-11-03 14:31:16 +0200

    update notebook

--- mlpf/tallinn/eval.sh
+++ mlpf/tallinn/eval.sh
@@ -4,12 +4,7 @@
 #SBATCH --mem-per-gpu=10G
 #SBATCH -o logs/slurm-%x-%j-%N.out
 
-#singularity exec --nv -B /scratch-persistent --env PYTHONPATH=hep_tfds --env TFDS_DATA_DIR=/scratch-persistent/joosep/tensorflow_datasets /home/software/singularity/tf-2.9.0.simg \
-#    python3 mlpf/pipeline.py evaluate \
-#    -t experiments/cms-gen_20220923_163529_426249.gpu0.local \
-#    -w experiments/cms-gen_20220923_163529_426249.gpu0.local/weights/weights-18-2.223574.hdf5 --nevents 5000
-
 singularity exec --nv -B /scratch-persistent --env PYTHONPATH=hep_tfds --env TFDS_DATA_DIR=/scratch-persistent/joosep/tensorflow_datasets /home/software/singularity/tf-2.9.0.simg \
     python3 mlpf/pipeline.py evaluate \
-    -t experiments/cms-transformer_20221012_092453_022862.gpu0.local \
-    --nevents 1000
+    -t experiments/cms-gen_20221019_233748_248552.gpu0.local \
+    --nevents 5000
