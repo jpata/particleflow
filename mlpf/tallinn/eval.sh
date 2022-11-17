1a8b013f60578ff9cad7726b725cd09217c870ce
Joosep Pata <joosep.pata@cern.ch>
2022-11-17 10:28:22 +0200

    up

--- mlpf/tallinn/eval.sh
+++ mlpf/tallinn/eval.sh
@@ -6,5 +6,5 @@
 
 singularity exec --nv -B /scratch-persistent --env PYTHONPATH=hep_tfds --env TFDS_DATA_DIR=/scratch-persistent/joosep/tensorflow_datasets /home/software/singularity/tf-2.10.0.simg \
     python3 mlpf/pipeline.py evaluate \
-    -t experiments/cms-transformer_20221109_115555_189488.gpu0.local \
+    -t experiments/cms-transformer_20221114_182159_902630.gpu0.local \
     --nevents 5000
