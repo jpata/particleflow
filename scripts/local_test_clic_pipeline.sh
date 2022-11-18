0794992aebb5f6d47cc1427c66b5d29b6855d0c7
Joosep Pata <joosep.pata@gmail.com>
2022-11-18 18:01:13 +0200

    up

--- scripts/local_test_clic_pipeline.sh
+++ scripts/local_test_clic_pipeline.sh
@@ -3,7 +3,8 @@ set -e
 export TFDS_DATA_DIR=`pwd`/tensorflow_datasets
 
 rm -Rf local_test_data/gev380ee_pythia6_ttbar_rfull201
-cd local_test_data/gev380ee_pythia6_ttbar_rfull201/
+mkdir -p local_test_data/gev380ee_pythia6_ttbar_rfull201
+cd local_test_data/gev380ee_pythia6_ttbar_rfull201
 
 #download some test data
 wget -q --no-check-certificate -nc https://jpata.web.cern.ch/jpata/mlpf/clic/gev380ee_pythia6_ttbar_rfull201/pythia6_ttbar_0001_pandora.parquet
