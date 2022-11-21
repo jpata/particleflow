630fe65e9911907a48ef63007e91f165fbeec017
Joosep Pata <joosep.pata@kbfi.ee>
2022-11-21 23:29:28 +0200

    up

---
+++ mlpf/lumi/train-gpu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=mlpf-train-cms-gen
+#SBATCH --account=project_465000301
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=50G
+#SBATCH --gpus-per-node=1
+#SBATCH --partition=eap
+#SBATCH -o logs/slurm-%x-%j-%N.out
+
+IMG=/users/patajoos/tf-rocm.simg
+cd ~/particleflow
+
+#TF training
+singularity exec \
+    --env ROCR_VISIBLE_DEVICES=$ROCR_VISIBLE_DEVICES \
+    --rocm \
+    -B /scratch/project_465000301 \
+    --env PYTHONPATH=hep_tfds \
+    --env TFDS_DATA_DIR=/scratch/project_465000301/tensorflow_datasets \
+    $IMG ./mlpf/lumi/train-gpu-worker.sh $1
