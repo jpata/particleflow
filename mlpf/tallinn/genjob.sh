69de348f74452ceb728e94ec5795d31cfbd76a45
Joosep Pata <joosep.pata@cern.ch>
2022-12-09 14:59:35 +0200

    prepare for new gen

--- mlpf/tallinn/genjob.sh
+++ mlpf/tallinn/genjob.sh
@@ -3,6 +3,8 @@
 #SBATCH --mem-per-cpu=4G
 #SBATCH --cpus-per-task=1
 #SBATCH -o logs/slurm-%x-%j-%N.out
+set -e
+set -x
 
 env
 df -h
@@ -10,7 +12,7 @@ df -h
 WORKDIR=/scratch/$USER/${SLURM_JOB_ID}
 SAMPLE=$1
 SEED=$2
-OUTDIR=/hdfs/local/joosep/mlpf/gen/v2_gun/
+OUTDIR=/local/joosep/mlpf/gen/v3/
 
 mkdir -p $WORKDIR
 cd $WORKDIR
