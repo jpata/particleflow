8112f2b357191d17206c259d3183edcbcfe71791
Joosep Pata <joosep.pata@gmail.com>
2023-01-23 16:22:34 +0200

    added new datasets

--- mlpf/plotting/plot_utils.py
+++ mlpf/plotting/plot_utils.py
@@ -70,6 +70,8 @@ EVALUATION_DATASET_NAMES = {
     "cms_pf_ttbar": r"CMS $\mathrm{t}\overline{\mathrm{t}}$+PU events",
     "cms_pf_single_neutron": r"CMS single neutron particle gun events",
     "clic_edm_ttbar_pf": r"CLIC $ee \rightarrow \mathrm{t}\overline{\mathrm{t}}$",
+    "clic_edm_qcd_pf": r"CLIC $ee \rightarrow \gamma/\mathrm{Z}^* \rightarrow \mathrm{hadrons}$",
+    "clic_edm_zz_fullhad_pf": r"CLIC $ee \rightarrow \mathrm{ZZ} \rightarrow \mathrm{hadrons}$",
 }
 
 
