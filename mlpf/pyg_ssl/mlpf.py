516ee0592c52ce40dc75e9d2bacaf4688d170fd2
Joosep Pata <joosep.pata@cern.ch>
2023-02-03 17:21:22 +0200

    fix

--- mlpf/pyg_ssl/mlpf.py
+++ mlpf/pyg_ssl/mlpf.py
@@ -93,7 +93,7 @@ class MLPF(nn.Module):
                 nn.Linear(width, embedding_dim),
             )
 
-        self.conv_type = "attention"
+        self.conv_type = "gravnet"
         # GNN that uses the embeddings learnt by VICReg as the input features
         if self.conv_type == "gravnet":
             self.conv_id = nn.ModuleList()
