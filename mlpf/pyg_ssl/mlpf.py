12f85093e5270fc29f670fa919cde4a24aea1bdc
Farouk Mokhtar <farouk.mokhtar@gmail.com>
2023-03-01 15:29:56 +0100

    optimizing VICReg (#173)
    * add evaluate script
    
    * set VICReg to evaluation mode
    
    * print best validation loss on vicreg loss plot
    
    * multigpu compatibility
    
    * Revert to bd2a3e8
    
    * fix loading of mlpf models
    
    * add plots for each component of each loss
    
    * oops
    
    * add notebook for ssl optimizations
    
    * fix quick datasplit mode to make sense
    
    * add vicreg embeddings after gnn in mlpf
    
    * for num_convs=0
    
    * mdmm
    
    * add extra dnn layer for native
    
    * early results
    
    * pooling after decoder
    
    * update vicreg, loss and multigpu
    
    * up early stopping
    
    * better gpu util
    
    ---------
    
    Co-authored-by: Joosep Pata <joosep.pata@gmail.com>

--- mlpf/pyg_ssl/mlpf.py
+++ mlpf/pyg_ssl/mlpf.py
@@ -51,25 +51,49 @@ class SelfAttentionLayer(nn.Module):
         return x
 
 
-def ffn(input_dim, output_dim, width, act, dropout):
-    return nn.Sequential(
-        nn.Linear(input_dim, width),
-        act(),
-        torch.nn.LayerNorm(width),
-        nn.Dropout(dropout),
-        nn.Linear(width, width),
-        act(),
-        torch.nn.LayerNorm(width),
-        nn.Dropout(dropout),
-        nn.Linear(width, width),
-        act(),
-        torch.nn.LayerNorm(width),
-        nn.Dropout(dropout),
-        nn.Linear(width, width),
-        act(),
-        torch.nn.LayerNorm(width),
-        nn.Linear(width, output_dim),
-    )
+def ffn(input_dim, output_dim, width, act, dropout, ssl):
+    if ssl:
+        return nn.Sequential(
+            nn.Linear(input_dim, width),
+            act(),
+            torch.nn.LayerNorm(width),
+            nn.Dropout(dropout),
+            nn.Linear(width, width),
+            act(),
+            torch.nn.LayerNorm(width),
+            nn.Dropout(dropout),
+            nn.Linear(width, width),
+            act(),
+            torch.nn.LayerNorm(width),
+            nn.Dropout(dropout),
+            nn.Linear(width, width),
+            act(),
+            torch.nn.LayerNorm(width),
+            nn.Linear(width, output_dim),
+        )
+    else:
+        return nn.Sequential(
+            nn.Linear(input_dim, width),
+            act(),
+            torch.nn.LayerNorm(width),
+            nn.Dropout(dropout),
+            nn.Linear(width, width),
+            act(),
+            torch.nn.LayerNorm(width),
+            nn.Dropout(dropout),
+            nn.Linear(width, width),
+            act(),
+            torch.nn.LayerNorm(width),
+            nn.Dropout(dropout),
+            nn.Linear(width, width),
+            act(),
+            torch.nn.LayerNorm(width),
+            nn.Dropout(dropout),
+            nn.Linear(width, width),
+            act(),
+            torch.nn.LayerNorm(width),
+            nn.Linear(width, output_dim),
+        )
 
 
 class MLPF(nn.Module):
@@ -80,99 +104,117 @@ class MLPF(nn.Module):
         width=126,
         num_convs=2,
         k=32,
-        native_mlpf=False,
         propagate_dimensions=32,
         space_dimensions=4,
         dropout=0.4,
+        ssl=False,
+        VICReg_embedding_dim=0,
     ):
         super(MLPF, self).__init__()
 
         self.act = nn.ELU
-        self.native_mlpf = native_mlpf  # boolean that is true for native mlpf and false for ssl
         self.dropout = dropout
+        self.input_dim = input_dim
+        self.num_convs = num_convs
+        self.ssl = ssl  # boolean that is True for ssl and False for native mlpf
 
         # embedding of the inputs
-        self.nn0 = nn.Sequential(
-            nn.Linear(input_dim, width),
-            self.act(),
-            nn.Linear(width, width),
-            self.act(),
-            nn.Linear(width, width),
-            self.act(),
-            nn.Linear(width, embedding_dim),
-        )
+        if num_convs != 0:
+            self.nn0 = nn.Sequential(
+                nn.Linear(input_dim, width),
+                self.act(),
+                nn.Linear(width, width),
+                self.act(),
+                nn.Linear(width, width),
+                self.act(),
+                nn.Linear(width, embedding_dim),
+            )
 
-        self.conv_type = "gravnet"
-        # GNN that uses the embeddings learnt by VICReg as the input features
-        if self.conv_type == "gravnet":
-            self.conv_id = nn.ModuleList()
-            self.conv_reg = nn.ModuleList()
-            for i in range(num_convs):
-                self.conv_id.append(GravNetLayer(embedding_dim, space_dimensions, propagate_dimensions, k, dropout))
-                self.conv_reg.append(GravNetLayer(embedding_dim, space_dimensions, propagate_dimensions, k, dropout))
-        elif self.conv_type == "attention":
-            self.conv_id = nn.ModuleList()
-            self.conv_reg = nn.ModuleList()
+            self.conv_type = "gravnet"
+            # GNN that uses the embeddings learnt by VICReg as the input features
+            if self.conv_type == "gravnet":
+                self.conv_id = nn.ModuleList()
+                self.conv_reg = nn.ModuleList()
+                for i in range(num_convs):
+                    self.conv_id.append(GravNetLayer(embedding_dim, space_dimensions, propagate_dimensions, k, dropout))
+                    self.conv_reg.append(GravNetLayer(embedding_dim, space_dimensions, propagate_dimensions, k, dropout))
+            elif self.conv_type == "attention":
+                self.conv_id = nn.ModuleList()
+                self.conv_reg = nn.ModuleList()
 
-            for i in range(num_convs):
-                self.conv_id.append(SelfAttentionLayer(embedding_dim))
-                self.conv_reg.append(SelfAttentionLayer(embedding_dim))
+                for i in range(num_convs):
+                    self.conv_id.append(SelfAttentionLayer(embedding_dim))
+                    self.conv_reg.append(SelfAttentionLayer(embedding_dim))
 
         decoding_dim = input_dim + num_convs * embedding_dim
+        if ssl:
+            decoding_dim += VICReg_embedding_dim
 
         # DNN that acts on the node level to predict the PID
-        self.nn_id = ffn(decoding_dim, NUM_CLASSES, width, self.act, dropout)
+        self.nn_id = ffn(decoding_dim, NUM_CLASSES, width, self.act, dropout, ssl)
 
         # elementwise DNN for node momentum regression
-        self.nn_pt = ffn(decoding_dim + NUM_CLASSES, 1, width, self.act, dropout)
-        self.nn_eta = ffn(decoding_dim + NUM_CLASSES, 1, width, self.act, dropout)
-        self.nn_phi = ffn(decoding_dim + NUM_CLASSES, 1, width, self.act, dropout)
-        self.nn_energy = ffn(decoding_dim + NUM_CLASSES, 1, width, self.act, dropout)
+        self.nn_pt = ffn(decoding_dim + NUM_CLASSES, 1, width, self.act, dropout, ssl)
+        self.nn_eta = ffn(decoding_dim + NUM_CLASSES, 1, width, self.act, dropout, ssl)
+        self.nn_phi = ffn(decoding_dim + NUM_CLASSES, 1, width, self.act, dropout, ssl)
+        self.nn_energy = ffn(decoding_dim + NUM_CLASSES, 1, width, self.act, dropout, ssl)
 
         # elementwise DNN for node charge regression, classes (-1, 0, 1)
-        self.nn_charge = ffn(decoding_dim + NUM_CLASSES, 3, width, self.act, dropout)
+        self.nn_charge = ffn(decoding_dim + NUM_CLASSES, 3, width, self.act, dropout, ssl)
 
     def forward(self, batch):
 
         # unfold the Batch object
-        input_ = batch.x.float()
-        batch_idx = batch.batch
+        if self.ssl:
+            input_ = batch.x.float()[:, : self.input_dim]
+            VICReg_embeddings = batch.x.float()[:, self.input_dim :]
+        else:
+            input_ = batch.x.float()
 
-        embedding = self.nn0(input_)
+        batch_idx = batch.batch
 
         embeddings_id = []
         embeddings_reg = []
 
-        if self.conv_type == "gravnet":
-            # perform a series of graph convolutions
-            for num, conv in enumerate(self.conv_id):
-                conv_input = embedding if num == 0 else embeddings_id[-1]
-                embeddings_id.append(conv(conv_input, batch_idx))
-            for num, conv in enumerate(self.conv_reg):
-                conv_input = embedding if num == 0 else embeddings_reg[-1]
-                embeddings_reg.append(conv(conv_input, batch_idx))
-        elif self.conv_type == "attention":
-            for num, conv in enumerate(self.conv_id):
-                conv_input = embedding if num == 0 else embeddings_id[-1]
-                input_padded, mask = torch_geometric.utils.to_dense_batch(conv_input, batch_idx)
-                out_padded = conv(input_padded, ~mask)
-                out_stacked = torch.cat([out_padded[i][mask[i]] for i in range(out_padded.shape[0])])
-                assert out_stacked.shape[0] == conv_input.shape[0]
-                embeddings_id.append(out_stacked)
-            for num, conv in enumerate(self.conv_reg):
-                conv_input = embedding if num == 0 else embeddings_reg[-1]
-                input_padded, mask = torch_geometric.utils.to_dense_batch(conv_input, batch_idx)
-                out_padded = conv(input_padded, ~mask)
-                out_stacked = torch.cat([out_padded[i][mask[i]] for i in range(out_padded.shape[0])])
-                assert out_stacked.shape[0] == conv_input.shape[0]
-                embeddings_reg.append(out_stacked)
+        if self.num_convs != 0:
+            embedding = self.nn0(input_)
 
-        embedding_id = torch.cat([input_] + embeddings_id, axis=-1)
+            if self.conv_type == "gravnet":
+                # perform a series of graph convolutions
+                for num, conv in enumerate(self.conv_id):
+                    conv_input = embedding if num == 0 else embeddings_id[-1]
+                    embeddings_id.append(conv(conv_input, batch_idx))
+                for num, conv in enumerate(self.conv_reg):
+                    conv_input = embedding if num == 0 else embeddings_reg[-1]
+                    embeddings_reg.append(conv(conv_input, batch_idx))
+            elif self.conv_type == "attention":
+                for num, conv in enumerate(self.conv_id):
+                    conv_input = embedding if num == 0 else embeddings_id[-1]
+                    input_padded, mask = torch_geometric.utils.to_dense_batch(conv_input, batch_idx)
+                    out_padded = conv(input_padded, ~mask)
+                    out_stacked = torch.cat([out_padded[i][mask[i]] for i in range(out_padded.shape[0])])
+                    assert out_stacked.shape[0] == conv_input.shape[0]
+                    embeddings_id.append(out_stacked)
+                for num, conv in enumerate(self.conv_reg):
+                    conv_input = embedding if num == 0 else embeddings_reg[-1]
+                    input_padded, mask = torch_geometric.utils.to_dense_batch(conv_input, batch_idx)
+                    out_padded = conv(input_padded, ~mask)
+                    out_stacked = torch.cat([out_padded[i][mask[i]] for i in range(out_padded.shape[0])])
+                    assert out_stacked.shape[0] == conv_input.shape[0]
+                    embeddings_reg.append(out_stacked)
+
+        if self.ssl:
+            embedding_id = torch.cat([input_] + embeddings_id + [VICReg_embeddings], axis=-1)
+        else:
+            embedding_id = torch.cat([input_] + embeddings_id, axis=-1)
 
         # predict the PIDs
         preds_id = self.nn_id(embedding_id)
 
-        embedding_reg = torch.cat([input_] + embeddings_reg + [preds_id], axis=-1)
+        if self.ssl:
+            embedding_reg = torch.cat([input_] + embeddings_reg + [preds_id] + [VICReg_embeddings], axis=-1)
+        else:
+            embedding_reg = torch.cat([input_] + embeddings_reg + [preds_id], axis=-1)
 
         # predict the 4-momentum, add it to the (pt, eta, phi, E) of the PFelement
         preds_pt = self.nn_pt(embedding_reg) + input_[:, 1:2]
