096779dd0d68ef9fd039c73af07de2e934806645
Farouk <farouk.mokhtar@gmail.com>
2023-03-05 14:44:55 +0100

    up

--- scripts/local_test_pyg_cms.sh
+++ scripts/local_test_pyg_cms.sh
@@ -1,34 +1,33 @@
-# #!/bin/bash
-# set -e
+#!/bin/bash
+set -e
 
-# # download and process the datasets under particleflow/data/cms/
-# rm -Rf data/cms/TTbar_14TeV_TuneCUETP8M1_cfi
-# rm -Rf data/cms/pythia8_qcd
+# download and process the datasets under particleflow/data/cms/
+rm -Rf data/cms/TTbar_14TeV_TuneCUETP8M1_cfi
+rm -Rf data/cms/pythia8_qcd
 
-# # make cms directories
-# mkdir -p data/cms/TTbar_14TeV_TuneCUETP8M1_cfi/raw
-# mkdir -p data/cms/QCDForPF_14TeV_TuneCUETP8M1_cfi/raw
+# make cms directories
+mkdir -p data/cms/TTbar_14TeV_TuneCUETP8M1_cfi/raw
+mkdir -p data/cms/QCDForPF_14TeV_TuneCUETP8M1_cfi/raw
 
-# # download some ttbar test data
-# cd data/cms/TTbar_14TeV_TuneCUETP8M1_cfi/raw/
-# wget --no-check-certificate -nc https://jpata.web.cern.ch/jpata/mlpf/cms/TTbar_14TeV_TuneCUETP8M1_cfi/raw/pfntuple_1.pkl.bz2
-# wget --no-check-certificate -nc https://jpata.web.cern.ch/jpata/mlpf/cms/TTbar_14TeV_TuneCUETP8M1_cfi/raw/pfntuple_2.pkl.bz2
-# wget --no-check-certificate -nc https://jpata.web.cern.ch/jpata/mlpf/cms/TTbar_14TeV_TuneCUETP8M1_cfi/raw/pfntuple_3.pkl.bz2
+# download some ttbar test data
+cd data/cms/TTbar_14TeV_TuneCUETP8M1_cfi/raw/
+wget --no-check-certificate -nc https://jpata.web.cern.ch/jpata/mlpf/cms/TTbar_14TeV_TuneCUETP8M1_cfi/raw/pfntuple_1.pkl.bz2
+wget --no-check-certificate -nc https://jpata.web.cern.ch/jpata/mlpf/cms/TTbar_14TeV_TuneCUETP8M1_cfi/raw/pfntuple_2.pkl.bz2
+wget --no-check-certificate -nc https://jpata.web.cern.ch/jpata/mlpf/cms/TTbar_14TeV_TuneCUETP8M1_cfi/raw/pfntuple_3.pkl.bz2
 
-# # download some qcd test data
-# cd ../../QCDForPF_14TeV_TuneCUETP8M1_cfi/raw/
-# wget --no-check-certificate -nc https://jpata.web.cern.ch/jpata/mlpf/cms/QCDForPF_14TeV_TuneCUETP8M1_cfi/raw/pfntuple_2001.pkl.bz2
-# wget --no-check-certificate -nc https://jpata.web.cern.ch/jpata/mlpf/cms/QCDForPF_14TeV_TuneCUETP8M1_cfi/raw/pfntuple_2002.pkl.bz2
-# wget --no-check-certificate -nc https://jpata.web.cern.ch/jpata/mlpf/cms/QCDForPF_14TeV_TuneCUETP8M1_cfi/raw/pfntuple_2003.pkl.bz2
+# download some qcd test data
+cd ../../QCDForPF_14TeV_TuneCUETP8M1_cfi/raw/
+wget --no-check-certificate -nc https://jpata.web.cern.ch/jpata/mlpf/cms/QCDForPF_14TeV_TuneCUETP8M1_cfi/raw/pfntuple_2001.pkl.bz2
+wget --no-check-certificate -nc https://jpata.web.cern.ch/jpata/mlpf/cms/QCDForPF_14TeV_TuneCUETP8M1_cfi/raw/pfntuple_2002.pkl.bz2
+wget --no-check-certificate -nc https://jpata.web.cern.ch/jpata/mlpf/cms/QCDForPF_14TeV_TuneCUETP8M1_cfi/raw/pfntuple_2003.pkl.bz2
 
-# # setup directory for processed datafiles
-# cd ../..
-# mkdir -p TTbar_14TeV_TuneCUETP8M1_cfi/processed
-# mkdir -p QCDForPF_14TeV_TuneCUETP8M1_cfi/processed
+# setup directory for processed datafiles
+cd ../..
+mkdir -p TTbar_14TeV_TuneCUETP8M1_cfi/processed
+mkdir -p QCDForPF_14TeV_TuneCUETP8M1_cfi/processed
 
 # process the raw datafiles
-# cd ../../mlpf/pyg/
-cd mlpf/pyg/
+cd ../../mlpf/pyg/
 echo -----------------------
 for sample in ../../data/cms/* ; do
   echo Processing $sample sample
--- scripts/local_test_pyg_delphes.sh
+++ scripts/local_test_pyg_delphes.sh
@@ -40,4 +40,4 @@ echo -----------------------
 
 # run a supervised training of mlpf on DELPHES dataset
 cd ../
-python pyg_pipeline.py --dataset DELPHES --prefix MLPF_test --n_train=1 --n_valid=1 --n_test=1
+python pyg_pipeline.py --dataset DELPHES --prefix MLPF_test --overwrite --n_train=1 --n_valid=1 --n_test=1
