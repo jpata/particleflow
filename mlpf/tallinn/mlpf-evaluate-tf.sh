6fcdde0ca2013c7e610e0803d236dd80226d1d64
Joosep Pata <joosep.pata@cern.ch>
2023-03-21 10:26:41 +0200

    up

---
+++ mlpf/tallinn/mlpf-evaluate-tf.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+IMG=/home/software/singularity/tf-2.11.0.simg
+cd ~/particleflow
+
+#TF training
+singularity exec -B /scratch-persistent --nv \
+    --env PYTHONPATH=hep_tfds \
+    --env TFDS_DATA_DIR=/scratch-persistent/joosep/tensorflow_datasets \
+    $IMG python mlpf/pipeline.py evaluate --train-dir $1
