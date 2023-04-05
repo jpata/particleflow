56b090d3d142b3a8383e2666cb318dbc9080220d
Joosep Pata <joosep.pata@cern.ch>
2023-04-05 09:11:04 +0300

    up

---
+++ mlpf/tallinn/mlpf-train-a100.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --partition gpu
+#SBATCH --gres gpu:a100:1
+#SBATCH --mem-per-gpu 40G
+#SBATCH -o logs/slurm-%x-%j-%N.out
+
+IMG=/home/software/singularity/tf-2.11.0.simg
+cd ~/particleflow
+
+#TF training
+singularity exec -B /scratch/persistent --nv \
+    --env PYTHONPATH=hep_tfds \
+    --env TFDS_DATA_DIR=/scratch/persistent/joosep/tensorflow_datasets \
+    $IMG python mlpf/pipeline.py train -c $1 --plot-freq 1 --num-cpus 16 --batch-multiplier $2
