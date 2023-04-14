bcc926e50324654b0fce0c27b5cafe4cf12416a1
Joosep Pata <joosep.pata@cern.ch>
2023-04-14 11:32:03 +0300

    up

--- mlpf/tallinn/mlpf-train.sh
+++ mlpf/tallinn/mlpf-train.sh
@@ -11,4 +11,4 @@ cd ~/particleflow
 singularity exec -B /scratch/persistent --nv \
     --env PYTHONPATH=hep_tfds \
     --env TFDS_DATA_DIR=/scratch/persistent/joosep/tensorflow_datasets \
-    $IMG python mlpf/pipeline.py train -c $1 --plot-freq 1 --num-cpus 16 --batch-multiplier $2
+    $IMG python mlpf/pipeline.py train -c $1 --plot-freq 1 --num-cpus 16 --batch-multiplier $2 --weights experiments/clic_20230412_155159_717751.gpu1.local/weights/weights-100-9.948204.hdf5
