0ef2de8f7f2d049b42c1ce906e86e52da742726c
Joosep Pata <joosep.pata@cern.ch>
2023-08-10 13:58:52 +0300

    up

---
+++ mlpf/heptfds/clic_pf_edm4hep_hits/single_pi.py
@@ -0,0 +1,67 @@
+from pathlib import Path
+
+import tensorflow as tf
+from utils_edm import (
+    X_FEATURES_CH,
+    X_FEATURES_TRK,
+    Y_FEATURES,
+    generate_examples,
+    split_sample_several,
+)
+
+import tensorflow_datasets as tfds
+
+_DESCRIPTION = """
+CLIC EDM4HEP dataset with single pi- with raw hits
+"""
+
+_CITATION = """
+"""
+
+
+class ClicEdmSinglePiHitsPf(tfds.core.GeneratorBasedBuilder):
+    VERSION = tfds.core.Version("1.2.0")
+    RELEASE_NOTES = {
+        "1.1.0": "Remove track referencepoint feature",
+        "1.2.0": "Keep all interacting genparticles",
+    }
+    MANUAL_DOWNLOAD_INSTRUCTIONS = """
+    FIXME
+    """
+
+    def _info(self) -> tfds.core.DatasetInfo:
+        """Returns the dataset metadata."""
+        return tfds.core.DatasetInfo(
+            builder=self,
+            description=_DESCRIPTION,
+            features=tfds.features.FeaturesDict(
+                {
+                    "X": tfds.features.Tensor(
+                        shape=(
+                            None,
+                            max(len(X_FEATURES_TRK), len(X_FEATURES_CH)),
+                        ),
+                        dtype=tf.float32,
+                    ),
+                    "ygen": tfds.features.Tensor(shape=(None, len(Y_FEATURES)), dtype=tf.float32),
+                    "ycand": tfds.features.Tensor(shape=(None, len(Y_FEATURES)), dtype=tf.float32),
+                }
+            ),
+            supervised_keys=None,
+            homepage="",
+            citation=_CITATION,
+            metadata=tfds.core.MetadataDict(
+                x_features_track=X_FEATURES_TRK,
+                x_features_calohit=X_FEATURES_CH,
+                y_features=Y_FEATURES,
+            ),
+        )
+
+    def _split_generators(self, dl_manager: tfds.download.DownloadManager):
+        path = dl_manager.manual_dir
+        return split_sample_several([
+            Path(path / "pi-/"), Path(path / "pi+/")
+        ])
+
+    def _generate_examples(self, files):
+        return generate_examples(files)
---
+++ scripts/tallinn/mlpf-train-a100-mir2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --partition gpu
+#SBATCH --gres gpu:a100:2
+#SBATCH --mem-per-gpu 40G
+#SBATCH -o logs/slurm-%x-%j-%N.out
+
+IMG=/home/software/singularity/tf-2.13.0.simg
+cd ~/particleflow
+
+#TF training
+singularity exec -B /scratch/persistent --nv \
+    --env PYTHONPATH=hep_tfds \
+    --env TFDS_DATA_DIR=/scratch/persistent/joosep/tensorflow_datasets \
+    $IMG python mlpf/pipeline.py train -c parameters/clic-test.yaml \
+    --plot-freq 0 --num-cpus 32 --batch-multiplier 5 \
+    --ntrain 50000 --ntest 50000 --nepochs 11 --benchmark_dir exp_dir
+
+#    --env TF_GPU_THREAD_MODE=gpu_private \
+#    --env TF_GPU_THREAD_COUNT=8 \
+#    --env TF_XLA_FLAGS="--tf_xla_auto_jit=2" \
