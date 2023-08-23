660d280ace052a18ce386d5ccab50ac2684e1d08
Joosep Pata <joosep.pata@cern.ch>
2023-08-23 13:36:00 +0300

    fix horovod training

--- scripts/tallinn/mlpf-train-a100.sh
+++ scripts/tallinn/mlpf-train-a100.sh
@@ -12,8 +12,7 @@ singularity exec -B /scratch/persistent --nv \
     --env PYTHONPATH=hep_tfds \
     --env TFDS_DATA_DIR=/scratch/persistent/joosep/tensorflow_datasets \
     $IMG python mlpf/pipeline.py train -c parameters/clic-hits.yaml \
-    --plot-freq 1 --num-cpus 32 --batch-multiplier 2 \
-    --weights experiments/clic-hits_20230804_231819_093246.gpu1.local/weights/weights-04-0.195574.hdf5
+    --plot-freq 1 --num-cpus 32 --batch-multiplier 2
 
 #    --env TF_GPU_THREAD_MODE=gpu_private \
 #    --env TF_GPU_THREAD_COUNT=8 \
