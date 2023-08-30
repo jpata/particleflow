87435cc0e894e35be1bc3f9d485d34dd4dea1571
Joosep Pata <joosep.pata@kbfi.ee>
2023-08-30 14:41:24 +0300

    new test with lumi scaling

--- scripts/lumi/train-gpu-3.sh
+++ scripts/lumi/train-gpu-3.sh
@@ -31,10 +31,10 @@ singularity exec \
     --rocm \
     -B /scratch/project_465000301 \
     -B /tmp \
-    --env LD_LIBRARY_PATH=/opt/rocm-5.4.0/lib/ \
+    --env LD_LIBRARY_PATH=/opt/rocm/lib/ \
     $IMG python3 mlpf/pipeline.py train \
     --config parameters/clic-test.yaml --plot-freq 1 --num-cpus 8 \
-    --batch-multiplier 5 --nepochs 5 --ntrain 50000 --ntest 50000 --benchmark_dir exp_dir
+    --batch-multiplier 5 --ntrain 50000 --ntest 50000 --benchmark_dir exp_dir
 
 #    --env MIOPEN_USER_DB_PATH=$MIPEN_USER_DB_PATH \
 #    --env MIOPEN_CUSTOM_CACHE_DIR=$MIOPEN_CUSTOM_CACHE_DIR \
