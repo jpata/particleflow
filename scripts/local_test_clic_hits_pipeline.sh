e2f3552fd0c9d38a3c6503a8c0df90e81dcd56df
Joosep Pata <joosep.pata@gmail.com>
2023-09-21 15:20:27 +0300

    up

--- scripts/local_test_clic_hits_pipeline.sh
+++ scripts/local_test_clic_hits_pipeline.sh
@@ -9,12 +9,12 @@ cd data/p8_ee_tt_ecm380
 
 #download some test data
 wget -q --no-check-certificate -nc https://jpata.web.cern.ch/jpata/clic_edm4hep_2023_02_27/p8_ee_tt_ecm380/reco_p8_ee_tt_ecm380_1.root
-# wget -q --no-check-certificate -nc https://jpata.web.cern.ch/jpata/clic_edm4hep_2023_02_27/p8_ee_tt_ecm380/reco_p8_ee_tt_ecm380_2.root
+wget -q --no-check-certificate -nc https://jpata.web.cern.ch/jpata/clic_edm4hep_2023_02_27/p8_ee_tt_ecm380/reco_p8_ee_tt_ecm380_2.root
 
 cd ../..
 
 python3 scripts/fcc/postprocessing_hits.py data/p8_ee_tt_ecm380/reco_p8_ee_tt_ecm380_1.root data/p8_ee_tt_ecm380/reco_p8_ee_tt_ecm380_1.parquet
-# python3 scripts/fcc/postprocessing_hits.py data/p8_ee_tt_ecm380/reco_p8_ee_tt_ecm380_2.root data/p8_ee_tt_ecm380/reco_p8_ee_tt_ecm380_2.parquet
+python3 scripts/fcc/postprocessing_hits.py data/p8_ee_tt_ecm380/reco_p8_ee_tt_ecm380_2.root data/p8_ee_tt_ecm380/reco_p8_ee_tt_ecm380_2.parquet
 
 tfds build mlpf/heptfds/clic_pf_edm4hep_hits/ttbar --manual_dir data
 
