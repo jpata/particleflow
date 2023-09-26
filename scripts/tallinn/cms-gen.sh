aca57acc6ba945a4904bb546da01bf658ea5740e
Joosep Pata <joosep.pata@cern.ch>
2023-09-26 19:20:18 +0300

    up

--- scripts/tallinn/cms-gen.sh
+++ scripts/tallinn/cms-gen.sh
@@ -11,4 +11,4 @@ cd ~/particleflow
 singularity exec -B /scratch/persistent --nv \
     --env PYTHONPATH=hep_tfds \
     --env TFDS_DATA_DIR=/scratch/persistent/joosep/tensorflow_datasets \
-    $IMG python mlpf/pipeline.py train -c parameters/cms-gen.yaml --plot-freq 1 --num-cpus 32 --batch-multiplier 2 --ntrain 10000 --ntest 10000
+    $IMG python mlpf/pipeline.py train -c parameters/cms-gen.yaml --plot-freq 1 --num-cpus 32 --batch-multiplier 2
