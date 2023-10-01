969e8b4e7bdf4eba7f67965d240b2deb45413f99
Farouk <farouk.mokhtar@gmail.com>
2023-10-01 18:21:02 +0200

    up

--- mlpf/pyg_pipeline.py
+++ mlpf/pyg_pipeline.py
@@ -114,6 +114,7 @@ def main():
             _logger.info(f"Will use single-gpu: {torch.cuda.get_device_name(0)}", color="purple")
 
         # CPU
+        print(device)
         if device == "cpu":
             _logger.info("Will use cpu", color="purple")
 
