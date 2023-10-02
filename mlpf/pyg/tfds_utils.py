d3c04ce15e76d996a82c727eec1b9caabd028844
Farouk <farouk.mokhtar@gmail.com>
2023-10-02 17:04:03 +0200

    up

--- mlpf/pyg/tfds_utils.py
+++ mlpf/pyg/tfds_utils.py
@@ -3,11 +3,67 @@ from typing import List, Optional, Sequence, Union
 import tensorflow_datasets as tfds
 import torch
 import torch.utils.data
-import torch_geometric
 from torch import Tensor
-from torch_geometric.data import Batch, Data
+from torch_geometric.data import Batch, Data, Dataset
 from torch_geometric.data.data import BaseData
 from torch_geometric.data.datapipes import DatasetAdapter
+
+
+class DataLoader(torch.utils.data.DataLoader):
+    """
+    Copied from https://pytorch-geometric.readthedocs.io/en/latest/_modules/torch_geometric/loader/dataloader.html#DataLoader
+    because we need to implement our own Collater class (see below)
+    """
+
+    def __init__(
+        self,
+        dataset: Union[Dataset, Sequence[BaseData], DatasetAdapter],
+        batch_size: int = 1,
+        shuffle: bool = False,
+        follow_batch: Optional[List[str]] = None,
+        exclude_keys: Optional[List[str]] = None,
+        **kwargs,
+    ):
+        # Remove for PyTorch Lightning:
+        kwargs.pop("collate_fn", None)
+
+        # Save for PyTorch Lightning < 1.6:
+        self.follow_batch = follow_batch
+        self.exclude_keys = exclude_keys
+
+        super().__init__(
+            dataset,
+            batch_size,
+            shuffle,
+            collate_fn=Collater(follow_batch, exclude_keys),
+            **kwargs,
+        )
+
+
+class Collater:
+    """Based on the Collater found on torch_geometric docs we build our own"""
+
+    def __init__(self, follow_batch=None, exclude_keys=None):
+        self.follow_batch = follow_batch
+        self.exclude_keys = exclude_keys
+
+    def __call__(self, inputs):
+        num_samples_in_batch = len(inputs)
+        elem_keys = list(inputs[0].keys())
+
+        batch = []
+        for ev in range(num_samples_in_batch):
+            batch.append(Data())
+            for elem_key in elem_keys:
+                batch[ev][elem_key] = Tensor(inputs[ev][elem_key])
+            batch[ev]["batch"] = torch.tensor([ev] * len(inputs[ev][elem_key]))
+
+        elem = batch[0]
+
+        if isinstance(elem, BaseData):
+            return Batch.from_data_list(batch, self.follow_batch, self.exclude_keys)
+
+        raise TypeError(f"DataLoader found invalid type: {type(elem)}")
 
 
 class Dataset:
@@ -56,63 +112,6 @@ class Dataset:
 
     def __repr__(self):
         return self.ds.__repr__()
-
-
-class DataLoader(torch.utils.data.DataLoader):
-    """
-    Copied from https://pytorch-geometric.readthedocs.io/en/latest/_modules/torch_geometric/loader/dataloader.html#DataLoader
-    because we need to implement our own Collater class (see below)
-    """
-
-    def __init__(
-        self,
-        dataset: Union[torch_geometric.data.Dataset, Sequence[BaseData], DatasetAdapter],
-        batch_size: int = 1,
-        shuffle: bool = False,
-        follow_batch: Optional[List[str]] = None,
-        exclude_keys: Optional[List[str]] = None,
-        **kwargs,
-    ):
-        # Remove for PyTorch Lightning:
-        kwargs.pop("collate_fn", None)
-
-        # Save for PyTorch Lightning < 1.6:
-        self.follow_batch = follow_batch
-        self.exclude_keys = exclude_keys
-
-        super().__init__(
-            dataset,
-            batch_size,
-            shuffle,
-            collate_fn=Collater(follow_batch, exclude_keys),
-            **kwargs,
-        )
-
-
-class Collater:
-    """Based on the Collater found on torch_geometric docs we build our own"""
-
-    def __init__(self, follow_batch=None, exclude_keys=None):
-        self.follow_batch = follow_batch
-        self.exclude_keys = exclude_keys
-
-    def __call__(self, inputs):
-        num_samples_in_batch = len(inputs)
-        elem_keys = list(inputs[0].keys())
-
-        batch = []
-        for ev in range(num_samples_in_batch):
-            batch.append(Data())
-            for elem_key in elem_keys:
-                batch[ev][elem_key] = Tensor(inputs[ev][elem_key])
-            batch[ev]["batch"] = torch.tensor([ev] * len(inputs[ev][elem_key]))
-
-        elem = batch[0]
-
-        if isinstance(elem, BaseData):
-            return Batch.from_data_list(batch, self.follow_batch, self.exclude_keys)
-
-        raise TypeError(f"DataLoader found invalid type: {type(elem)}")
 
 
 class InterleavedIterator(object):
