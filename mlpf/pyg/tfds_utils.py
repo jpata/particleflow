332f35cef9537911471012fa514bb113c243681a
Farouk <farouk.mokhtar@gmail.com>
2023-10-02 17:14:33 +0200

    up

--- mlpf/pyg/tfds_utils.py
+++ mlpf/pyg/tfds_utils.py
@@ -1,90 +1,7 @@
-# from typing import List, Optional, Sequence, Union
-
-# import tensorflow_datasets as tfds
-# import torch
-# import torch.utils.data
-# from torch import Tensor
-# from torch_geometric.data import Batch, Data, Dataset
-# from torch_geometric.data.data import BaseData
-# from torch_geometric.data.datapipes import DatasetAdapter
-
-
-# class Dataset:
-#     def __init__(self, name="clic_edm_ttbar_pf:1.5.0", split="train"):
-#         builder = tfds.builder(name, data_dir="/pfvol/tensorflow_datasets/clic/clusters/")
-
-#         self.ds = builder.as_data_source(split=split)
-
-#         # to prevent a warning from tfds about accessing sequences of indices
-#         self.ds.__class__.__getitems__ = self.my_getitem
-
-#     def my_getitem(self, vals):
-#         records = self.data_source.__getitems__(vals)
-#         return [self.dataset_info.features.deserialize_example_np(record, decoders=self.decoders) for record in records]
-
-#     def get_sampler(self):
-#         sampler = torch.utils.data.RandomSampler(self.ds)
-#         return sampler
-
-#     def get_distributed_sampler(self):
-#         sampler = torch.utils.data.distributed.DistributedSampler(self.ds)
-#         return sampler
-
-#     def get_loader(self, world_size, batch_size=20, num_workers=2, prefetch_factor=4):
-#         if world_size > 1:
-#             return DataLoader(
-#                 self.ds,
-#                 batch_size=batch_size,
-#                 collate_fn=Collater(),
-#                 sampler=self.get_distributed_sampler(),
-#                 num_workers=num_workers,
-#                 prefetch_factor=prefetch_factor,
-#             )
-#         else:
-#             return DataLoader(
-#                 self.ds,
-#                 batch_size=batch_size,
-#                 collate_fn=Collater(),
-#                 sampler=self.get_sampler(),
-#                 num_workers=num_workers,
-#                 prefetch_factor=prefetch_factor,
-#             )
-
-#     def __len__(self):
-#         return len(self.ds)
-
-#     def __repr__(self):
-#         return self.ds.__repr__()
-
-
-# class InterleavedIterator(object):
-#     def __init__(self, data_loaders):
-#         self.idx = 0
-#         self.data_loaders_iter = [iter(dl) for dl in data_loaders]
-#         max_loader_size = max([len(dl) for dl in data_loaders])
-
-#         # interleave loaders of different length
-#         self.loader_ds_indices = []
-#         for i in range(max_loader_size):
-#             for iloader, loader in enumerate(data_loaders):
-#                 if i < len(loader):
-#                     self.loader_ds_indices.append(iloader)
-
-#         self.cur_index = 0
-
-#     def __iter__(self):
-#         return self
-
-#     def __next__(self):
-#         iloader = self.loader_ds_indices[self.cur_index]
-#         self.cur_index += 1
-#         return next(self.data_loaders_iter[iloader])
-
 from typing import List, Optional, Sequence, Union
 
 import tensorflow_datasets as tfds
 import torch
-import torch.nn as nn
 import torch.utils.data
 from torch import Tensor
 from torch_geometric.data import Batch, Data, Dataset
@@ -156,25 +73,35 @@ class Dataset:
         self.ds = builder.as_data_source(split=split)
 
         # to prevent a warning from tfds about accessing sequences of indices
-        self.ds.__class__.__getitems__ = self.my_getitem
-
-    def my_getitem(self, vals):
-        records = self.data_source.__getitems__(vals)
-        return [self.dataset_info.features.deserialize_example_np(record, decoders=self.decoders) for record in records]
+        self.ds.__class__.__getitems__ = my_getitem
 
     def get_sampler(self):
-        sampler = torch.utils.data.SequentialSampler(self.ds)
+        sampler = torch.utils.data.RandomSampler(self.ds)
         return sampler
 
-    def get_loader(self, batch_size=20, num_workers=2, prefetch_factor=4):
-        return DataLoader(
-            self.ds,
-            batch_size=batch_size,
-            collate_fn=Collater(),
-            sampler=self.get_sampler(),
-            num_workers=num_workers,
-            prefetch_factor=prefetch_factor,
-        )
+    def get_distributed_sampler(self):
+        sampler = torch.utils.data.distributed.DistributedSampler(self.ds)
+        return sampler
+
+    def get_loader(self, batch_size, world_size, num_workers=2, prefetch_factor=4):
+        if world_size > 1:
+            return DataLoader(
+                self.ds,
+                batch_size=batch_size,
+                collate_fn=Collater(),
+                sampler=self.get_distributed_sampler(),
+                num_workers=num_workers,
+                prefetch_factor=prefetch_factor,
+            )
+        else:
+            return DataLoader(
+                self.ds,
+                batch_size=batch_size,
+                collate_fn=Collater(),
+                sampler=self.get_sampler(),
+                num_workers=num_workers,
+                prefetch_factor=prefetch_factor,
+            )
 
     def __len__(self):
         return len(self.ds)
@@ -183,18 +110,9 @@ class Dataset:
         return self.ds.__repr__()
 
 
-def collate_padded_batch(inputs):
-    # num_samples_in_batch = len(inputs)
-    elem_keys = list(inputs[0].keys())
-    ret = {}
-    for elem_key in elem_keys:
-        batch = [Tensor(i[elem_key]) for i in inputs]
-        max_seq_len = max([x.shape[0] for x in batch])
-        padded_batch_data = [nn.functional.pad(x, (0, 0, 0, max_seq_len - x.shape[0])) for x in batch]
-        ret[elem_key] = torch.stack(padded_batch_data, axis=0)
-    ret["mask"] = ret["X"][:, :, 0] == 0
-
-    return ret
+def my_getitem(self, vals):
+    records = self.data_source.__getitems__(vals)
+    return [self.dataset_info.features.deserialize_example_np(record, decoders=self.decoders) for record in records]
 
 
 class InterleavedIterator(object):
