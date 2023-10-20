1591a2ce041babd9dc2a5beaf09ee4a3797e4237
Eric Wulff <eric.wulff@cern.ch>
2023-10-20 09:10:21 -0400

    wip: implement HPO in pytorch pipeline

--- mlpf/pyg_pipeline.py
+++ mlpf/pyg_pipeline.py
@@ -5,12 +5,14 @@ Author: Farouk Mokhtar
 """
 
 import argparse
+from datetime import datetime
+from functools import partial
 import logging
 import os
 import os.path as osp
 import pickle as pkl
 from pathlib import Path
-
+import shutil
 import yaml
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
@@ -26,35 +28,42 @@ from pyg.training import train_mlpf
 from pyg.utils import CLASS_LABELS, X_FEATURES, InterleavedIterator, PFDataset, save_HPs
 from utils import create_experiment_dir
 
+
 logging.basicConfig(level=logging.INFO)
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--config", type=str, default="parameters/pyg-cms.yaml", help="yaml config")
-parser.add_argument("--prefix", type=str, default="test_", help="prefix appended to result dir name")
-parser.add_argument("--data_dir", type=str, default="/pfvol/tensorflow_datasets/", help="path to `tensorflow_datasets/`")
-parser.add_argument("--gpus", type=str, default="0", help="to use CPU set to empty string; else e.g., `0,1`")
+# add default=None to all arparse arguments to ensure they do not override
+# values loaded from the config file given by --config unless explicitly given
+parser.add_argument("--config", type=str, default=None, help="yaml config")
+parser.add_argument("--prefix", type=str, default=None, help="prefix appended to result dir name")
+parser.add_argument("--data_dir", type=str, default=None, help="path to `tensorflow_datasets/`")
+parser.add_argument("--gpus", type=str, default=None, help="to use CPU set to empty string; else e.g., `0,1`")
 parser.add_argument(
-    "--gpu-batch-multiplier", type=int, default=1, help="increase batch size per GPU by this constant factor"
+    "--gpu_batch_multiplier", type=int, default=None, help="Increase batch size per GPU by this constant factor"
 )
-parser.add_argument("--dataset", type=str, choices=["clic", "cms", "delphes"], required=True, help="which dataset?")
+parser.add_argument("--dataset", type=str, default=None, choices=["clic", "cms", "delphes"], required=False, help="which dataset?")
+parser.add_argument("--load", type=str, default=None, help="dir from which to load a saved model")
+parser.add_argument("--train", action="store_true", default=None, help="initiates a training")
+parser.add_argument("--test", action="store_true", default=None, help="tests the model")
+parser.add_argument("--num_epochs", type=int, default=None, help="number of training epochs")
+parser.add_argument("--patience", type=int, default=None, help="patience before early stopping")
+parser.add_argument("--lr", type=float, default=None, help="learning rate")
+parser.add_argument("--conv_type", type=str, default=None, help="choices are ['gnn_lsh', 'gravnet', 'attention']")
+parser.add_argument("--make_plots", action="store_true", default=None, help="make plots of the test predictions")
+parser.add_argument("--export_onnx", action="store_true", default=None, help="exports the model to onnx")
 parser.add_argument("--ntrain", type=int, default=None, help="training samples to use, if None use entire dataset")
-parser.add_argument("--ntest", type=int, default=None, help="testing samples to use, if None use entire dataset")
+parser.add_argument("--ntest", type=int, default=None, help="training samples to use, if None use entire dataset")
 parser.add_argument("--nvalid", type=int, default=500, help="validation samples to use, default will use 500 events")
 parser.add_argument("--num-workers", type=int, default=None, help="number of processes to load the data")
 parser.add_argument("--prefetch-factor", type=int, default=2, help="number of samples to fetch & prefetch at every call")
-parser.add_argument("--load", type=str, default=None, help="dir from which to load a saved model")
-parser.add_argument("--train", action="store_true", help="initiates a training")
-parser.add_argument("--test", action="store_true", help="tests the model")
-parser.add_argument("--num-epochs", type=int, default=3, help="number of training epochs")
-parser.add_argument("--patience", type=int, default=20, help="patience before early stopping")
-parser.add_argument("--lr", type=float, default=1e-4, help="learning rate")
-parser.add_argument("--conv-type", type=str, default="gravnet", help="choices are ['gnn-lsh', 'gravnet', 'attention']")
-parser.add_argument("--make-plots", action="store_true", help="make plots of the test predictions")
-parser.add_argument("--export-onnx", action="store_true", help="exports the model to onnx")
+parser.add_argument("--hpo", type=str, default=None, help="perform hyperparameter optimization, name of HPO experiment")
+parser.add_argument("--local", action="store_true", default=None, help="perform HPO locally, without a Ray cluster")
+parser.add_argument("--ray_cpus", type=int, default=None, help="CPUs per trial for HPO")
+parser.add_argument("--ray_gpus", type=int, default=None, help="GPUs per trial for HPO")
 
 
-def run(rank, world_size, args, outdir, logfile):
+def run(rank, world_size, config, args, outdir, logfile):
     """Demo function that will be passed to each gpu if (world_size > 1) else will run normally on the given device."""
 
     if world_size > 1:
@@ -65,11 +74,9 @@ def run(rank, world_size, args, outdir, logfile):
     if (rank == 0) or (rank == "cpu"):  # keep writing the logs
         _configLogger("mlpf", filename=logfile)
 
-    with open(args.config, "r") as stream:  # load config (includes: which physics samples, model params)
-        config = yaml.safe_load(stream)
+    if config["load"]:  # load a pre-trained model
+        outdir = config["load"]  # in case both --load and --train are provided
 
-    if args.load:  # load a pre-trained model
-        outdir = args.load  # in case both --load and --train are provided
         with open(f"{outdir}/model_kwargs.pkl", "rb") as f:
             model_kwargs = pkl.load(f)
 
@@ -89,12 +96,12 @@ def run(rank, world_size, args, outdir, logfile):
 
     else:  # instantiate a new model in the outdir created
         model_kwargs = {
-            "input_dim": len(X_FEATURES[args.dataset]),
-            "num_classes": len(CLASS_LABELS[args.dataset]),
-            **config["model"][args.conv_type],
+            "input_dim": len(X_FEATURES[config["dataset"]]),
+            "num_classes": len(CLASS_LABELS[config["dataset"]]),
+            **config["model"][config["conv_type"]],
         }
         model = MLPF(**model_kwargs)
-        optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
+        optimizer = torch.optim.AdamW(model.parameters(), lr=config["lr"])
 
     model.to(rank)
 
@@ -107,19 +114,22 @@ def run(rank, world_size, args, outdir, logfile):
 
     if args.train:
         if (rank == 0) or (rank == "cpu"):
+            if args.hpo:
+                from ray import train as ray_train
+                outdir = ray_train.get_context().get_trial_dir()  #TODO: EW, check if this is needed
             save_HPs(args, model, model_kwargs, outdir)  # save model_kwargs and hyperparameters
-            _logger.info(f"Creating experiment dir {outdir}")
+            _logger.info("Creating experiment dir {}".format(outdir))
             _logger.info(f"Model directory {outdir}", color="bold")
 
         train_loaders = []
-        for sample in config["train_dataset"][args.dataset]:
-            version = config["train_dataset"][args.dataset][sample]["version"]
-            batch_size = config["train_dataset"][args.dataset][sample]["batch_size"] * args.gpu_batch_multiplier
+        for sample in config["train_dataset"][config["dataset"]]:
+            version = config["train_dataset"][config["dataset"]][sample]["version"]
+            batch_size = config["train_dataset"][config["dataset"]][sample]["batch_size"] * config["gpu_batch_multiplier"]
 
-            ds = PFDataset(args.data_dir, f"{sample}:{version}", "train", ["X", "ygen"], num_samples=args.ntrain)
+            ds = PFDataset(config["data_dir"], f"{sample}:{version}", "train", ["X", "ygen"], num_samples=config["ntrain"])
             _logger.info(f"train_dataset: {ds}, {len(ds)}", color="blue")
 
-            train_loaders.append(ds.get_loader(batch_size, world_size, args.num_workers, args.prefetch_factor))
+            train_loaders.append(ds.get_loader(batch_size, world_size, args.num_workers, args.prefetch_factor))  # TODO: change to use config
 
         train_loader = InterleavedIterator(train_loaders)
 
@@ -145,25 +155,26 @@ def run(rank, world_size, args, outdir, logfile):
             optimizer,
             train_loader,
             valid_loader,
-            args.num_epochs,
-            args.patience,
+            config["num_epochs"],
+            config["patience"],
             outdir,
         )
 
     if args.test:
-        if args.load is None:
+
+        if config["load"] is None:
             # if we don't load, we must have a newly trained model
             assert args.train, "Please train a model before testing, or load a model with --load"
             assert outdir is not None, "Error: no outdir to evaluate model from"
         else:
-            outdir = args.load
+            outdir = config["load"]
 
         test_loaders = {}
-        for sample in config["test_dataset"][args.dataset]:
-            version = config["test_dataset"][args.dataset][sample]["version"]
-            batch_size = config["test_dataset"][args.dataset][sample]["batch_size"] * args.gpu_batch_multiplier
+        for sample in config["test_dataset"][config["dataset"]]:
+            version = config["test_dataset"][config["dataset"]][sample]["version"]
+            batch_size = config["test_dataset"][config["dataset"]][sample]["batch_size"] * config["gpu_batch_multiplier"]
 
-            ds = PFDataset(args.data_dir, f"{sample}:{version}", "test", ["X", "ygen", "ycand"], args.ntest)
+            ds = PFDataset(config["data_dir"], f"{sample}:{version}", "test", ["X", "ygen", "ycand"], num_samples=config["ntest"])
             _logger.info(f"test_dataset: {ds}, {len(ds)}", color="blue")
 
             test_loaders[sample] = InterleavedIterator(
@@ -195,10 +206,10 @@ def run(rank, world_size, args, outdir, logfile):
 
     if (rank == 0) or (rank == "cpu"):  # make plots and export to onnx only on a single machine
         if args.make_plots:
-            for sample in config["test_dataset"][args.dataset]:
+            for sample in config["test_dataset"][config["dataset"]]:
                 _logger.info(f"Plotting distributions for {sample}")
 
-                make_plots(outdir, sample, args.dataset)
+                make_plots(outdir, sample, config["dataset"])
 
         if args.export_onnx:
             try:
@@ -226,9 +237,17 @@ def run(rank, world_size, args, outdir, logfile):
         dist.destroy_process_group()
 
 
-def main():
-    args = parser.parse_args()
-    world_size = len(args.gpus.split(","))  # will be 1 for both cpu ("") and single-gpu ("0")
+def override_config(config, args):
+    """override config with values from argparse Namespace"""
+    for arg in vars(args):
+        arg_value = getattr(args, arg)
+        if arg_value is not None:
+            config[arg] = arg_value
+    return config
+
+    
+
+def device_agnostic_run(config, args, world_size):
 
     if args.train:  # create a new outdir when training a model to never overwrite
         outdir = create_experiment_dir(prefix=args.prefix + Path(args.config).stem + "_")
@@ -243,7 +262,7 @@ def main():
 
         os.system(f"cp {args.config} {outdir}/test-config.yaml")
 
-    if args.gpus:
+    if config["gpus"]:
         assert (
             world_size <= torch.cuda.device_count()
         ), f"--gpus is too high (specefied {world_size} gpus but only {torch.cuda.device_count()} gpus are available)"
@@ -256,19 +275,102 @@ def main():
 
             mp.spawn(
                 run,
-                args=(world_size, args, outdir, logfile),
+                args=(world_size, config, args, outdir, logfile),
                 nprocs=world_size,
                 join=True,
             )
         elif world_size == 1:
             rank = 0
             _logger.info(f"Will use single-gpu: {torch.cuda.get_device_name(rank)}", color="purple")
-            run(rank, world_size, args, outdir, logfile)
+            run(rank, world_size, config, args, outdir, logfile)
 
     else:
         rank = "cpu"
         _logger.info("Will use cpu", color="purple")
-        run(rank, world_size, args, outdir, logfile)
+        run(rank, world_size, config, args, outdir, logfile)
+
+
+def main():
+    args = parser.parse_args()
+    world_size = len(args.gpus.split(","))  # will be 1 for both cpu ("") and single-gpu ("0")
+
+    with open(args.config, "r") as stream:  # load config (includes: which physics samples, model params)
+        config = yaml.safe_load(stream)
+
+    # override loaded config with values from command line args
+    config = override_config(config, args)
+
+    if args.hpo:
+        import ray
+        from ray import tune
+        # from ray.tune.logger import TBXLoggerCallback
+        from raytune.pt_search_space import raytune_num_samples, search_space, set_hps_from_search_space
+        from raytune.utils import get_raytune_schedule, get_raytune_search_alg
+
+        name = args.hpo  # name of Ray Tune experiment directory
+
+        os.environ["TUNE_DISABLE_STRICT_METRIC_CHECKING"] = "1"  # don't crash if a metric is missing
+        if isinstance(config["raytune"]["local_dir"], type(None)):
+            raise TypeError("Please specify a local_dir in the raytune section of the config file.")
+        trd = config["raytune"]["local_dir"] + "/tune_result_dir"
+        os.environ["TUNE_RESULT_DIR"] = trd
+
+        expdir = Path(config["raytune"]["local_dir"]) / name
+        expdir.mkdir(parents=True, exist_ok=True)
+        shutil.copy(
+            "mlpf/raytune/search_space.py",
+            str(Path(config["raytune"]["local_dir"]) / name / "search_space.py"),
+        )  # Copy the config file to the train dir for later reference
+        shutil.copy(
+            args.config,
+            str(Path(config["raytune"]["local_dir"]) / name / "config.yaml"),
+        )  # Copy the config file to the train dir for later reference
+
+        if not args.local:
+            ray.init(address="auto")
+
+        sched = get_raytune_schedule(config["raytune"])
+        search_alg = get_raytune_search_alg(config["raytune"])
+
+        def hpo(search_space, config, args, world_size):
+            config = set_hps_from_search_space(search_space, config)
+            device_agnostic_run(config, args, world_size)
+
+        start = datetime.now()
+        analysis = tune.run(
+            partial(
+                hpo,
+                config=config,
+                args=args,
+                world_size=world_size,
+            ),
+            config=search_space,
+            resources_per_trial={"cpu": args.ray_cpus, "gpu": args.ray_gpus},
+            name=name,
+            scheduler=sched,
+            search_alg=search_alg,
+            num_samples=raytune_num_samples,
+            local_dir=config["raytune"]["local_dir"],
+            # callbacks=[TBXLoggerCallback()],
+            log_to_file=True,
+            resume=False,  # TODO: make this configurable
+            max_failures=2,
+            # sync_config=sync_config,
+        )
+        end = datetime.now()
+        logging.info("Total time of tune.run(...): {}".format(end - start))
+
+        logging.info(
+            "Best hyperparameters found according to {} were: ".format(config["raytune"]["default_metric"]),
+            analysis.get_best_config(
+                metric=config["raytune"]["default_metric"],
+                mode=config["raytune"]["default_mode"],
+                scope='all',
+            ),
+        )
+
+    else:
+        device_agnostic_run(config, args, world_size)
 
 
 if __name__ == "__main__":
---
+++ mlpf/raytune/pt_search_space.py
@@ -0,0 +1,49 @@
+from ray.tune import choice  # grid_search, choice, loguniform, quniform
+
+raytune_num_samples = 4  # Number of random samples to draw from search space. Set to 1 for grid search.
+samp = choice
+
+# gnn scan
+search_space = {
+    # optimizer parameters
+    "lr": samp([1e-4, 1e-3, 1e-2]),
+    # "gpu_batch_multiplier": samp([10, 20, 40]),
+    # model arch parameters
+    "conv_type": samp(["gnn_lsh"]),
+    "embedding_dim": samp([252, 512]),
+    # "width": samp([512]),
+    # "num_convs": samp([3]),
+    # "dropout": samp([0.0]),
+    # "patience": samp([20])
+}
+
+
+def set_hps_from_search_space(search_space, config):
+    if "lr" in search_space.keys():
+        config["lr"] = search_space["lr"]
+    
+    if "gpu_batch_multiplier" in search_space.keys():
+        config["gpu_batch_multiplier"] = search_space["gpu_batch_multiplier"]
+
+    if "conv_type" in search_space.keys():
+        conv_type = search_space["conv_type"]
+        config["conv_type"] = conv_type
+        
+        if conv_type == "gnn_lsh" or conv_type == "transformer":
+            if "embedding_dim" in search_space.keys():
+                config["model"][conv_type]["embedding_dim"] = search_space["embedding_dim"]
+
+            if "width" in search_space.keys():
+                config["model"][conv_type]["width"] = search_space["width"]
+
+            if "num_convs" in search_space.keys():
+                config["model"][conv_type]["num_convs"] = search_space["num_convs"]
+
+            if "num_convs" in search_space.keys():
+                config["model"][conv_type]["num_convs"] = search_space["num_convs"]
+
+
+    if "embedding_dim" in search_space.keys():
+        config["embedding_dim"] = search_space["embedding_dim"]
+
+    return config
