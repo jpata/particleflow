85dbaf5b5c395c0722aa5407992aad47a98ee6bf
Farouk <farouk.mokhtar@gmail.com>
2023-10-20 15:13:24 +0200

    up

--- mlpf/pyg/training.py
+++ mlpf/pyg/training.py
@@ -272,13 +272,12 @@ def train(
                         + f"best_val_loss={best_val_loss:.2f} "
                         + f"stale={stale_epochs} "
                     )
-                    stale_epochs += 55  # TODO remove
+
                 model.train()  # prepare for next training loop
 
             if world_size > 1:
                 dist.barrier()  # wait until validation run on rank 0 is finished before going to the next epoch
                 dist.broadcast(stale_epochs, src=0)  # broadcast stale_epochs to all gpus
-                print("rank", rank, "stale_epochs", stale_epochs)
 
             if stale_epochs > patience:
                 _logger.info("breaking due to stale epochs")
