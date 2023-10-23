6edec2b949ed818cbacdfcc2fddf83456ea7e28e
Farouk <farouk.mokhtar@gmail.com>
2023-10-23 17:04:11 +0200

    up

--- mlpf/pyg/PFDataset.py
+++ mlpf/pyg/PFDataset.py
@@ -19,6 +19,7 @@ class PFDataset:
 
         builder = tfds.builder(name, data_dir=data_dir)
         self.ds = builder.as_data_source(split=split)
+        del self.ds.dataset_info
 
         self.keys_to_get = keys_to_get
 
@@ -85,17 +86,17 @@ class PFDataset:
     def __repr__(self):
         return self.ds.__repr__()
 
-    def __getstate__(self):
-        state = self.ds.__dict__.copy()
-        # remove unpicklable entries
-        self.dataset_info = state["dataset_info"]
-        del state["dataset_info"]
-        return state
+    # def __getstate__(self):
+    #     state = self.ds.__dict__.copy()
+    #     # remove unpicklable entries
+    #     self.dataset_info = state["dataset_info"]
+    #     del state["dataset_info"]
+    #     return state
 
-    def __setstate__(self, state):
-        """Used for deserializing"""
-        # restore the state which was picklable
-        self.__dict__.update(state)
+    # def __setstate__(self, state):
+    #     """Used for deserializing"""
+    #     # restore the state which was picklable
+    #     self.__dict__.update(state)
 
 
 class DataLoader(torch.utils.data.DataLoader):
