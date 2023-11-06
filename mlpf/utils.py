b5304b71a3f7dc05815669e36796f7042f12a614
Eric Wulff <eric.wulff@cern.ch>
2023-11-06 08:47:07 -0500

    add CometML logging to pytorch training pipeline

--- mlpf/utils.py
+++ mlpf/utils.py
@@ -1,6 +1,8 @@
 import datetime
+import logging
 import platform
 from pathlib import Path
+from comet_ml import OfflineExperiment, Experiment  # isort:skip
 
 
 def create_experiment_dir(prefix=None, suffix=None):
@@ -15,3 +17,38 @@ def create_experiment_dir(prefix=None, suffix=None):
     train_dir.mkdir(parents=True)
 
     return str(train_dir)
+
+
+def create_comet_experiment(comet_exp_name, comet_offline=False, outdir=None):
+    try:
+        if comet_offline:
+            logging.info("Using comet-ml OfflineExperiment, saving logs locally.")
+            if outdir is None:
+                raise ValueError("Please specify am output directory when setting comet_offline to True")
+
+            experiment = OfflineExperiment(
+                project_name=comet_exp_name,
+                auto_metric_logging=True,
+                auto_param_logging=True,
+                auto_histogram_weight_logging=True,
+                auto_histogram_gradient_logging=False,
+                auto_histogram_activation_logging=False,
+                offline_directory=outdir + "/cometml",
+                auto_output_logging="simple",
+            )
+        else:
+            logging.info("Using comet-ml Experiment, streaming logs to www.comet.ml.")
+
+            experiment = Experiment(
+                project_name=comet_exp_name,
+                auto_metric_logging=True,
+                auto_param_logging=True,
+                auto_histogram_weight_logging=True,
+                auto_histogram_gradient_logging=False,
+                auto_histogram_activation_logging=False,
+                auto_output_logging="simple",
+            )
+    except Exception as e:
+        logging.warning("Failed to initialize comet-ml dashboard: {}".format(e))
+        experiment = None
+    return experiment
