85062a170a29cf6fb5bc07c46dc2aa524e89c9e5
Joosep Pata <joosep.pata@cern.ch>
2023-11-06 11:07:49 +0200

    up

--- scripts/tallinn/a100/cms-train.sh
+++ scripts/tallinn/a100/cms-train.sh
@@ -7,7 +7,7 @@
 IMG=/home/software/singularity/tf-2.14.0.simg
 cd ~/particleflow
 
-# export TF_XLA_FLAGS="--tf_xla_auto_jit=2 --tf_xla_cpu_global_jit" 
+# export TF_XLA_FLAGS="--tf_xla_auto_jit=2 --tf_xla_cpu_global_jit"
 
 #TF training
 singularity exec -B /scratch/persistent --nv \
