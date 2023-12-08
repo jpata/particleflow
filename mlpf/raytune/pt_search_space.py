5f8a4db1acbd46284948e1890f578bd19ba98a10
Eric Wulff <eric.wulff@cern.ch>
2023-12-08 07:51:20 -0500

    update raytune search space

--- mlpf/raytune/pt_search_space.py
+++ mlpf/raytune/pt_search_space.py
@@ -9,6 +9,7 @@ search_space = {
     "lr": samp([1e-4, 1e-3, 1e-2]),
     "gpu_batch_multiplier": samp([4]),
     # model arch parameters
+    "activation": samp(["elu"]),
     "conv_type": samp(["gravnet"]),  # can be "gnn_lsh", "gravnet", "attention"
     "embedding_dim": samp([128, 252, 512]),
     "width": samp([256, 512]),
@@ -16,44 +17,52 @@ search_space = {
     "dropout": samp([0.0]),
     "patience": samp([20]),
     # only for gravnet
-    "gravnet_k": samp([8, 16]),
+    "k": samp([8, 16]),
     "propagate_dimensions": samp([16, 32]),
     "space_dimensions": samp([4]),
+    # only for gnn-lsh
+    "bin_size": samp([640]),
+    "max_num_bins": samp([200]),
+    "distance_dim": samp([128]),
+    "layernorm": samp([True]),
+    "num_node_messages": samp([2]),
+    "ffn_dist_hidden_dim": samp([128]),
 }
 
 
 def set_hps_from_search_space(search_space, config):
-    if "lr" in search_space.keys():
-        config["lr"] = search_space["lr"]
-
-    if "gpu_batch_multiplier" in search_space.keys():
-        config["gpu_batch_multiplier"] = search_space["gpu_batch_multiplier"]
+    varaible_names = ["lr", "gpu_batch_multiplier"]
+    for var in varaible_names:
+        if var in search_space.keys():
+            config[var] = search_space[var]
 
     if "conv_type" in search_space.keys():
         conv_type = search_space["conv_type"]
         config["conv_type"] = conv_type
 
+        common_varaible_names = ["embedding_dim", "width", "num_convs", "activation"]
         if conv_type == "gnn_lsh" or conv_type == "gravnet" or conv_type == "attention":
-            if "embedding_dim" in search_space.keys():
-                config["model"][conv_type]["embedding_dim"] = search_space["embedding_dim"]
+            for var in common_varaible_names:
+                if var in search_space.keys():
+                    config["model"][conv_type][var] = search_space[var]
 
-            if "width" in search_space.keys():
-                config["model"][conv_type]["width"] = search_space["width"]
-
-            if "num_convs" in search_space.keys():
-                config["model"][conv_type]["num_convs"] = search_space["num_convs"]
-
-            if "num_convs" in search_space.keys():
-                config["model"][conv_type]["num_convs"] = search_space["num_convs"]
-
+        gravnet_variable_names = ["k", "propagate_dimensions", "space_dimensions"]
         if conv_type == "gravnet":
-            if "gravnet_k" in search_space.keys():
-                config["model"][conv_type]["k"] = search_space["gravnet_k"]
+            for var in gravnet_variable_names:
+                if var in search_space.keys():
+                    config["model"][conv_type][var] = search_space[var]
 
-            if "propagate_dimensions" in search_space.keys():
-                config["model"][conv_type]["propagate_dimensions"] = search_space["propagate_dimensions"]
-
-            if "space_dimensions" in search_space.keys():
-                config["model"][conv_type]["space_dimensions"] = search_space["space_dimensions"]
+        gnn_lsh_varaible_names = [
+            "bin_size",
+            "max_num_bins",
+            "distance_dim",
+            "layernorm",
+            "num_node_messages",
+            "ffn_dist_hidden_dim",
+        ]
+        if conv_type == "gnn_lsh":
+            for var in gnn_lsh_varaible_names:
+                if var in search_space.keys():
+                    config["model"][conv_type][var] = search_space[var]
 
     return config
