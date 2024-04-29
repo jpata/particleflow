57aeaa5357871b96298bcb9bed993b56862e618d
Farouk <farouk.mokhtar@gmail.com>
2024-04-29 15:43:58 +0200

    up MET training v1

---
+++ mlpf/met_finetuning_pipeline.py
@@ -0,0 +1,237 @@
+"""
+Developing a PyTorch Geometric supervised training of MLPF using DistributedDataParallel.
+
+Authors: Farouk Mokhtar, Joosep Pata, Eric Wulff
+"""
+
+import argparse
+import logging
+import pickle as pkl
+from pathlib import Path
+
+import torch
+import torch.nn as nn
+import yaml
+from pyg.logger import _configLogger, _logger
+from pyg.mlpf import MLPF
+from pyg.PFDataset import get_interleaved_dataloaders
+from pyg.training_met import override_config, train_mlpf
+from pyg.utils import load_checkpoint, save_HPs
+from utils import create_experiment_dir
+
+logging.basicConfig(level=logging.INFO)
+
+parser = argparse.ArgumentParser()
+
+# add default=None to all arparse arguments to ensure they do not override
+# values loaded from the config file given by --config unless explicitly given
+parser.add_argument("--config", type=str, default=None, help="yaml config")
+parser.add_argument("--prefix", type=str, default=None, help="prefix appended to result dir name")
+parser.add_argument("--data-dir", type=str, default=None, help="path to `tensorflow_datasets/`")
+parser.add_argument("--gpus", type=int, default=None, help="to use CPU set to 0; else e.g., 4")
+parser.add_argument(
+    "--gpu-batch-multiplier", type=int, default=None, help="Increase batch size per GPU by this constant factor"
+)
+parser.add_argument(
+    "--dataset",
+    type=str,
+    default=None,
+    choices=["clic", "cms", "delphes", "clic_hits"],
+    required=False,
+    help="which dataset?",
+)
+parser.add_argument("--num-workers", type=int, default=None, help="number of processes to load the data")
+parser.add_argument("--prefetch-factor", type=int, default=None, help="number of samples to fetch & prefetch at every call")
+parser.add_argument("--load", type=str, default=None, help="load checkpoint and start new training from epoch 1")
+parser.add_argument("--train", action="store_true", default=None, help="initiates a training")
+parser.add_argument("--test", action="store_true", default=None, help="tests the model")
+parser.add_argument("--num-epochs", type=int, default=None, help="number of training epochs")
+parser.add_argument("--patience", type=int, default=None, help="patience before early stopping")
+parser.add_argument("--lr", type=float, default=None, help="learning rate")
+parser.add_argument(
+    "--conv-type",
+    type=str,
+    default=None,
+    help="which graph layer to use",
+    choices=["gravnet", "attention", "gnn_lsh", "mamba"],
+)
+parser.add_argument("--num-convs", type=int, default=None, help="number of convlution (GNN, attention, Mamba) layers")
+parser.add_argument("--make-plots", action="store_true", default=None, help="make plots of the test predictions")
+parser.add_argument("--export-onnx", action="store_true", default=None, help="exports the model to onnx")
+parser.add_argument("--ntrain", type=int, default=None, help="training samples to use, if None use entire dataset")
+parser.add_argument("--ntest", type=int, default=None, help="training samples to use, if None use entire dataset")
+parser.add_argument("--nvalid", type=int, default=None, help="validation samples to use")
+parser.add_argument("--val-freq", type=int, default=None, help="run extra validation every val_freq training steps")
+parser.add_argument("--checkpoint-freq", type=int, default=None, help="epoch frequency for checkpointing")
+parser.add_argument("--in-memory", action="store_true", default=None, help="if True will load the data into memory first")
+parser.add_argument("--numtrain", type=int, default=10000, help="training samples to use")
+parser.add_argument("--numvalid", type=int, default=1000, help="validation samples to use")
+
+parser.add_argument(
+    "--dtype",
+    type=str,
+    default=None,
+    help="data type for training",
+    choices=["float32", "float16", "bfloat16"],
+)
+parser.add_argument(
+    "--attention-type",
+    type=str,
+    default=None,
+    help="attention type for self-attention layer",
+    choices=["math", "efficient", "flash", "flash_external"],
+)
+
+
+parser.add_argument(
+    "--use-latentX", action="store_true", default=None, help="if True will use the latent representations of MLPF"
+)
+
+
+def ffn(input_dim, output_dim, width, act, dropout):
+    return nn.Sequential(
+        nn.Linear(input_dim, width),
+        act(),
+        torch.nn.LayerNorm(width),
+        nn.Dropout(dropout),
+        nn.Linear(width, output_dim),
+    )
+
+
+class DeepMET(nn.Module):
+    def __init__(
+        self,
+        input_dim=14,
+        output_dim=2,
+        width=256,
+        dropout=0,
+    ):
+        super(DeepMET, self).__init__()
+
+        """
+        Takes as input either (1) the MLPF candidates OR (2) the latent representations of the MLPF candidates,
+        and runs an MLP to predict two outputs per candidate: "w_xi" and "w_yi"; which will enter the loss as follows:
+            pred_met_x = sum(w_xi * pxi)
+            pred_met_y = sum(w_yi * pyi)
+
+            LOSS = Huber(true_met_x, pred_met_x) + Huber(true_met_y, pred_met_y)
+
+        Note: default `input_dim` is 14 which stands for "clf_nodes (6) + charge_nodes (3) + regression_nodes (5)"
+        """
+
+        self.act = nn.ELU
+        self.nn = ffn(input_dim, output_dim, width, self.act, dropout)
+
+    # @torch.compile
+    def forward(self, X):
+
+        MET = self.nn(X)
+
+        return MET[:, :, 0], MET[:, :, 1]
+
+
+def main():
+    args = parser.parse_args()
+
+    with open(args.config, "r") as stream:  # load config (includes: which physics samples, model params)
+        config = yaml.safe_load(stream)
+
+    # override loaded config with values from command line args
+    config = override_config(config, args)
+
+    assert config["load"], "Must pass an MLPF model to --load"
+
+    if "best_weights" in Path(config["load"]).name:
+        loaddir = str(Path(config["load"]).parent)
+    else:
+        # the checkpoint is provided directly
+        loaddir = str(Path(config["load"]).parent.parent)
+
+    outdir = create_experiment_dir(
+        prefix=(args.prefix or "") + "_",
+        experiments_dir=loaddir,
+    )
+
+    # Save config for later reference. Note that saving happens after parameters are overwritten by cmd line args.
+    config_filename = "train-config.yaml" if args.train else "test-config.yaml"
+    with open((Path(outdir) / config_filename), "w") as file:
+        yaml.dump(config, file)
+
+    logfile = f"{outdir}/train.log"
+    _configLogger("mlpf", filename=logfile)
+
+    if config["gpus"]:
+        assert torch.cuda.device_count() > 0, "--No gpu available"
+
+        torch.cuda.empty_cache()
+
+        rank = 0
+        _logger.info(f"Will use single-gpu: {torch.cuda.get_device_name(rank)}", color="purple")
+    else:
+        rank = "cpu"
+        _logger.info("Will use cpu", color="purple")
+
+    # load the pre-trained mlpf model
+    _configLogger("mlpf", filename=logfile)
+
+    _logger.info("Will load a pre-trained MLPF model", color="orange")
+
+    with open(f"{loaddir}/model_kwargs.pkl", "rb") as f:
+        mlpf_kwargs = pkl.load(f)
+    _logger.info("mlpf_kwargs: {}".format(mlpf_kwargs))
+
+    mlpf_kwargs["attention_type"] = config["model"]["attention"]["attention_type"]
+
+    mlpf = MLPF(**mlpf_kwargs).to(torch.device(rank))
+    checkpoint = torch.load(config["load"], map_location=torch.device(rank))
+
+    mlpf = load_checkpoint(checkpoint, mlpf)
+    mlpf.eval()
+
+    _logger.info(mlpf)
+
+    if args.use_latentX:  # the dimension (791) will be the same as the input to one of the regression MLPs
+        deepmet_input_dim = mlpf.nn_pt.nn[0].in_features
+    else:
+        deepmet_input_dim = 14
+
+    # define the deepmet model
+    deepmet = DeepMET(input_dim=deepmet_input_dim).to(torch.device(rank))
+    optimizer = torch.optim.AdamW(deepmet.parameters(), lr=args.lr)
+    _logger.info(deepmet)
+
+    if args.train:
+        save_HPs(args, deepmet, mlpf_kwargs, outdir)  # save model_kwargs and hyperparameters
+        _logger.info("Creating experiment dir {}".format(outdir))
+        _logger.info(f"Model directory {outdir}", color="bold")
+
+        loaders = get_interleaved_dataloaders(
+            1,
+            rank,
+            config,
+            use_cuda=rank != "cpu",
+            pad_3d=True,
+            use_ray=False,
+        )
+
+        train_mlpf(
+            rank,
+            deepmet,
+            mlpf,
+            args.use_latentX,
+            optimizer,
+            loaders["train"],
+            loaders["valid"],
+            config["num_epochs"],
+            config["patience"],
+            outdir,
+            trainable=config["model"]["trainable"],
+            checkpoint_freq=config["checkpoint_freq"],
+        )
+
+
+if __name__ == "__main__":
+
+    # e.g.
+    # noqa: python mlpf/met_finetuning_pipeline.py --dataset clic --data-dir tensorflow_datasets --config parameters/pytorch/pyg-clic-ttbar-qq.yaml --gpus 1 --prefix MLPF_test1 --num-epochs 10 --train --load /pfvol/experiments/MLPF_clic_backbone_pyg-clic_20240429_101112_971749/best_weights.pth --gpu-batch-multiplier 100 --num-workers 2 --prefetch-factor 2 --checkpoint-freq 1 --lr 1e-4 --use-latentX
+    main()
---
+++ mlpf/pyg/training_met.py
@@ -0,0 +1,327 @@
+import pickle as pkl
+import time
+from pathlib import Path
+
+import numpy as np
+import torch
+import tqdm
+from pyg.logger import _logger
+from pyg.utils import (
+    get_model_state_dict,
+    save_checkpoint,
+    unpack_predictions,
+    unpack_target,
+)
+from torch.utils.tensorboard import SummaryWriter
+
+# Ignore divide by 0 errors
+np.seterr(divide="ignore", invalid="ignore")
+
+
+def configure_model_trainable(model, trainable, is_training):
+    if is_training:
+        model.train()
+        if trainable != "all":
+            model.eval()
+            for param in model.parameters():
+                param.requires_grad = False
+
+            for layer in trainable:
+                layer = getattr(model, layer)
+                layer.train()
+                for param in layer.parameters():
+                    param.requires_grad = True
+    else:
+        model.eval()
+
+
+def train_and_valid(
+    rank,
+    deepmet,
+    mlpf,
+    use_latentX,
+    optimizer,
+    train_loader,
+    valid_loader,
+    trainable,
+    is_train=True,
+    epoch=None,
+):
+    """
+    Performs training over a given epoch. Will run a validation step every val_freq.
+    """
+
+    train_or_valid = "train" if is_train else "valid"
+    _logger.info(f"Initiating epoch #{epoch} {train_or_valid} run on device rank={rank}", color="red")
+
+    configure_model_trainable(deepmet, trainable, is_train)
+
+    if is_train:
+        data_loader = train_loader
+    else:
+        data_loader = valid_loader
+
+    # only show progress bar on rank 0
+    iterator = tqdm.tqdm(
+        enumerate(data_loader), total=len(data_loader), desc=f"Epoch {epoch} {train_or_valid} loop on rank={rank}"
+    )
+
+    epoch_loss = {}  # this one will keep accumulating `train_loss` and then return the average
+
+    loss = {}  # this one is redefined every iteration
+
+    if use_latentX:  # must set forward hooks to retrieve the intermediate latent representations
+        latent_reps = {}
+
+        def get_latent_reps(name):
+            def hook(mlpf, input, output):
+                latent_reps[name] = output.detach()
+
+            return hook
+
+        mlpf.conv_reg[0].dropout.register_forward_hook(get_latent_reps("conv_reg0"))
+        mlpf.conv_reg[1].dropout.register_forward_hook(get_latent_reps("conv_reg1"))
+        mlpf.conv_reg[2].dropout.register_forward_hook(get_latent_reps("conv_reg2"))
+        mlpf.nn_id.register_forward_hook(get_latent_reps("nn_id"))
+
+    for itrain, batch in iterator:
+
+        batch = batch.to(rank, non_blocking=True)
+        ygen = unpack_target(batch.ygen)
+
+        # run the MLPF model in inference mode to get the MLPF cands / latent representations
+        for i in range(5):
+            with torch.no_grad():
+                with torch.autocast(device_type="cuda", dtype=torch.bfloat16, enabled=True):
+                    ymlpf = mlpf(batch.X, batch.mask)
+        ymlpf = unpack_predictions(ymlpf)
+
+        msk_ymlpf = ymlpf["cls_id"] != 0
+        pred_px = (ymlpf["pt"] * ymlpf["cos_phi"]) * msk_ymlpf
+        pred_py = (ymlpf["pt"] * ymlpf["sin_phi"]) * msk_ymlpf
+
+        if use_latentX:  # use the latent representations
+            for layer in latent_reps:
+                if "conv" in layer:
+                    latent_reps[layer] *= batch.mask.unsqueeze(-1)
+
+            X = torch.cat(
+                [
+                    batch.X,  # 17
+                    latent_reps["conv_reg0"],  # 256
+                    latent_reps["conv_reg1"],  # 256
+                    latent_reps["conv_reg2"],  # 256
+                    latent_reps["nn_id"],  # 6
+                ],
+                axis=-1,
+            )
+
+        else:  # use the MLPF cands
+            p4_masked = ymlpf["momentum"] * msk_ymlpf.unsqueeze(-1)
+            X = torch.cat([p4_masked, ymlpf["cls_id_onehot"], ymlpf["charge"]], axis=-1)
+            # X = torch.cat([ymlpf["momentum"], ymlpf["cls_id_onehot"], ymlpf["charge"]], axis=-1)
+
+        assert X.requires_grad is False, "The MLPF model must be frozen."
+
+        if is_train:
+            wx, wy = deepmet(X)
+        else:
+            with torch.no_grad():
+                wx, wy = deepmet(X)
+
+        pred_met_x = torch.sum(wx * pred_px, axis=1)
+        pred_met_y = torch.sum(wy * pred_py, axis=1)
+
+        # genMET to compute the loss
+        msk_gen = ygen["cls_id"] != 0
+        gen_px = (ygen["pt"] * ygen["cos_phi"]) * msk_gen
+        gen_py = (ygen["pt"] * ygen["sin_phi"]) * msk_gen
+
+        true_met_x = torch.sum(gen_px, axis=1)
+        true_met_y = torch.sum(gen_py, axis=1)
+
+        if is_train:
+            loss["MET"] = torch.nn.functional.huber_loss(true_met_x, pred_met_x) + torch.nn.functional.huber_loss(
+                true_met_y, pred_met_y
+            )
+            for param in deepmet.parameters():
+                param.grad = None
+            loss["MET"].backward()
+            optimizer.step()
+        else:
+            with torch.no_grad():
+                loss["MET"] = torch.nn.functional.huber_loss(true_met_x, pred_met_x) + torch.nn.functional.huber_loss(
+                    true_met_y, pred_met_y
+                )
+
+        # monitor the MLPF (and PF) MET loss
+        with torch.no_grad():
+            loss["MET_mlpf"] = torch.nn.functional.huber_loss(
+                true_met_x, torch.sum(pred_px, axis=1)
+            ) + torch.nn.functional.huber_loss(true_met_y, torch.sum(pred_py, axis=1))
+
+            ycand = unpack_target(batch.ycand)
+            msk_ycand = ycand["cls_id"] != 0
+            cand_px = (ycand["pt"] * ycand["cos_phi"]) * msk_ycand
+            cand_py = (ycand["pt"] * ycand["sin_phi"]) * msk_ycand
+
+            loss["MET_PF"] = torch.nn.functional.huber_loss(
+                true_met_x, torch.sum(cand_px, axis=1)
+            ) + torch.nn.functional.huber_loss(true_met_y, torch.sum(cand_py, axis=1))
+
+        for loss_ in loss.keys():
+            if loss_ not in epoch_loss:
+                epoch_loss[loss_] = 0.0
+            epoch_loss[loss_] += loss[loss_].detach()
+
+    for loss_ in epoch_loss:
+        epoch_loss[loss_] = epoch_loss[loss_].cpu().item() / len(data_loader)
+
+    return epoch_loss
+
+
+def train_mlpf(
+    rank,
+    deepmet,
+    mlpf,
+    use_latentX,
+    optimizer,
+    train_loader,
+    valid_loader,
+    num_epochs,
+    patience,
+    outdir,
+    trainable="all",
+    checkpoint_freq=None,
+):
+    """
+    Will run a full training by calling train().
+
+    Args:
+        rank: 'cpu' or int representing the gpu device id
+        model: a pytorch model (may be wrapped by DistributedDataParallel)
+        train_loader: a pytorch geometric Dataloader that loads the training data in the form ~ DataBatch(X, ygen, ycands)
+        valid_loader: a pytorch geometric Dataloader that loads the validation data in the form ~ DataBatch(X, ygen, ycands)
+        patience: number of stale epochs before stopping the training
+        outdir: path to store the model weights and training plots
+    """
+
+    tensorboard_writer_train = SummaryWriter(f"{outdir}/runs/train")
+    tensorboard_writer_valid = SummaryWriter(f"{outdir}/runs/valid")
+
+    t0_initial = time.time()
+
+    losses_of_interest = ["MET", "MET_mlpf", "MET_PF"]
+
+    losses = {}
+    losses["train"], losses["valid"] = {}, {}
+    for loss in losses_of_interest:
+        losses["train"][loss], losses["valid"][loss] = [], []
+
+    stale_epochs, best_val_loss = torch.tensor(0, device=rank), float("inf")
+
+    start_epoch = 1
+    for epoch in range(start_epoch, num_epochs + 1):
+        t0 = time.time()
+
+        losses_t = train_and_valid(
+            rank,
+            deepmet,
+            mlpf,
+            use_latentX,
+            optimizer,
+            train_loader=train_loader,
+            valid_loader=valid_loader,
+            trainable=trainable,
+            is_train=True,
+            epoch=epoch,
+        )
+
+        losses_v = train_and_valid(
+            rank,
+            deepmet,
+            mlpf,
+            use_latentX,
+            optimizer,
+            train_loader=train_loader,
+            valid_loader=valid_loader,
+            trainable=trainable,
+            is_train=False,
+            epoch=epoch,
+        )
+
+        extra_state = {"epoch": epoch}
+        if losses_v["MET"] < best_val_loss:
+            best_val_loss = losses_v["MET"]
+            stale_epochs = 0
+            torch.save(
+                {"model_state_dict": get_model_state_dict(deepmet), "optimizer_state_dict": optimizer.state_dict()},
+                f"{outdir}/best_weights.pth",
+            )
+            save_checkpoint(f"{outdir}/best_weights.pth", deepmet, optimizer, extra_state)
+        else:
+            stale_epochs += 1
+
+        if checkpoint_freq and (epoch != 0) and (epoch % checkpoint_freq == 0):
+            checkpoint_dir = Path(outdir) / "checkpoints"
+            checkpoint_dir.mkdir(exist_ok=True)
+            checkpoint_path = "{}/checkpoint-{:02d}-{:.6f}.pth".format(checkpoint_dir, epoch, losses_v["MET"])
+            save_checkpoint(checkpoint_path, deepmet, optimizer, extra_state)
+
+        if stale_epochs > patience:
+            break
+
+        for loss in losses_of_interest:
+            losses["train"][loss].append(losses_t[loss])
+            losses["valid"][loss].append(losses_v[loss])
+
+        for k, v in losses_t.items():
+            tensorboard_writer_train.add_scalar("epoch/loss_" + k, v, epoch)
+
+        for k, v in losses_v.items():
+            tensorboard_writer_valid.add_scalar("epoch/loss_" + k, v, epoch)
+
+        t1 = time.time()
+
+        epochs_remaining = num_epochs - epoch
+        time_per_epoch = (t1 - t0_initial) / epoch
+        eta = epochs_remaining * time_per_epoch / 60
+
+        _logger.info(
+            f"Rank {rank}: epoch={epoch} / {num_epochs} "
+            + f"train_loss={losses_t['MET']:.4f} "
+            + f"valid_loss={losses_v['MET']:.4f} "
+            + f"stale={stale_epochs} "
+            + f"time={round((t1-t0)/60, 2)}m "
+            + f"eta={round(eta, 1)}m",
+            color="bold",
+        )
+
+        with open(f"{outdir}/mlpf_losses.pkl", "wb") as f:
+            pkl.dump(losses, f)
+
+        if tensorboard_writer_train:
+            tensorboard_writer_train.flush()
+        if tensorboard_writer_valid:
+            tensorboard_writer_valid.flush()
+
+    _logger.info(f"Done with training. Total training time on device {rank} is {round((time.time() - t0_initial)/60,3)}min")
+
+
+def override_config(config, args):
+    """override config with values from argparse Namespace"""
+    for arg in vars(args):
+        arg_value = getattr(args, arg)
+        if arg_value is not None:
+            config[arg] = arg_value
+
+    if not (args.attention_type is None):
+        config["model"]["attention"]["attention_type"] = args.attention_type
+
+    if not (args.num_convs is None):
+        for model in ["gnn_lsh", "gravnet", "attention", "attention", "mamba"]:
+            config["model"][model]["num_convs"] = args.num_convs
+
+    args.test_datasets = config["test_dataset"]
+
+    return config
