8bd199fb064bb40558466d906d46498218848e5c
Joosep Pata <joosep.pata@gmail.com>
2024-05-16 18:54:49 +0300

    Full CMS pytorch training in May 2024 (#316)
    * May 2024 training

--- scripts/tallinn/a100/pytorch.sh
+++ scripts/tallinn/a100/pytorch.sh
@@ -4,7 +4,7 @@
 #SBATCH --mem-per-gpu 80G
 #SBATCH -o logs/slurm-%x-%j-%N.out
 
-IMG=/home/software/singularity/pytorch.simg:2024-03-11
+IMG=/home/software/singularity/pytorch.simg:2024-04-30
 cd ~/particleflow
 
 singularity exec -B /scratch/persistent --nv \
@@ -12,4 +12,4 @@ singularity exec -B /scratch/persistent --nv \
     --env KERAS_BACKEND=torch \
     $IMG python3.10 mlpf/pyg_pipeline.py --dataset cms --gpus 1 \
     --data-dir /scratch/persistent/joosep/tensorflow_datasets --config parameters/pytorch/pyg-cms.yaml \
-    --train --conv-type attention --num-epochs 100 --gpu-batch-multiplier 20 --num-workers 4 --prefetch-factor 50 --checkpoint-freq 1 --comet
+    --train --conv-type attention --num-epochs 100 --gpu-batch-multiplier 40 --num-workers 4 --prefetch-factor 50 --checkpoint-freq 1 --comet
