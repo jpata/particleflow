279f37c49cdc4d3f8c901e1336ce3faef2775e85
Joosep Pata <joosep.pata@cern.ch>
2024-05-28 14:08:04 +0300

    update recipe

--- scripts/cmssw/validation_job.sh
+++ scripts/cmssw/validation_job.sh
@@ -5,18 +5,19 @@ SAMPLE=$3
 NJOB=$4
 
 PREVDIR=`pwd`
+
 #change this as needed, need enough space for outputs
-# OUTDIR=$CMSSW_BASE/out/
-# WORKDIR=$CMSSW_BASE/work_${SAMPLE}_${JOBTYPE}_${NJOB}
+OUTDIR=$CMSSW_BASE/out/
+WORKDIR=$CMSSW_BASE/work_${SAMPLE}_${JOBTYPE}_${NJOB}
 
-#when running at T2_EE_Estonia
-source /cvmfs/cms.cern.ch/cmsset_default.sh
-cd /scratch/persistent/joosep/CMSSW_14_1_0_pre3
-eval `scram runtime -sh`
-cd $PREVDIR
-
-export OUTDIR=/local/joosep/mlpf/results/cms/${CMSSW_VERSION}/
-export WORKDIR=/scratch/local/$USER/${SLURM_JOB_ID}
+# uncomment the following when running at T2_EE_Estonia
+# source /cvmfs/cms.cern.ch/cmsset_default.sh
+# cd /scratch/persistent/joosep/CMSSW_14_1_0_pre3
+# eval `scram runtime -sh`
+# cd $PREVDIR
+#
+# export OUTDIR=/local/joosep/mlpf/results/cms/${CMSSW_VERSION}/
+# export WORKDIR=/scratch/local/$USER/${SLURM_JOB_ID}
 
 #abort on error, print all commands
 set -e
