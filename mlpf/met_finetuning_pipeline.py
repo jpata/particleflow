d7fedb412fdf7c96f6a5ff873323483db45850bf
Farouk <farouk.mokhtar@gmail.com>
2024-06-15 10:38:42 +0200

    bigger deepmet

--- mlpf/met_finetuning_pipeline.py
+++ mlpf/met_finetuning_pipeline.py
@@ -111,6 +111,9 @@ def ffn(input_dim, output_dim, width, act, dropout):
         act(),
         torch.nn.LayerNorm(width),
         nn.Dropout(dropout),
+        act(),
+        torch.nn.LayerNorm(width),
+        nn.Dropout(dropout),
         nn.Linear(width, output_dim),
     )
 
