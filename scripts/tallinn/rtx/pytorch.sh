4e2ee647c259f3fef20630bac276d0be4e81cd39
Joosep Pata <joosep.pata@cern.ch>
2024-09-02 12:48:12 +0300

    fix params

--- scripts/tallinn/rtx/pytorch.sh
+++ scripts/tallinn/rtx/pytorch.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #SBATCH --partition gpu
-#SBATCH --gres gpu:rtx:4
+#SBATCH --gres gpu:rtx:8
 #SBATCH --mem-per-gpu 40G
 #SBATCH -o logs/slurm-%x-%j-%N.out
 
-IMG=/home/software/singularity/pytorch.simg:2024-07-03
+IMG=/home/software/singularity/pytorch.simg:2024-08-18
 
+ulimit -n 10000
 singularity exec -B /scratch/persistent --nv \
     --env PYTHONPATH=hep_tfds \
     --env KERAS_BACKEND=torch \
-    $IMG python3.10 mlpf/pyg_pipeline.py --dataset clic --gpus 4 \
+    $IMG python3.10 mlpf/pyg_pipeline.py --dataset clic --gpus 8 \
     --data-dir /scratch/persistent/joosep/tensorflow_datasets --config parameters/pytorch/pyg-clic.yaml \
-    --train --test --make-plots --conv-type attention --gpu-batch-multiplier 10 --num-workers 1 --prefetch-factor 10 --attention-type math --dtype float32
+    --train --test --make-plots --conv-type attention --gpu-batch-multiplier 10 --num-workers 2 --prefetch-factor 100 --attention-type math --dtype float32
