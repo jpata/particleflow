5719b540417ecffc92da7f62e3088e39cbbff784
Joosep Pata <joosep.pata@kbfi.ee>
2024-09-16 14:57:14 +0300

    up

--- scripts/tallinn/a100/pytorch-small-eval-cms.sh
+++ scripts/tallinn/a100/pytorch-small-eval-cms.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 #SBATCH --partition gpu
 #SBATCH --gres gpu:mig:1
-<<<<<<< HEAD
-#SBATCH --mem-per-gpu 150G
-#SBATCH -o logs/slurm-%x-%j-%N.out
-
-IMG=/home/software/singularity/pytorch.simg:2024-07-08
-cd ~/particleflow
-
-WEIGHTS=experiments/pyg-cms_20240804_095032_809397/checkpoints/checkpoint-13-19.827532.pth
-singularity exec -B /scratch/persistent --nv \
-     --env PYTHONPATH=hep_tfds \
-     --env KERAS_BACKEND=torch \
-     $IMG  python3.10 mlpf/pyg_pipeline.py --dataset cms --gpus 1 \
-     --data-dir /scratch/persistent/joosep/tensorflow_datasets --config parameters/pytorch/pyg-cms.yaml \
-     --test --make-plots --gpu-batch-multiplier 20 --load $WEIGHTS --ntest 50000 --dtype bfloat16
-=======
 #SBATCH --mem-per-gpu 50G
 #SBATCH -o logs/slurm-%x-%j-%N.out
 
@@ -30,4 +15,3 @@ singularity exec -B /scratch/persistent --nv \
      $IMG python mlpf/pyg_pipeline.py --dataset cms --gpus 1 \
      --data-dir /scratch/persistent/joosep/tensorflow_datasets --config parameters/pytorch/pyg-cms.yaml \
      --test --make-plots --gpu-batch-multiplier 1 --load $WEIGHTS --ntrain 1000 --ntest 1000 --nvalid 1000 --dtype bfloat16 --num-epochs 20
->>>>>>> origin/fixes_sep6
