3487b6fee00c9c60b50d162f97b563de7ad32381
Joosep Pata <joosep.pata@cern.ch>
2024-09-20 11:42:55 +0300

    re-enable numba

--- scripts/tallinn/a100/pytorch-small-eval-cms.sh
+++ scripts/tallinn/a100/pytorch-small-eval-cms.sh
@@ -7,11 +7,11 @@
 IMG=/home/software/singularity/pytorch.simg:2024-08-18
 cd ~/particleflow
 
-WEIGHTS=experiments/pyg-cms_20240917_155112_841033/checkpoints/checkpoint-06-3.120692.pth
+WEIGHTS=experiments/pyg-cms_20240917_155112_841033/checkpoints/checkpoint-09-2.998500.pth
 env
 singularity exec -B /scratch/persistent --nv \
      --env PYTHONPATH=hep_tfds \
      --env KERAS_BACKEND=torch \
      $IMG python mlpf/pyg_pipeline.py --dataset cms --gpus 1 \
      --data-dir /scratch/persistent/joosep/tensorflow_datasets --config parameters/pytorch/pyg-cms.yaml \
-     --test --make-plots --gpu-batch-multiplier 2 --load $WEIGHTS --ntest 5000 --dtype bfloat16 --test-datasets cms_pf_qcd
+     --test --make-plots --gpu-batch-multiplier 2 --load $WEIGHTS --ntest 5000 --dtype bfloat16 --test-datasets cms_pf_ttbar
