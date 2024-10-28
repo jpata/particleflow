b80313bf211b490f310974a9c131fc783657816f
Eric Wulff <eric.wulff@cern.ch>
2024-10-28 05:00:29 -0400

    chore: update flatiron batch scripts

--- scripts/flatiron/pt_raytune_a100_1GPUperTrial.sh
+++ scripts/flatiron/pt_raytune_a100_1GPUperTrial.sh
@@ -26,7 +26,7 @@ module --force purge; module load modules/2.2-20230808
 module load slurm gcc cmake cuda/12.1.1 cudnn/8.9.2.26-12.x nccl openmpi apptainer
 
 nvidia-smi
-source ~/miniconda3/bin/activate pytorch
+source ~/miniforge3/bin/activate mlpf
 which python3
 python3 --version
 
