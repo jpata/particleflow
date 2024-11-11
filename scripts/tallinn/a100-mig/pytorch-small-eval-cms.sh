3d71be5471cab0c8570d05d6ea663e1e53c9ce47
Joosep Pata <joosep.pata@cern.ch>
2024-11-11 09:47:16 +0200

    up

--- scripts/tallinn/a100-mig/pytorch-small-eval-cms.sh
+++ scripts/tallinn/a100-mig/pytorch-small-eval-cms.sh
@@ -7,7 +7,7 @@
 IMG=/home/software/singularity/pytorch.simg:2024-08-18
 cd ~/particleflow
 
-WEIGHTS=experiments/pyg-cms_20241002_205216_443429/checkpoints/checkpoint-20-3.460449.pth
+WEIGHTS=experiments/pyg-cms_20241101_090645_682892/checkpoints/checkpoint-08-2.986092.pth
 DATASET=$1
 env
 singularity exec -B /scratch/persistent --nv \
@@ -15,4 +15,4 @@ singularity exec -B /scratch/persistent --nv \
      --env KERAS_BACKEND=torch \
      $IMG python mlpf/pipeline.py --gpus 1 \
      --data-dir /scratch/persistent/joosep/tensorflow_datasets --config parameters/pytorch/pyg-cms-nopu.yaml \
-     --test --make-plots --gpu-batch-multiplier 2 --load $WEIGHTS --ntest 5000 --dtype bfloat16 --num-workers 8 --prefetch-factor 10 --test-datasets $DATASET
+     --test --make-plots --gpu-batch-multiplier 2 --load $WEIGHTS --ntest 20000 --dtype bfloat16 --num-workers 8 --prefetch-factor 10 --test-datasets $DATASET
--- scripts/tallinn/prepare_dataset_lxplus.sh
+++ scripts/tallinn/prepare_dataset_lxplus.sh
@@ -2,7 +2,8 @@
 
 rm -f scripts/files_to_copy.txt
 
-maxfiles=100000
+maxfiles=100
+
 samplestocopy=(
     "nopu/SingleElectronFlatPt1To1000_pythia8_cfi"
     "nopu/SingleGammaFlatPt1To1000_pythia8_cfi"
@@ -21,11 +22,20 @@ samplestocopy=(
     "pu55to75/ZTT_All_hadronic_14TeV_TuneCUETP8M1_cfi"
 )
 
+#samplestocopy=(
+#    "p8_ee_qq_ecm380"
+#    "p8_ee_tt_ecm380"
+#    "p8_ee_WW_fullhad_ecm380"
+#    "p8_ee_ZH_Htautau_ecm380"
+#    "p8_ee_Z_Ztautau_ecm380"
+#)
+
 
 #get a few files from each sample, both the root and postprocessed (raw)
 for sample in "${samplestocopy[@]}"; do
-    # find "/local/joosep/mlpf/cms/./20240823_simcluster/$sample/root" -type f | sort | head -n$maxfiles >> scripts/files_to_copy.txt
+    find "/local/joosep/mlpf/cms/./20240823_simcluster/$sample/root" -type f | sort | head -n$maxfiles >> scripts/files_to_copy.txt
     find "/local/joosep/mlpf/cms/./20240823_simcluster/$sample/raw" -type f | sort | head -n$maxfiles >> scripts/files_to_copy.txt
+    # find /local/joosep/clic_edm4hep/./2024_07/$sample/root/ -type f | sort | head -n$maxfiles >> scripts/files_to_copy.txt
 done
 
 #get the total size
