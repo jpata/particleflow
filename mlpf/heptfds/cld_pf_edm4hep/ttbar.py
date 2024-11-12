0b9814eab99ba430d2fd5c7fd701b352816de0da
Farouk <farouk.mokhtar@gmail.com>
2024-11-12 18:10:23 +0100

    up

--- mlpf/heptfds/cld_pf_edm4hep/ttbar.py
+++ mlpf/heptfds/cld_pf_edm4hep/ttbar.py
@@ -24,10 +24,11 @@ FIXME
 
 
 class CldEdmTtbarPf(tfds.core.GeneratorBasedBuilder):
-    VERSION = tfds.core.Version("2.3.0")
+    VERSION = tfds.core.Version("2.5.0")
     RELEASE_NOTES = {
         "2.0.0": "Initial release",
         "2.3.0": "Fix target/truth momentum, st=1 more inclusive: PR352",
+        "2.5.0": "Use 10 splits, skip 2.4.0 to unify with CMS datasets",
     }
     MANUAL_DOWNLOAD_INSTRUCTIONS = """
     For the raw input files in ROOT EDM4HEP format, please see the citation above.
