f3b02bf6790a5180fc0525d26b34316a4be17cd2
Joosep Pata <joosep.pata@cern.ch>
2024-12-14 20:24:37 +0200

    up

--- mlpf/pipeline.py
+++ mlpf/pipeline.py
@@ -18,7 +18,8 @@ os.environ["OPENBLAS_NUM_THREADS"] = "1"
 os.environ["OMP_NUM_THREADS"] = "1"
 
 import yaml
-from mlpf.model.training import device_agnostic_run, override_config, run_hpo, run_ray_training
+from mlpf.model.training import device_agnostic_run, override_config
+from mlpf.model.distributed_ray import run_hpo, run_ray_training
 from mlpf.model.PFDataset import SHARING_STRATEGY
 from utils import create_experiment_dir
 
