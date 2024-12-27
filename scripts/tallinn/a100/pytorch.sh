7a5cb441e4e66e1544c76a0a83a837ebf333e3da
Joosep Pata <joosep.pata@cern.ch>
2024-12-27 11:03:21 +0200

    use NANO for CMSSW evals

--- scripts/tallinn/a100/pytorch.sh
+++ scripts/tallinn/a100/pytorch.sh
@@ -14,4 +14,4 @@ singularity exec -B /scratch/persistent --nv \
     $IMG python3 mlpf/pipeline.py --gpus 1 \
     --data-dir /scratch/persistent/joosep/tensorflow_datasets --config parameters/pytorch/pyg-cms.yaml \
     --train --conv-type attention \
-    --gpu-batch-multiplier 5 --checkpoint-freq 1 --num-workers 8 --prefetch-factor 50 --comet --num-epochs 10
+    --gpu-batch-multiplier 5 --checkpoint-freq 1 --num-workers 8 --prefetch-factor 50 --comet
