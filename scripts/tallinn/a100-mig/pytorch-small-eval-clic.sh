0f06cb72af9d2fe5225dbea177e1ea73a06ebe89
Joosep Pata <joosep.pata@cern.ch>
2025-01-14 16:44:38 +0200

    add triton timing script

--- scripts/tallinn/a100-mig/pytorch-small-eval-clic.sh
+++ scripts/tallinn/a100-mig/pytorch-small-eval-clic.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH --partition gpu
 #SBATCH --gres gpu:mig:1
-#SBATCH --mem-per-gpu 200G
+#SBATCH --mem-per-gpu 100G
 #SBATCH -o logs/slurm-%x-%j-%N.out
 
-IMG=/home/software/singularity/pytorch.simg:2024-08-18
+IMG=/home/software/singularity/pytorch.simg:2024-12-03
 cd ~/particleflow
 
-WEIGHTS=experiments/pyg-clic_20241106_104416_929167/checkpoints/checkpoint-20-1.914489.pth
+WEIGHTS=experiments/pyg-clic_20250106_193536_269746/checkpoints/checkpoint-04-2.043485.pth
 singularity exec -B /scratch/persistent --nv \
      --env PYTHONPATH=`pwd` \
      --env KERAS_BACKEND=torch \
      $IMG  python3 mlpf/pipeline.py --gpus 1 \
      --data-dir /scratch/persistent/joosep/tensorflow_datasets --config parameters/pytorch/pyg-clic.yaml \
-     --test --make-plots --gpu-batch-multiplier 100 --load $WEIGHTS --dtype bfloat16 --prefetch-factor 10 --num-workers 8 --ntest 50000
+     --test --make-plots --gpu-batch-multiplier 100 --load $WEIGHTS --dtype bfloat16 --num-workers 0 --ntest 50000
