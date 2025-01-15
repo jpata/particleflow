3d7ddacbb6446c0423cf1d979f6a8bb0056eb05e
Joosep Pata <joosep.pata@cern.ch>
2025-01-15 10:37:43 +0200

    up

--- scripts/tallinn/a100-mig/pytorch-small-eval-clic.sh
+++ scripts/tallinn/a100-mig/pytorch-small-eval-clic.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --partition gpu
-#SBATCH --gres gpu:mig:1
+#SBATCH --gres gpu:a100-mig:1
 #SBATCH --mem-per-gpu 100G
 #SBATCH -o logs/slurm-%x-%j-%N.out
 
 IMG=/home/software/singularity/pytorch.simg:2024-12-03
 cd ~/particleflow
 
-WEIGHTS=experiments/pyg-clic_20250106_193536_269746/checkpoints/checkpoint-04-2.043485.pth
+WEIGHTS=experiments/pyg-clic_20250106_193536_269746/checkpoints/checkpoint-05-1.995116.pth
 singularity exec -B /scratch/persistent --nv \
      --env PYTHONPATH=`pwd` \
      --env KERAS_BACKEND=torch \
--- scripts/tallinn/deploy_clic.sh
+++ scripts/tallinn/deploy_clic.sh
@@ -3,8 +3,8 @@
 
 rm -f files_to_copy.txt
 maxfiles=100
-path=experiments/pyg-clic_20241106_104416_929167/./preds_checkpoint-20-1.914489
-targetpath=/home/joosep/huggingface/particleflow/clic/clusters/v2.1.0/pyg-clic_20241106_104416_929167/
+path=experiments/pyg-clic_20250106_193536_269746/./preds_checkpoint-04-2.043485
+targetpath=/scratch/persistent/joosep/huggingface/particleflow/clic/clusters/v2.2.0/pyg-clic_20250106_193536_269746/
 
 mkdir -p $targetpath
 cp $path/../* $targetpath/
@@ -22,4 +22,4 @@ for sample in "${samplestocopy[@]}"; do
     find "$path/$sample" -type f | sort | head -n$maxfiles >> files_to_copy.txt
 done
 
-# rsync --progress --relative --files-from files_to_copy.txt `pwd` $targetpath
+rsync --progress --relative --files-from files_to_copy.txt `pwd` $targetpath
