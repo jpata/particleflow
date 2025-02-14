45f04f69989f5bde9c831d0c83362664bc7bdaae
Farouk <farouk.mokhtar@gmail.com>
2025-02-14 16:21:29 +0100

    up

--- mlpf/model/inference.py
+++ mlpf/model/inference.py
@@ -116,6 +116,7 @@ def predict_one_batch(conv_type, model, i, batch, rank, jetdef, jet_ptcut, jet_m
         print("pt", ydata["pt"])
         
         cluster = fastjet.ClusterSequence(vec.to_xyzt(), jetdef)
+        print("cluster", cluster.constituents)
         jets = cluster.inclusive_jets(min_pt=jet_ptcut)
         jets_coll[typ] = vector.awk(awkward.zip({"px": jets.px, "py": jets.py, "pz": jets.pz, "E": jets.e}))
 
