8c14f5e43744d8f9f6c3b2c1297e4fe496b1d4a2
Joosep Pata <joosep.pata@cern.ch>
2025-05-09 08:08:12 +0300

    update postprocesing paths

--- mlpf/data/cms/postprocessing_jobs.py
+++ mlpf/data/cms/postprocessing_jobs.py
@@ -34,29 +34,19 @@ def write_script(infiles, outfiles):
 
 samples = [
     # PU
-    # "/local/joosep/mlpf/cms/20240823_simcluster/pu55to75/TTbar_14TeV_TuneCUETP8M1_cfi",
-    # "/local/joosep/mlpf/cms/20240823_simcluster/pu55to75/QCDForPF_14TeV_TuneCUETP8M1_cfi",
-    # "/local/joosep/mlpf/cms/20240823_simcluster/pu55to75/ZTT_All_hadronic_14TeV_TuneCUETP8M1_cfi",
+    "/local/joosep/mlpf/cms/20250508_cmssw_15_0_5_d3c6d1/pu55to75/TTbar_14TeV_TuneCUETP8M1_cfi/",
+    # "/local/joosep/mlpf/cms/20250508_cmssw_15_0_5_d3c6d1/pu55to75/QCDForPF_14TeV_TuneCUETP8M1_cfi",
+    # "/local/joosep/mlpf/cms/20250508_cmssw_15_0_5_d3c6d1/pu55to75/ZTT_All_hadronic_14TeV_TuneCUETP8M1_cfi",
     # NoPU
-    "/local/joosep/mlpf/cms/20240823_simcluster/nopu/TTbar_14TeV_TuneCUETP8M1_cfi",
-    # "/local/joosep/mlpf/cms/20240823_simcluster/nopu/QCDForPF_14TeV_TuneCUETP8M1_cfi",
-    # "/local/joosep/mlpf/cms/20240823_simcluster/nopu/ZTT_All_hadronic_14TeV_TuneCUETP8M1_cfi",
-    # Single particle gun
-    # "/local/joosep/mlpf/cms/20240823_simcluster/nopu/SinglePiMinusFlatPt0p7To1000_cfi"
-    # "/local/joosep/mlpf/cms/20240823_simcluster/nopu/SinglePi0Pt1To1000_pythia8_cfi",
-    # "/local/joosep/mlpf/cms/20240823_simcluster/nopu/SingleGammaFlatPt1To1000_pythia8_cfi",
-    # "/local/joosep/mlpf/cms/20240823_simcluster/nopu/SingleK0FlatPt1To1000_pythia8_cfi",
-    # "/local/joosep/mlpf/cms/20240823_simcluster/nopu/SingleElectronFlatPt1To1000_pythia8_cfi",
-    # "/local/joosep/mlpf/cms/20240823_simcluster/nopu/SingleMuFlatPt1To1000_pythia8_cfi",
-    # "/local/joosep/mlpf/cms/20240823_simcluster/nopu/SingleNeutronFlatPt0p7To1000_cfi",
-    # "/local/joosep/mlpf/cms/20240823_simcluster/nopu/SingleProtonMinusFlatPt0p7To1000_cfi",
-    # "/local/joosep/mlpf/cms/20240823_simcluster/nopu/SingleTauFlatPt1To1000_cfi",
+    # "/local/joosep/mlpf/cms/20250508_cmssw_15_0_5_d3c6d1/nopu/TTbar_14TeV_TuneCUETP8M1_cfi",
+    # "/local/joosep/mlpf/cms/20250508_cmssw_15_0_5_d3c6d1/nopu/QCDForPF_14TeV_TuneCUETP8M1_cfi",
+    # "/local/joosep/mlpf/cms/20250508_cmssw_15_0_5_d3c6d1/nopu/ZTT_All_hadronic_14TeV_TuneCUETP8M1_cfi",
 ]
 
 ichunk = 1
 for sample in samples:
     infiles = sorted(list(glob.glob(f"{sample}/root/pfntuple*.root")))
-    for infiles_chunk in chunks(infiles, 50):
+    for infiles_chunk in chunks(infiles, 10):
         outfiles_chunk = [inf.replace(".root", ".pkl.bz2").replace("/root/", "/raw/") for inf in infiles_chunk]
         os.makedirs(os.path.dirname(outfiles_chunk[0]), exist_ok=True)
         scr = write_script(infiles_chunk, outfiles_chunk)
