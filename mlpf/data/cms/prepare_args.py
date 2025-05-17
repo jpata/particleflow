70ecde3a39cc8dcab0bae09f001b3db5564e670c
Joosep Pata <joosep.pata@cern.ch>
2025-05-17 23:26:38 +0300

    start 2.5.0 training

--- mlpf/data/cms/prepare_args.py
+++ mlpf/data/cms/prepare_args.py
@@ -16,11 +16,11 @@ samples = [
     #    ("VBF_TuneCP5_14TeV_pythia8_cfi",                          700000, 705010, "genjob_pu55to75.sh", outdir + "/pu55to75"),
     #    ("SinglePiMinusFlatPt0p7To1000_cfi",                       800000, 801010, "genjob_pu55to75.sh", outdir + "/pu55to75"),
 
-    ("ZpTT_1500_14TeV_TuneCP5_cfi",                           720000, 750010, "genjob_nopu.sh", outdir + "/nopu"),
-    ("TTbar_14TeV_TuneCUETP8M1_cfi",                           820000, 850010, "genjob_nopu.sh", outdir + "/nopu"),
+    #    ("ZpTT_1500_14TeV_TuneCP5_cfi",                           720000, 750010, "genjob_nopu.sh", outdir + "/nopu"),
+    #    ("TTbar_14TeV_TuneCUETP8M1_cfi",                           820000, 850010, "genjob_nopu.sh", outdir + "/nopu"),
     #    ("VBF_TuneCP5_14TeV_pythia8_cfi",                          900000, 920010, "genjob_nopu.sh", outdir + "/nopu"),
     #    ("QCDForPF_14TeV_TuneCUETP8M1_cfi",                       1000000,1050010, "genjob_nopu.sh", outdir + "/nopu"),
-    ("ZTT_All_hadronic_14TeV_TuneCUETP8M1_cfi",               1120000,1150010, "genjob_nopu.sh", outdir + "/nopu"),
+    #    ("ZTT_All_hadronic_14TeV_TuneCUETP8M1_cfi",               1120000,1150010, "genjob_nopu.sh", outdir + "/nopu"),
 
     #    ("MultiParticlePFGun50_cfi",                               800000, 801000, "genjob_nopu.sh", outdir + "/nopu"),
     #    ("SingleElectronFlatPt1To1000_pythia8_cfi",                900000, 910010, "genjob_nopu.sh", outdir + "/nopu"),
--- scripts/tallinn/submit_tfds.sh
+++ scripts/tallinn/submit_tfds.sh
@@ -2,15 +2,15 @@
 
 SUB=scripts/tallinn/generate_tfds.sh
 
-export DATA_DIR=/local/joosep/mlpf/tensorflow_datasets/cms2/
-export MANUAL_DIR=/local/joosep/mlpf/cms/20250508_cmssw_15_0_5_d3c6d1/
+# export DATA_DIR=/local/joosep/mlpf/tensorflow_datasets/cms2/
+# export MANUAL_DIR=/local/joosep/mlpf/cms/20250508_cmssw_15_0_5_d3c6d1/
 # for i in `seq 1 10`; do
 #     sbatch $SUB cms_pf/qcd_nopu $i nopu
 #     sbatch $SUB cms_pf/ttbar_nopu $i nopu
 #     sbatch $SUB cms_pf/ztt_nopu $i nopu
 #     sbatch $SUB cms_pf/qcd $i pu55to75
 #     sbatch $SUB cms_pf/ttbar $i pu55to75
-#     sbatch $SUB cms_pf/ztt $i pu55to75
+#      sbatch $SUB cms_pf/ztt $i pu55to75
 # done
 
 # export DATA_DIR=/local/joosep/mlpf/tensorflow_datasets/clic
