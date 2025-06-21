3a2f2c8754e6d2f619f5c23fa2728e91a0a3b448
Joosep Pata <joosep.pata@cern.ch>
2025-06-21 11:28:07 +0300

    merge

--- scripts/tallinn/a100-mig/pytorch-small-eval-cms.sh
+++ scripts/tallinn/a100-mig/pytorch-small-eval-cms.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 #SBATCH --partition gpu
 #SBATCH --gres gpu:mig:1
-#SBATCH --mem-per-gpu 20G
+#SBATCH --mem-per-gpu 40G
 #SBATCH -o logs/slurm-%x-%j-%N.out
 
 IMG=/home/software/singularity/pytorch.simg:2024-12-03
-cd ~/particleflow
+cd ~/particleflow2
 
-WEIGHTS=experiments/pyg-cms_20250517_232752_544969/checkpoints/checkpoint-08-3.863894.pth
-EXPDIR=experiments/pyg-cms_20250517_232752_544969
-DATASET=$1
+WEIGHTS=experiments/pyg-cms_20250613_214954_106598/checkpoints/checkpoint-10-3.664833.pth
+EXPDIR=experiments/pyg-cms_20250613_214954_106598/
+DATASET=cms_pf_qcd
+
 env
 singularity exec -B /local -B /scratch/persistent --nv \
      --env PYTHONPATH=`pwd` \
      --env KERAS_BACKEND=torch \
      $IMG python mlpf/pipeline.py --gpus 1 \
      --data-dir /scratch/persistent/joosep/tensorflow_datasets --config parameters/pytorch/pyg-cms.yaml \
-     --test --make-plots --gpu-batch-multiplier 2 --load $WEIGHTS --ntest 1000 --dtype bfloat16 --num-workers 1 --prefetch-factor 10 --test-datasets $DATASET --experiment-dir $EXPDIR
+     --test --make-plots --gpu-batch-multiplier 2 --load $WEIGHTS --ntest 5000 --dtype bfloat16 --num-workers 1 --prefetch-factor 10 --test-datasets $DATASET --experiment-dir $EXPDIR
--- scripts/tallinn/a100-mig/pytorch-small.sh
+++ scripts/tallinn/a100-mig/pytorch-small.sh
@@ -5,14 +5,14 @@
 #SBATCH -o logs/slurm-%x-%j-%N.out
 
 IMG=/home/software/singularity/pytorch.simg:2025-06-12
-cd ~/particleflow
+cd ~/particleflow2
 
 env
 
 ulimit -n 10000
 singularity exec -B /scratch/persistent --nv \
-    --env PYTHONPATH=hep_tfds \
+    --env PYTHONPATH=`pwd` \
     --env KERAS_BACKEND=torch \
     $IMG python3 mlpf/pipeline.py --gpus 1 \
     --data-dir /scratch/persistent/joosep/tensorflow_datasets --config parameters/pytorch/pyg-cms.yaml \
-    --train --test --make-plots --conv-type attention --attention-type flash --gpu-batch-multiplier 1 --num-workers 1 --prefetch-factor 10 --dtype bfloat16 --checkpoint-freq -1 --ntrain 10000 --nvalid 1000 --ntest 1000 --num-epochs 10
+    --train --test --make-plots --conv-type litemla --gpu-batch-multiplier 5 --num-workers 1 --prefetch-factor 10 --dtype bfloat16 --checkpoint-freq -1 --ntrain 10000 --nvalid 1000 --ntest 1000 --num-epochs 10
