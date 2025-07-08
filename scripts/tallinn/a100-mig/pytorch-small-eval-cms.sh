c4ea4b83abaedd4f137cde455831f7cffad097d5
Joosep Pata <joosep.pata@cern.ch>
2025-07-08 14:09:48 +0300

    up

--- scripts/tallinn/a100-mig/pytorch-small-eval-cms.sh
+++ scripts/tallinn/a100-mig/pytorch-small-eval-cms.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition gpu
 #SBATCH --gres gpu:mig:1
-#SBATCH --mem-per-gpu 20G
+#SBATCH --mem-per-gpu 100G
 #SBATCH -o logs/slurm-%x-%j-%N.out
 
 IMG=/home/software/singularity/pytorch.simg:2024-12-03
@@ -16,4 +16,4 @@ singularity exec -B /local -B /scratch/persistent --nv \
      --env KERAS_BACKEND=torch \
      $IMG python mlpf/pipeline.py --gpus 1 \
      --data-dir /scratch/persistent/joosep/tensorflow_datasets --config parameters/pytorch/pyg-cms.yaml \
-     --test --make-plots --gpu-batch-multiplier 2 --load $WEIGHTS --ntest 1000 --dtype bfloat16 --num-workers 1 --prefetch-factor 10 --test-datasets $DATASET --experiment-dir $EXPDIR
+     --test --gpu-batch-multiplier 2 --load $WEIGHTS --ntest 20000 --dtype bfloat16 --num-workers 1 --prefetch-factor 10 --test-datasets $DATASET --experiment-dir $EXPDIR
