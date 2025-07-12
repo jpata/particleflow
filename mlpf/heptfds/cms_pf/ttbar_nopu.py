d924996609f428d694552565e6c1289937b55a29
Joosep Pata <joosep.pata@cern.ch>
2025-07-12 08:47:06 +0300

    generate 2.7.0 with no caloparticle splitting

--- mlpf/heptfds/cms_pf/ttbar_nopu.py
+++ mlpf/heptfds/cms_pf/ttbar_nopu.py
@@ -22,7 +22,7 @@ _CITATION = """
 class CmsPfTtbarNopu(tfds.core.GeneratorBasedBuilder):
     """DatasetBuilder for cms_pf_ttbar_nopu dataset."""
 
-    VERSION = tfds.core.Version("2.6.0")
+    VERSION = tfds.core.Version("2.7.0")
     RELEASE_NOTES = {
         "1.7.1": "First version",
         "1.8.0": "Add ispu, genjets, genmet; disable genjet_idx; improved merging",
@@ -33,6 +33,7 @@ class CmsPfTtbarNopu(tfds.core.GeneratorBasedBuilder):
         "2.5.0": "Remove neutrinos from genjets, split to 10",
         "2.5.1": "Associate ele with GSF first",
         "2.6.0": "Regenerate with 20250508_cmssw_15_0_5_d3c6d1",
+        "2.7.0": "Remove split_caloparticle",
     }
     MANUAL_DOWNLOAD_INSTRUCTIONS = """
     rsync -r --progress lxplus.cern.ch:/eos/user/j/jpata/mlpf/tensorflow_datasets/cms/cms_pf_ttbar_nopu ~/tensorflow_datasets/
@@ -69,7 +70,7 @@ class CmsPfTtbarNopu(tfds.core.GeneratorBasedBuilder):
         """Returns SplitGenerators."""
         path = dl_manager.manual_dir
         sample_dir = "TTbar_14TeV_TuneCUETP8M1_cfi"
-        return cms_utils.split_sample(path / sample_dir / "raw", self.builder_config, num_splits=cms_utils.NUM_SPLITS)
+        return cms_utils.split_sample(path / sample_dir / "raw2", self.builder_config, num_splits=cms_utils.NUM_SPLITS)
 
     def _generate_examples(self, files):
         return cms_utils.generate_examples(files)
--- mlpf/heptfds/cms_pf/ztt_nopu.py
+++ mlpf/heptfds/cms_pf/ztt_nopu.py
@@ -21,11 +21,12 @@ _CITATION = """
 class CmsPfZttNopu(tfds.core.GeneratorBasedBuilder):
     """DatasetBuilder for cms_pf_ztt_nopu dataset."""
 
-    VERSION = tfds.core.Version("2.6.0")
+    VERSION = tfds.core.Version("2.7.0")
     RELEASE_NOTES = {
         "2.5.0": "Remove neutrinos from genjets, split to 10",
         "2.5.1": "Associate ele with GSF first",
         "2.6.0": "Regenerate with 20250508_cmssw_15_0_5_d3c6d1",
+        "2.7.0": "Remove split_caloparticle",
     }
     MANUAL_DOWNLOAD_INSTRUCTIONS = """
     rsync -r --progress lxplus.cern.ch:/eos/user/j/jpata/mlpf/tensorflow_datasets/cms/cms_pf_ztt ~/tensorflow_datasets/
@@ -62,7 +63,7 @@ class CmsPfZttNopu(tfds.core.GeneratorBasedBuilder):
         """Returns SplitGenerators."""
         path = dl_manager.manual_dir
         sample_dir = "ZTT_All_hadronic_14TeV_TuneCUETP8M1_cfi"
-        return cms_utils.split_sample(path / sample_dir / "raw", self.builder_config, num_splits=cms_utils.NUM_SPLITS)
+        return cms_utils.split_sample(path / sample_dir / "raw2", self.builder_config, num_splits=cms_utils.NUM_SPLITS)
 
     def _generate_examples(self, files):
         return cms_utils.generate_examples(files)
