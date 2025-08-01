7b688dc606516f665becca5add32c1e2a5189075
Joosep Pata <joosep.pata@cern.ch>
2025-08-01 14:44:27 +0300

    plots with puppi

--- scripts/tallinn/submit_validate_cms.sh
+++ scripts/tallinn/submit_validate_cms.sh
@@ -4,19 +4,19 @@ SUBSCRIPT=scripts/tallinn/cmssw-el8.sh
 
 END=`wc -l scripts/tallinn/jetmet0.txt | cut -f1 -d' '`
 for ifile in $(seq 1 $END); do
-    sbatch $SUBSCRIPT scripts/cmssw/validation_job_data.sh False mlpfpu scripts/tallinn/jetmet0.txt JetMET0 $ifile
+    sbatch $SUBSCRIPT scripts/cmssw/validation_job_data.sh False mlpf scripts/tallinn/jetmet0.txt JetMET0 $ifile
     sbatch $SUBSCRIPT scripts/cmssw/validation_job_data.sh False pf scripts/tallinn/jetmet0.txt JetMET0 $ifile
 done
 
 END=`wc -l scripts/tallinn/photonjet_pu_13p6.txt | cut -f1 -d' '`
 for ifile in $(seq 1 $END); do
-    sbatch $SUBSCRIPT scripts/cmssw/validation_job.sh False mlpfpu scripts/tallinn/photonjet_pu_13p6.txt PhotonJet_PU_13p6 $ifile
+    sbatch $SUBSCRIPT scripts/cmssw/validation_job.sh False mlpf scripts/tallinn/photonjet_pu_13p6.txt PhotonJet_PU_13p6 $ifile
     sbatch $SUBSCRIPT scripts/cmssw/validation_job.sh False pf scripts/tallinn/photonjet_pu_13p6.txt PhotonJet_PU_13p6 $ifile
 done
 
 END=`wc -l scripts/tallinn/photonjet_nopu_13p6.txt | cut -f1 -d' '`
 for ifile in $(seq 1 $END); do
-    sbatch $SUBSCRIPT scripts/cmssw/validation_job.sh False mlpfpu scripts/tallinn/photonjet_nopu_13p6.txt PhotonJet_noPU_13p6 $ifile
+    sbatch $SUBSCRIPT scripts/cmssw/validation_job.sh False mlpf scripts/tallinn/photonjet_nopu_13p6.txt PhotonJet_noPU_13p6 $ifile
     sbatch $SUBSCRIPT scripts/cmssw/validation_job.sh False pf scripts/tallinn/photonjet_nopu_13p6.txt PhotonJet_noPU_13p6 $ifile
 done
 
@@ -28,7 +28,7 @@ done
 
 END=`wc -l scripts/tallinn/ttbar_pu_13p6.txt | cut -f1 -d' '`
 for ifile in $(seq 1 $END); do
-    sbatch $SUBSCRIPT scripts/cmssw/validation_job.sh False mlpfpu scripts/tallinn/ttbar_pu_13p6.txt TTbar_PU_13p6 $ifile
+    sbatch $SUBSCRIPT scripts/cmssw/validation_job.sh False mlpf scripts/tallinn/ttbar_pu_13p6.txt TTbar_PU_13p6 $ifile
     sbatch $SUBSCRIPT scripts/cmssw/validation_job.sh False pf scripts/tallinn/ttbar_pu_13p6.txt TTbar_PU_13p6 $ifile
 done
 
