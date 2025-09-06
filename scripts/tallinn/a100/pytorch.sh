6e91cf565d0ee129d402d927a05c6dc78b532b8c
Joosep Pata <joosep.pata@cern.ch>
2025-09-06 18:20:41 +0300

    update plots

--- scripts/tallinn/a100/pytorch.sh
+++ scripts/tallinn/a100/pytorch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --partition gpu
 #SBATCH --gres gpu:a100:1
-#SBATCH --mem-per-gpu 400G
+#SBATCH --mem-per-gpu 200G
 #SBATCH -o logs/slurm-%x-%j-%N.out
 
-IMG=/home/software/singularity/pytorch.simg:2025-08-26
+IMG=/home/software/singularity/pytorch.simg:2025-09-01
 cd ~/particleflow
 
 ulimit -n 100000
@@ -17,11 +17,11 @@ singularity exec -B /scratch/persistent -B /local --nv \
     train \
     --gpus 1 \
     --gpu-batch-multiplier 16 \
-    --num-workers 8 \
-    --prefetch-factor 10 \
+    --num-workers 2 \
+    --prefetch-factor 2 \
     --conv-type attention \
     --dtype bfloat16 \
-    --optimizer lamb \
+    --optimizer adamw \
     --comet \
     --test-datasets cms_pf_qcd \
-    --num-steps 1000000 --load experiments/pyg-cms_20250831_101432_291783/checkpoints/checkpoint-11000.pth
+    --num-steps 1000000
