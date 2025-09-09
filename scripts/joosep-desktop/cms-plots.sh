211627265ff76a904f5b80c09629548ecab07395
Joosep Pata <joosep.pata@gmail.com>
2025-09-09 13:49:41 +0300

    update plots

--- scripts/joosep-desktop/cms-plots.sh
+++ scripts/joosep-desktop/cms-plots.sh
@@ -3,71 +3,70 @@
 export PYTHONPATH=`pwd`
 IMG=~/HEP-KBFI/singularity/pytorch.simg\:2025-09-01
 NANO_PATH=/mnt/work/particleflow/CMSSW_15_0_5_mlpf_v2.6.0pre1_puppi_2372e2/cuda_False
-# singularity exec -B /mnt/work/ $IMG  \
-#   python3 mlpf/plotting/data_preparation.py \
-#   --input-dir $NANO_PATH \
-#   --sample QCD_PU &
-#
-# singularity exec -B /mnt/work/ $IMG \
-#   python3 mlpf/plotting/data_preparation.py \
-#   --input-dir $NANO_PATH \
-#   --sample QCD_PU_13p6 &
-#
-# singularity exec -B /mnt/work/ $IMG \
-#   python3 mlpf/plotting/data_preparation.py \
-#   --input-dir $NANO_PATH \
-#   --sample TTbar_PU &
-#
-# singularity exec -B /mnt/work/ $IMG \
-#   python3 mlpf/plotting/data_preparation.py \
-#   --input-dir $NANO_PATH \
-#   --sample TTbar_PU_13p6 &
-#
-# singularity exec -B /mnt/work/ $IMG \
-#   python3 mlpf/plotting/data_preparation.py \
-#   --input-dir $NANO_PATH \
-#   --sample QCD_noPU_13p6 &
-#
-# singularity exec -B /mnt/work/ $IMG \
-#   python3 mlpf/plotting/data_preparation.py \
-#   --input-dir $NANO_PATH \
-#   --sample TTbar_noPU_13p6 &
-#
-# singularity exec -B /mnt/work/ $IMG \
-#   python3 mlpf/plotting/data_preparation.py \
-#   --input-dir $NANO_PATH \
-#   --sample PhotonJet_PU_13p6 &
-#
-# singularity exec -B /mnt/work/ $IMG \
-#   python3 mlpf/plotting/data_preparation.py \
-#   --input-dir $NANO_PATH \
-#   --sample PhotonJet_noPU_13p6 &
-# wait
+singularity exec -B /mnt/work/ $IMG  \
+  python3 mlpf/plotting/data_preparation.py \
+  --input-dir $NANO_PATH \
+  --sample QCD_PU &
+
+singularity exec -B /mnt/work/ $IMG \
+  python3 mlpf/plotting/data_preparation.py \
+  --input-dir $NANO_PATH \
+  --sample QCD_PU_13p6 &
+
+singularity exec -B /mnt/work/ $IMG \
+  python3 mlpf/plotting/data_preparation.py \
+  --input-dir $NANO_PATH \
+  --sample TTbar_PU &
+
+singularity exec -B /mnt/work/ $IMG \
+  python3 mlpf/plotting/data_preparation.py \
+  --input-dir $NANO_PATH \
+  --sample TTbar_PU_13p6 &
+
+singularity exec -B /mnt/work/ $IMG \
+  python3 mlpf/plotting/data_preparation.py \
+  --input-dir $NANO_PATH \
+  --sample QCD_noPU_13p6 &
+
+singularity exec -B /mnt/work/ $IMG \
+  python3 mlpf/plotting/data_preparation.py \
+  --input-dir $NANO_PATH \
+  --sample TTbar_noPU_13p6 &
+
+singularity exec -B /mnt/work/ $IMG \
+  python3 mlpf/plotting/data_preparation.py \
+  --input-dir $NANO_PATH \
+  --sample PhotonJet_PU_13p6 &
+
+singularity exec -B /mnt/work/ $IMG \
+  python3 mlpf/plotting/data_preparation.py \
+  --input-dir $NANO_PATH \
+  --sample PhotonJet_noPU_13p6 &
+wait
 
 
 # Exctract jet corrections
-# sample=QCD_PU_13p6
-# singularity exec -B /mnt/work/ $IMG \
-#   python3 mlpf/plotting/corrections.py \
-#   --input-pf-parquet ${sample}_pf.parquet \
-#   --input-mlpf-parquet ${sample}_mlpf.parquet \
-#   --corrections-file jec_ak4.npz \
-#   --output-dir ./plots/${sample}/ak4 \
-#   --jet-type ak4 \
-#   --sample-name ${sample}
+sample=QCD_PU_13p6
+singularity exec -B /mnt/work/ $IMG \
+  python3 mlpf/plotting/corrections.py \
+  --input-pf-parquet ${sample}_pf.parquet \
+  --input-mlpf-parquet ${sample}_mlpf.parquet \
+  --corrections-file jec_ak4.npz \
+  --output-dir ./plots/${sample}/ak4/jec \
+  --jet-type ak4 \
+  --sample-name ${sample}
 
-# singularity exec -B /mnt/work/ $IMG \
-#   python3 mlpf/plotting/corrections.py \
-#   --input-pf-parquet ${sample}_pf.parquet \
-#   --input-mlpf-parquet ${sample}_mlpf.parquet \
-#   --corrections-file jec_ak8.npz \
-#   --output-dir ./plots/${sample}/ak8 \
-#   --jet-type ak8 \
-#   --sample-name ${sample}
+singularity exec -B /mnt/work/ $IMG \
+  python3 mlpf/plotting/corrections.py \
+  --input-pf-parquet ${sample}_pf.parquet \
+  --input-mlpf-parquet ${sample}_mlpf.parquet \
+  --corrections-file jec_ak8.npz \
+  --output-dir ./plots/${sample}/ak8/jec \
+  --jet-type ak8 \
+  --sample-name ${sample}
 
 #Do jet-level plots
 for sample in QCD_noPU_13p6 QCD_PU_13p6 TTbar_PU_13p6 TTbar_noPU_13p6 PhotonJet_noPU_13p6 PhotonJet_PU_13p6; do
-# for sample in QCD_noPU_13p6; do
  singularity exec -B /mnt/work/ $IMG \
    python3 mlpf/plotting/plot_validation.py \
    --input-pf-parquet ${sample}_pf.parquet \
@@ -108,13 +107,21 @@ done
 wait
 
 #Do the 14 TeV to 13.6 TeV comparison
-# for sample in QCD_PU TTbar_PU; do
-#   singularity exec -B /mnt/work $IMG \
-#     python mlpf/plotting/plot_jet_response_comparison.py \
-#       --input-13p6-tev-parquet ${sample}_13p6_mlpf.parquet \
-#       --input-14-tev-parquet ${sample}_mlpf.parquet \
-#       --output-dir ./plots \
-#       --sample-name $sample \
-#       --jet-type ak4 &
-# done
-# wait
+for sample in QCD_PU TTbar_PU; do
+  singularity exec -B /mnt/work $IMG \
+    python mlpf/plotting/plot_jet_response_comparison.py \
+      --input-13p6-tev-parquet ${sample}_13p6_mlpf.parquet \
+      --input-14-tev-parquet ${sample}_mlpf.parquet \
+      --output-dir ./plots \
+      --sample-name $sample \
+      --jet-type ak4 &
+
+  singularity exec -B /mnt/work $IMG \
+    python mlpf/plotting/plot_jet_response_comparison.py \
+      --input-13p6-tev-parquet ${sample}_13p6_mlpf.parquet \
+      --input-14-tev-parquet ${sample}_mlpf.parquet \
+      --output-dir ./plots \
+      --sample-name $sample \
+      --jet-type ak8 &
+done
+wait
