0701b3a3435f46a7d0306ae5ad5c9acd36264536
Joosep Pata <joosep.pata@kbfi.ee>
2025-09-22 09:30:11 +0300

    merge

--- scripts/lumi/train.sh
+++ scripts/lumi/train.sh
@@ -7,18 +7,18 @@ rocm-smi --showdriverversion
 python3 mlpf/pipeline.py \
     --data-dir $TFDS_DATA_DIR \
     --config parameters/pytorch/pyg-cms.yaml \
-    --experiment-dir experiments/pyg-cms_20250912_201225_216787 \
+    --experiment-dir experiments/pyg-cms_20250918_072337_217846 \
     train \
     --gpus 8 \
-    --gpu-batch-multiplier 8 \
+    --gpu-batch-multiplier 4 \
     --num-workers 1 \
     --prefetch-factor 1 \
     --conv-type attention \
     --dtype bfloat16 \
     --optimizer lamb \
     --lr 0.002 \
-    --num-steps 50000 \
-    --val-freq 5000 \
+    --num-steps 100000 \
+    --val-freq 10000 \
     --checkpoint-freq 100 \
     --test-datasets cms_pf_qcd \
-    --load experiments/pyg-cms_20250912_201225_216787/checkpoints/checkpoint-35000.pth
+    --load experiments/pyg-cms_20250918_072337_217846/checkpoints/checkpoint-49900.pth
