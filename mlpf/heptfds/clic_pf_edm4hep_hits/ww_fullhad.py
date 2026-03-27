246a41937b01db7f991c89d304806f1f27d46737
Joosep Pata <joosep.pata@cern.ch>
2026-03-27 10:52:26 +0200

    added CLIC hit-based files

---
+++ mlpf/heptfds/clic_pf_edm4hep_hits/ww_fullhad.py
@@ -0,0 +1,77 @@
+from pathlib import Path
+
+import os
+import numpy as np
+import tensorflow_datasets as tfds
+from .utils_hits import (
+    NUM_SPLITS,
+    X_FEATURES,
+    Y_FEATURES,
+    generate_examples,
+    split_sample,
+)
+
+_DESCRIPTION = """
+CLIC EDM4HEP dataset with ee -> WW at 380 GeV, using hit-level information.
+  - X: reconstructed hits, variable number N per event
+  - ytarget: stable generator particles, zero-padded to N per event
+  - ycand: empty/zeros for hit-level
+"""
+
+_CITATION = """
+FIXME
+"""
+
+
+class ClicEdmWwFullhadHits(tfds.core.GeneratorBasedBuilder):
+    VERSION = tfds.core.Version(os.environ.get("TFDS_VERSION", "3.1.0"))
+    RELEASE_NOTES = {
+        "3.1.0": "Hit-level version with separated tracker and calorimeter hits",
+    }
+    MANUAL_DOWNLOAD_INSTRUCTIONS = """
+    For the raw input files in ROOT EDM4HEP format, please see the citation above.
+    """
+
+    # create configs 1 ... NUM_SPLITS + 1 that allow to parallelize the dataset building
+    BUILDER_CONFIGS = [tfds.core.BuilderConfig(name=str(group)) for group in range(1, NUM_SPLITS + 1)]
+
+    def __init__(self, *args, **kwargs):
+        kwargs["file_format"] = tfds.core.FileFormat.ARRAY_RECORD
+        super(ClicEdmWwFullhadHits, self).__init__(*args, **kwargs)
+
+    def _info(self) -> tfds.core.DatasetInfo:
+        """Returns the dataset metadata."""
+        return tfds.core.DatasetInfo(
+            builder=self,
+            description=_DESCRIPTION,
+            features=tfds.features.FeaturesDict(
+                {
+                    "X": tfds.features.Tensor(
+                        shape=(
+                            None,
+                            len(X_FEATURES),
+                        ),
+                        dtype=np.float32,
+                    ),
+                    "ytarget": tfds.features.Tensor(shape=(None, len(Y_FEATURES)), dtype=np.float32),
+                    "ycand": tfds.features.Tensor(shape=(None, len(Y_FEATURES)), dtype=np.float32),
+                    "genmet": tfds.features.Scalar(dtype=np.float32),
+                    "genjets": tfds.features.Tensor(shape=(None, 4), dtype=np.float32),
+                    "targetjets": tfds.features.Tensor(shape=(None, 4), dtype=np.float32),
+                }
+            ),
+            supervised_keys=None,
+            homepage="https://github.com/jpata/particleflow",
+            citation=_CITATION,
+            metadata=tfds.core.MetadataDict(
+                x_features=X_FEATURES,
+                y_features=Y_FEATURES,
+            ),
+        )
+
+    def _split_generators(self, dl_manager: tfds.download.DownloadManager):
+        path = dl_manager.manual_dir
+        return split_sample(Path(path / "p8_ee_WW_fullhad_ecm380"), self.builder_config, num_splits=NUM_SPLITS)
+
+    def _generate_examples(self, files):
+        return generate_examples(files)
