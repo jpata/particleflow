a95639e992226aa437692d752f61fa760313ef55
Joosep Pata <joosep.pata@gmail.com>
2026-06-19 15:17:17 +0300

    submission script

---
+++ scripts/tallinn/l40/train_cld_hits_compare_models.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --partition gpu
+#SBATCH --gres gpu:l40:1
+#SBATCH --mem-per-gpu 80G
+#SBATCH --cpus-per-gpu 4
+#SBATCH -o logs/slurm-%x-%j-%N.out
+#SBATCH --job-name=train-cld-hits-%a
+#SBATCH --array=0-3
+
+set -e
+export PF_SITE=tallinn
+
+export NCCL_P2P_DISABLE=1
+export NCCL_DEBUG=INFO
+export NCCL_IB_DISABLE=1
+
+nvidia-smi topo -m
+
+MODELS=(attention gnnlsh hept heptv2)
+MODEL_TYPE=${MODELS[$SLURM_ARRAY_TASK_ID]}
+
+DATA_DIR=$(pixi run python3 scripts/get_param.py particleflow_spec.yaml productions.cld.workspace_dir)/tfds/
+uv run python3 mlpf/pipeline.py \
+    --spec-file particleflow_spec.yaml \
+    --model-name pyg-cld-hits-v1 \
+    --production cld \
+    --data-dir $DATA_DIR \
+    train \
+    --gpus 1 \
+    --num_workers 4 \
+    --prefetch_factor 2 \
+    --model.type $MODEL_TYPE \
+    --gpu_batch_multiplier 2
